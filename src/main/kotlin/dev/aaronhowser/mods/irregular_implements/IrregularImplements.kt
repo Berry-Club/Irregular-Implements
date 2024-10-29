c4dfe5664da4fff969d95558df36fa7e90bfde77
Aaron Howser <aaronhowser1@gmail.com>
2024-10-29 10:24:41 -0500

    The rest

---
+++ src/main/kotlin/dev/aaronhowser/mods/irregular_implements/IrregularImplements.kt
@@ -0,0 +1,28 @@
+package dev.aaronhowser.mods.irregular_implements
+
+import net.neoforged.api.distmarker.Dist
+import net.neoforged.fml.ModContainer
+import net.neoforged.fml.common.Mod
+import net.neoforged.neoforge.client.gui.ConfigurationScreen
+import net.neoforged.neoforge.client.gui.IConfigScreenFactory
+import org.apache.logging.log4j.LogManager
+import org.apache.logging.log4j.Logger
+import thedarkcolour.kotlinforforge.neoforge.forge.runWhenOn
+
+@Mod(IrregularImplements.ID)
+class IrregularImplements(
+    modContainer: ModContainer
+) {
+    companion object {
+        const val ID = "irregular_implements"
+        val LOGGER: Logger = LogManager.getLogger(ID)
+    }
+
+    init {
+        runWhenOn(Dist.CLIENT) {
+            val screenFactory = IConfigScreenFactory { container, screen -> ConfigurationScreen(container, screen) }
+            modContainer.registerExtensionPoint(IConfigScreenFactory::class.java, screenFactory)
+        }
+    }
+
+}
