6d692a668b171a13e4221c3b994382fa2f4030e5
Aaron Howser <aaronhowser1@gmail.com>
2024-10-30 15:34:07 -0500

    actually lets start with a simpler one lmao

--- src/main/java/dev/aaronhowser/mods/irregular_implements/mixin/BlockRenderDispatcherMixins.java
+++ src/main/java/dev/aaronhowser/mods/irregular_implements/mixin/BlockRenderDispatcherMixins.java
@@ -1,15 +1,21 @@
 package dev.aaronhowser.mods.irregular_implements.mixin;
 
 import com.mojang.blaze3d.vertex.PoseStack;
+import dev.aaronhowser.mods.irregular_implements.block.block_entity.LightRedirectorBlockEntity;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.block.BlockRenderDispatcher;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.state.BlockState;
 import net.neoforged.neoforge.client.model.data.ModelData;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.HashSet;
 
 @Mixin(BlockRenderDispatcher.class)
 public class BlockRenderDispatcherMixins {
@@ -29,5 +35,20 @@ public class BlockRenderDispatcherMixins {
             CallbackInfo ci
     ) {
 
+        synchronized (LightRedirectorBlockEntity.Companion.getRedirectorSet()) {
+            if (LightRedirectorBlockEntity.Companion.getRedirectorSet().isEmpty()) return;
+
+            BlockPos changedPos = irregular_Implements$getSwitchedPosition(levelReader, pos);
+
+        }
     }
+
+    @Unique
+    private static HashSet<BlockPos> irregular_Implements$posSet = new HashSet<>();
+
+    @Unique
+    private static BlockPos irregular_Implements$getSwitchedPosition(LevelReader levelReader, BlockPos pos) {
+
+    }
+
 }
--- src/main/kotlin/dev/aaronhowser/mods/irregular_implements/block/block_entity/LightRedirectorBlockEntity.kt
+++ src/main/kotlin/dev/aaronhowser/mods/irregular_implements/block/block_entity/LightRedirectorBlockEntity.kt
@@ -2,11 +2,24 @@ package dev.aaronhowser.mods.irregular_implements.block.block_entity
 
 import dev.aaronhowser.mods.irregular_implements.registries.ModBlockEntities
 import net.minecraft.core.BlockPos
+import net.minecraft.core.Direction
 import net.minecraft.world.level.block.entity.BlockEntity
 import net.minecraft.world.level.block.state.BlockState
+import java.util.*
 
 class LightRedirectorBlockEntity(
     pPos: BlockPos,
     pBlockState: BlockState
 ) : BlockEntity(ModBlockEntities.LIGHT_REDIRECTOR.get(), pPos, pBlockState) {
+
+    companion object {
+        val redirectorSet: HashSet<LightRedirectorBlockEntity> = HashSet()
+    }
+
+    val targets: EnumMap<Direction, Boolean> = EnumMap(Direction::class.java)
+
+    init {
+        redirectorSet.add(this)
+    }
+
 }
