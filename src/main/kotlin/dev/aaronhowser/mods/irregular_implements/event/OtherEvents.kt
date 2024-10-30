10272915ad66bce5a88d635941c6cec17be8bdea
Aaron Howser <aaronhowser1@gmail.com>
2024-10-30 15:53:08 -0500

    event

---
+++ src/main/kotlin/dev/aaronhowser/mods/irregular_implements/event/OtherEvents.kt
@@ -0,0 +1,19 @@
+package dev.aaronhowser.mods.irregular_implements.event
+
+import dev.aaronhowser.mods.irregular_implements.IrregularImplements
+import net.minecraft.network.chat.Component
+import net.neoforged.bus.api.SubscribeEvent
+import net.neoforged.fml.common.EventBusSubscriber
+import net.neoforged.neoforge.event.ServerChatEvent
+
+@EventBusSubscriber(
+    modid = IrregularImplements.ID
+)
+object OtherEvents {
+
+    @SubscribeEvent
+    fun onChat(event: ServerChatEvent) {
+        event.player.sendSystemMessage(Component.literal("Is raining: ${event.player.level().isRainingAt(event.player.blockPosition())}"))
+    }
+
+}
