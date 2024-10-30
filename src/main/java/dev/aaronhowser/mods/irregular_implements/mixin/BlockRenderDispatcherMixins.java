94887230a97519fb85e2a0e31898d16d158178a7
Aaron Howser <aaronhowser1@gmail.com>
2024-10-30 15:12:17 -0500

    meep

--- src/main/java/dev/aaronhowser/mods/irregular_implements/mixin/BlockRenderDispatcherMixins.java
+++ src/main/java/dev/aaronhowser/mods/irregular_implements/mixin/BlockRenderDispatcherMixins.java
@@ -2,8 +2,10 @@ package dev.aaronhowser.mods.irregular_implements.mixin;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.block.BlockRenderDispatcher;
 import net.minecraft.world.level.block.state.BlockState;
+import net.neoforged.neoforge.client.model.data.ModelData;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -13,10 +15,19 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class BlockRenderDispatcherMixins {
 
     @Inject(
-            method = "renderSingleBlock(Lnet/minecraft/world/level/block/state/BlockState;Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;II)V",
+            method = "renderSingleBlock(Lnet/minecraft/world/level/block/state/BlockState;Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;IILnet/neoforged/neoforge/client/model/data/ModelData;Lnet/minecraft/client/renderer/RenderType;)V",
             at = @At("HEAD")
     )
-    private void renderSingleBlock(BlockState state, PoseStack poseStack, MultiBufferSource bufferSource, int packedLight, int packedOverlay, CallbackInfo ci) {
+    private void renderSingleBlock(
+            BlockState state,
+            PoseStack poseStack,
+            MultiBufferSource bufferSource,
+            int packedLight,
+            int packedOverlay,
+            ModelData modelData,
+            RenderType renderType,
+            CallbackInfo ci
+    ) {
 
     }
 }
