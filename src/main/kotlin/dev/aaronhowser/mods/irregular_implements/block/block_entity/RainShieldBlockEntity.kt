b4381c0d86dcf33ddaa415c8a1f4334d6f825bc4
Aaron Howser <aaronhowser1@gmail.com>
2024-10-31 10:04:01 -0500

    remove sync

--- src/main/kotlin/dev/aaronhowser/mods/irregular_implements/block/block_entity/RainShieldBlockEntity.kt
+++ src/main/kotlin/dev/aaronhowser/mods/irregular_implements/block/block_entity/RainShieldBlockEntity.kt
@@ -20,32 +20,28 @@ class RainShieldBlockEntity(
         val shields: MutableSet<RainShieldBlockEntity> = mutableSetOf()
 
         fun isNearActiveRainShield(level: LevelReader, blockPos: BlockPos): Boolean {
-            synchronized(shields) {
-                if (rainCache.getOrDefault(blockPos, false)) {
-                    return true
-                }
-
-                for (shield in shields) {
-                    if (shield.level != level) continue
-                    if (shield.isRemoved) continue
-                    if (!shield.blockState.getValue(RainShieldBlock.ENABLED)) continue
-                    if (!shield.blockPos.closerThan(blockPos, 10.0)) continue
-
-                    rainCache[blockPos] = true
-                    return true
-                }
-
-                rainCache[blockPos] = false
-                return false
+            if (rainCache.getOrDefault(blockPos, false)) {
+                return true
             }
+
+            for (shield in shields) {
+                if (shield.level != level) continue
+                if (shield.isRemoved) continue
+                if (!shield.blockState.getValue(RainShieldBlock.ENABLED)) continue
+                if (!shield.blockPos.closerThan(blockPos, 10.0)) continue
+
+                rainCache[blockPos] = true
+                return true
+            }
+
+            rainCache[blockPos] = false
+            return false
         }
 
     }
 
     init {
-        synchronized(shields) {
-            shields.add(this)
-        }
+        shields.add(this)
     }
 
 }
