5b709eeb4a292b92bac9016c81c7190f1c6eb603
Aaron Howser <aaronhowser1@gmail.com>
2024-11-01 21:54:39 -0500

    improve block of sticks

--- src/main/kotlin/dev/aaronhowser/mods/irregular_implements/block/BlockOfSticksBlock.kt
+++ src/main/kotlin/dev/aaronhowser/mods/irregular_implements/block/BlockOfSticksBlock.kt
@@ -1,13 +1,17 @@
 package dev.aaronhowser.mods.irregular_implements.block
 
+import dev.aaronhowser.mods.irregular_implements.util.OtherUtil.isTrue
 import net.minecraft.core.BlockPos
 import net.minecraft.server.level.ServerLevel
 import net.minecraft.util.RandomSource
+import net.minecraft.world.item.context.BlockPlaceContext
 import net.minecraft.world.level.Level
 import net.minecraft.world.level.block.Block
 import net.minecraft.world.level.block.Blocks
 import net.minecraft.world.level.block.SoundType
 import net.minecraft.world.level.block.state.BlockState
+import net.minecraft.world.level.block.state.StateDefinition
+import net.minecraft.world.level.block.state.properties.BooleanProperty
 import net.minecraft.world.level.material.MapColor
 import net.minecraft.world.level.material.PushReaction
 
@@ -26,6 +30,28 @@ class BlockOfSticksBlock(
         .noOcclusion()
 ) {
 
+    companion object {
+        val SHOULD_DROP: BooleanProperty = BooleanProperty.create("should_drop")
+    }
+
+    init {
+        registerDefaultState(
+            defaultBlockState()
+                .setValue(SHOULD_DROP, true)
+        )
+    }
+
+    override fun createBlockStateDefinition(builder: StateDefinition.Builder<Block, BlockState>) {
+        builder.add(SHOULD_DROP)
+    }
+
+    override fun getStateForPlacement(context: BlockPlaceContext): BlockState? {
+        return defaultBlockState().setValue(
+            SHOULD_DROP,
+            !context.player?.hasInfiniteMaterials().isTrue  // Only drop if placed by a player with infinite materials
+        )
+    }
+
     override fun onPlace(
         pState: BlockState,
         pLevel: Level,
@@ -43,16 +69,7 @@ class BlockOfSticksBlock(
         pPos: BlockPos,
         pRandom: RandomSource
     ) {
-        val nearestPlayer = pLevel.getNearestPlayer(
-            pPos.x.toDouble(),
-            pPos.y.toDouble(),
-            pPos.z.toDouble(),
-            100.0,
-            false   // "Should exclude creative players" == false
-        )
-
-        //TODO: Make this a blockstate property instead
-        val shouldDrop = nearestPlayer == null || !nearestPlayer.hasInfiniteMaterials()
+        val shouldDrop = pState.getValue(SHOULD_DROP)
 
         if (!this.returning) {
             pLevel.destroyBlock(pPos, shouldDrop)
@@ -60,6 +77,14 @@ class BlockOfSticksBlock(
         }
 
         if (shouldDrop) {
+            val nearestPlayer = pLevel.getNearestPlayer(
+                pPos.x.toDouble(),
+                pPos.y.toDouble(),
+                pPos.z.toDouble(),
+                100.0,
+                false   // "Should exclude creative players" == false
+            )
+
             val drops = getDrops(pState, pLevel, pPos, null)
             for (drop in drops) {
                 popResource(pLevel, nearestPlayer?.blockPosition() ?: pPos, drop)
