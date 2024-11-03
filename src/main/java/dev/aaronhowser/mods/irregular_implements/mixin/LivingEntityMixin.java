5cb1f61fed904f781070b66816b3979473355048
Aaron Howser <aaronhowser1@gmail.com>
2024-11-03 11:48:19 -0600

    magic hood particles

--- src/main/java/dev/aaronhowser/mods/irregular_implements/mixin/LivingEntityMixin.java
+++ src/main/java/dev/aaronhowser/mods/irregular_implements/mixin/LivingEntityMixin.java
@@ -2,12 +2,16 @@ package dev.aaronhowser.mods.irregular_implements.mixin;
 
 import dev.aaronhowser.mods.irregular_implements.datagen.tag.ModBlockTagsProvider;
 import dev.aaronhowser.mods.irregular_implements.registries.ModItems;
+import net.minecraft.core.particles.ParticleOptions;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.List;
 
 @Mixin(LivingEntity.class)
 public abstract class LivingEntityMixin {
@@ -40,6 +44,24 @@ public abstract class LivingEntityMixin {
         ) {
             cir.setReturnValue(true);
         }
+    }
+
+    @ModifyVariable(
+            method = "updateSynchronizedMobEffectParticles",
+            at = @At(
+                    value = "STORE",
+                    target = "Ljava/util/stream/Stream;toList()Ljava/util/List;"
+            )
+    )
+    private List<ParticleOptions> hideMobEffectParticles(List<ParticleOptions> value) {
+        if (
+                ((LivingEntity) (Object) this)
+                        .getItemBySlot(EquipmentSlot.HEAD)
+                        .is(ModItems.INSTANCE.getMAGIC_HOOD())
+        ) {
+            return List.of();
+        }
+        return value;
     }
 
 }
