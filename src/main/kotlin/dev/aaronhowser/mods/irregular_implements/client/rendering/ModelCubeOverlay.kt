9972edfdb3445fbcd572c7e85c0d2e8a967561dd
Aaron Howser <aaronhowser1@gmail.com>
2024-11-05 20:38:42 -0600

    FUCK those classes im doing it my OWN STYLE

--- src/main/kotlin/dev/aaronhowser/mods/irregular_implements/client/rendering/ModelCubeOverlay.kt
+++
@@ -1,6 +0,0 @@
-package dev.aaronhowser.mods.irregular_implements.client.rendering
-
-import net.neoforged.neoforge.client.extensions.IBakedModelExtension
-
-class ModelCubeOverlay : IBakedModelExtension {
-}
