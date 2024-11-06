ea056576cace38fa64ef8efdb1ba2216358b7065
Aaron Howser <aaronhowser1@gmail.com>
2024-11-06 00:39:53 -0600

    *sigh*

--- src/main/kotlin/dev/aaronhowser/mods/irregular_implements/block/renderer/CustomCraftingTableBER.kt
+++ src/main/kotlin/dev/aaronhowser/mods/irregular_implements/block/renderer/CustomCraftingTableBER.kt
@@ -1,14 +1,26 @@
 package dev.aaronhowser.mods.irregular_implements.block.renderer
 
 import com.mojang.blaze3d.vertex.PoseStack
+import com.mojang.blaze3d.vertex.VertexConsumer
 import dev.aaronhowser.mods.irregular_implements.block.block_entity.CustomCraftingTableBlockEntity
+import dev.aaronhowser.mods.irregular_implements.util.OtherUtil
+import net.minecraft.client.renderer.LightTexture
 import net.minecraft.client.renderer.MultiBufferSource
+import net.minecraft.client.renderer.RenderType
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderer
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider
+import org.joml.Matrix4f
 
 class CustomCraftingTableBER(
     val context: BlockEntityRendererProvider.Context
 ) : BlockEntityRenderer<CustomCraftingTableBlockEntity> {
+
+    companion object {
+        val TOP_TEXTURE = OtherUtil.modResource("textures/block/crafting_table/top.png")
+        val SIDE_TEXTURE = OtherUtil.modResource("textures/block/crafting_table/side.png")
+        val FRONT_TEXTURE = OtherUtil.modResource("textures/block/crafting_table/front.png")
+    }
+
     override fun render(
         blockEntity: CustomCraftingTableBlockEntity,
         partialTick: Float,
@@ -21,10 +33,6 @@ class CustomCraftingTableBER(
 
         poseStack.pushPose()
 
-        //FIXME: This is obviously a horrible way of doing it. I'm just trying to get it to work for now.
-        poseStack.scale(0.999f, 0.999f, 0.999f)
-        poseStack.translate(0.0005, 0.0005, 0.0005)
-
         context.blockRenderDispatcher
             .renderSingleBlock(
                 baseBlock.defaultBlockState(),
@@ -34,6 +42,61 @@ class CustomCraftingTableBER(
                 packedOverlay
             )
 
+        renderTop(
+            bufferSource.getBuffer(RenderType.entityTranslucent(TOP_TEXTURE)),
+            poseStack.last().pose(),
+            LightTexture.FULL_BRIGHT,
+            packedOverlay
+        )
+
         poseStack.popPose()
     }
+
+    private fun renderTop(
+        vertexConsumer: VertexConsumer,
+        matrix: Matrix4f,
+        packedLight: Int,
+        packedOverlay: Int,
+    ) {
+        val xMin = 0.0f
+        val xMax = 1.0f
+        val zMin = 0.0f
+        val zMax = 1.0f
+        val yTop = 1.01f
+
+        vertexConsumer
+            .addVertex(matrix, xMin, yTop, zMin)
+            .setColor(1.0f, 1.0f, 1.0f, 1.0f)
+            .setUv(1f, 0f)
+            .setOverlay(packedOverlay)
+            .setLight(packedLight)
+            .setNormal(0.0f, 1.0f, 0.0f)
+
+        vertexConsumer
+            .addVertex(matrix, xMin, yTop, zMax)
+            .setColor(1.0f, 1.0f, 1.0f, 1.0f)
+            .setUv(1f, 0f)
+            .setOverlay(packedOverlay)
+            .setLight(packedLight)
+            .setNormal(0.0f, 1.0f, 0.0f)
+
+        vertexConsumer
+            .addVertex(matrix, xMax, yTop, zMax)
+            .setColor(1.0f, 1.0f, 1.0f, 1.0f)
+            .setUv(1f, 0f)
+            .setOverlay(packedOverlay)
+            .setLight(packedLight)
+            .setNormal(0.0f, 1.0f, 0.0f)
+
+        vertexConsumer
+            .addVertex(matrix, xMax, yTop, zMin)
+            .setColor(1.0f, 1.0f, 1.0f, 1.0f)
+            .setUv(1f, 0f)
+            .setOverlay(packedOverlay)
+            .setLight(packedLight)
+            .setNormal(0.0f, 1.0f, 0.0f)
+
+    }
+
+
 }
