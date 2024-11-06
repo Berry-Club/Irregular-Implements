d97f2c33c5816c98536dfaf0f1f3f325335a9913
Aaron Howser <aaronhowser1@gmail.com>
2024-11-05 18:49:34 -0600

    trying to improve it

--- src/main/kotlin/dev/aaronhowser/mods/irregular_implements/block/TriggerGlass.kt
+++ src/main/kotlin/dev/aaronhowser/mods/irregular_implements/block/TriggerGlass.kt
@@ -4,7 +4,6 @@ import net.minecraft.core.BlockPos
 import net.minecraft.core.Direction
 import net.minecraft.server.level.ServerLevel
 import net.minecraft.util.RandomSource
-import net.minecraft.world.item.context.BlockPlaceContext
 import net.minecraft.world.level.BlockGetter
 import net.minecraft.world.level.Level
 import net.minecraft.world.level.block.Block
@@ -13,6 +12,7 @@ import net.minecraft.world.level.block.TransparentBlock
 import net.minecraft.world.level.block.state.BlockState
 import net.minecraft.world.level.block.state.StateDefinition
 import net.minecraft.world.level.block.state.properties.BooleanProperty
+import net.minecraft.world.level.block.state.properties.IntegerProperty
 import net.minecraft.world.phys.shapes.CollisionContext
 import net.minecraft.world.phys.shapes.Shapes
 import net.minecraft.world.phys.shapes.VoxelShape
@@ -24,23 +24,22 @@ class TriggerGlass : TransparentBlock(
 ) {
 
     companion object {
+        private const val MAX_RECURSIONS = 50
+
         val NOT_SOLID: BooleanProperty = BooleanProperty.create("not_solid")
+        val ITERATIONS_REMAINING: IntegerProperty = IntegerProperty.create("iterations_remaining", 0, MAX_RECURSIONS)
     }
 
     init {
         registerDefaultState(
             stateDefinition.any()
                 .setValue(NOT_SOLID, false)
+                .setValue(ITERATIONS_REMAINING, MAX_RECURSIONS)
         )
     }
 
     override fun createBlockStateDefinition(builder: StateDefinition.Builder<Block, BlockState>) {
-        builder.add(NOT_SOLID)
-    }
-
-    override fun getStateForPlacement(context: BlockPlaceContext): BlockState? {
-        return defaultBlockState()
-            .setValue(NOT_SOLID, false)
+        builder.add(NOT_SOLID, ITERATIONS_REMAINING)
     }
 
     override fun getShape(state: BlockState, level: BlockGetter, pos: BlockPos, context: CollisionContext): VoxelShape {
@@ -68,26 +67,31 @@ class TriggerGlass : TransparentBlock(
         val isPowered = level.hasNeighborSignal(pos)
         if (!isPowered) return
 
-        propagate(level, pos, 0)
+        propagate(level, pos, MAX_RECURSIONS)
     }
 
     private fun propagate(
         level: Level,
         pos: BlockPos,
-        recursions: Int
+        recursionsLeft: Int
     ) {
-        if (recursions >= 20) return        //TODO: Configurable range
+        if (recursionsLeft <= 0) return        //TODO: Configurable range
 
         val state = level.getBlockState(pos)
         if (state.block != this) return
-        if (state.getValue(NOT_SOLID)) return
 
-        level.setBlockAndUpdate(pos, state.setValue(NOT_SOLID, true))
+        if (state.getValue(NOT_SOLID) && state.getValue(ITERATIONS_REMAINING) >= recursionsLeft) return
+
+        val newState = state
+            .setValue(NOT_SOLID, true)
+            .setValue(ITERATIONS_REMAINING, recursionsLeft)
+
+        level.setBlock(pos, newState, 2)
         level.scheduleTick(pos, this, 20 * 3)   //TODO: configurable duration
 
         for (direction in Direction.entries) {
             val offset = pos.relative(direction)
-            propagate(level, offset, recursions + 1)
+            propagate(level, offset, recursionsLeft - 1)
         }
     }
 
@@ -97,7 +101,12 @@ class TriggerGlass : TransparentBlock(
         pos: BlockPos,
         random: RandomSource
     ) {
-        level.setBlockAndUpdate(pos, state.setValue(NOT_SOLID, false))
+        level.setBlock(pos, defaultBlockState(), 2)
+
+        val isPowered = level.hasNeighborSignal(pos)
+        if (isPowered) {
+            propagate(level, pos, MAX_RECURSIONS)
+        }
     }
 
 }
