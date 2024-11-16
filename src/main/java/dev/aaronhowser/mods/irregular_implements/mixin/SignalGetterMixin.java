0d8099e5fd67c99c803d2f7dbdf36e7c16aa7280
Aaron Howser <aaronhowser1@gmail.com>
2024-11-16 13:33:21 -0600

    works???????? not really?????

--- src/main/java/dev/aaronhowser/mods/irregular_implements/mixin/SignalGetterMixin.java
+++ src/main/java/dev/aaronhowser/mods/irregular_implements/mixin/SignalGetterMixin.java
@@ -11,14 +11,35 @@ import org.spongepowered.asm.mixin.injection.At;
 @Mixin(SignalGetter.class)
 public interface SignalGetterMixin {
 
+    //FIXME: This is checked for EVERY SURROUNDING BLOCK, not the block itself!
     @ModifyReturnValue(
             method = "getSignal",
             at = @At("RETURN")
     )
-    default int getSignal(int original, BlockPos pos, Direction direction) {
+    default int irregular_implements$getSignal(int original, BlockPos pos, Direction direction) {
         return original >= 15 || !(this instanceof PoweredRedstoneInterfaces f)
                 ? original
                 : Math.max(original, f.irregular_implements$getStrongPower(pos, direction));
+    }
+
+    @ModifyReturnValue(
+            method = "hasNeighborSignal",
+            at = @At("RETURN")
+    )
+    default boolean irregular_implements$hasNeighborSignal(boolean original, BlockPos pos) {
+        return original || !(this instanceof PoweredRedstoneInterfaces f)
+                ? original
+                : f.irregular_implements$getStrongPower(pos, null) > 0;
+    }
+
+    @ModifyReturnValue(
+            method = "getBestNeighborSignal",
+            at = @At("RETURN")
+    )
+    default int irregular_implements$getBestNeighborSignal(int original, BlockPos pos) {
+        return original >= 15 || !(this instanceof PoweredRedstoneInterfaces f)
+                ? original
+                : Math.max(original, f.irregular_implements$getStrongPower(pos, null));
     }
 
 }
