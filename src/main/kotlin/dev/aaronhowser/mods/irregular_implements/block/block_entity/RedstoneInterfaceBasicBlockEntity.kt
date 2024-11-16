cdabf241f9d1eef4c33015483c5d10339c35373a
Aaron Howser <aaronhowser1@gmail.com>
2024-11-16 14:55:31 -0600

    Moved logic to the interface

--- src/main/kotlin/dev/aaronhowser/mods/irregular_implements/block/block_entity/RedstoneInterfaceBasicBlockEntity.kt
+++ src/main/kotlin/dev/aaronhowser/mods/irregular_implements/block/block_entity/RedstoneInterfaceBasicBlockEntity.kt
@@ -3,7 +3,6 @@ package dev.aaronhowser.mods.irregular_implements.block.block_entity
 import dev.aaronhowser.mods.irregular_implements.block.block_entity.base.RedstoneInterfaceBlockEntity
 import dev.aaronhowser.mods.irregular_implements.block.block_entity.base.RedstoneToolLinkable
 import dev.aaronhowser.mods.irregular_implements.registries.ModBlockEntities
-import dev.aaronhowser.mods.irregular_implements.util.ServerScheduler
 import net.minecraft.core.BlockPos
 import net.minecraft.core.HolderLookup
 import net.minecraft.nbt.CompoundTag
@@ -43,21 +42,12 @@ class RedstoneInterfaceBasicBlockEntity(
 
     override fun saveAdditional(tag: CompoundTag, registries: HolderLookup.Provider) {
         super.saveAdditional(tag, registries)
-
-        val linkedPos = linkedPos
-        if (linkedPos != null) {
-            tag.putLong(RedstoneToolLinkable.LINKED_POS_NBT, linkedPos.asLong())
-        }
+        saveToTag(tag)
     }
 
     override fun loadAdditional(tag: CompoundTag, registries: HolderLookup.Provider) {
         super.loadAdditional(tag, registries)
-
-        if (tag.contains(RedstoneToolLinkable.LINKED_POS_NBT)) {
-            ServerScheduler.scheduleTaskInTicks(1) {
-                linkedPos = BlockPos.of(tag.getLong(RedstoneToolLinkable.LINKED_POS_NBT))
-            }
-        }
+        loadFromTag(tag)
     }
 
 }
--- src/main/kotlin/dev/aaronhowser/mods/irregular_implements/block/block_entity/base/RedstoneToolLinkable.kt
+++ src/main/kotlin/dev/aaronhowser/mods/irregular_implements/block/block_entity/base/RedstoneToolLinkable.kt
@@ -1,13 +1,30 @@
 package dev.aaronhowser.mods.irregular_implements.block.block_entity.base
 
+import dev.aaronhowser.mods.irregular_implements.util.ServerScheduler
 import net.minecraft.core.BlockPos
+import net.minecraft.nbt.CompoundTag
 
 interface RedstoneToolLinkable {
 
     companion object {
-        const val LINKED_POS_NBT = "linked_pos"
+        private const val LINKED_POS_NBT = "linked_pos"
     }
 
     var linkedPos: BlockPos?
+
+    fun loadFromTag(tag: CompoundTag) {
+        if (tag.contains(LINKED_POS_NBT)) {
+            ServerScheduler.scheduleTaskInTicks(1) {
+                linkedPos = BlockPos.of(tag.getLong(LINKED_POS_NBT))
+            }
+        }
+    }
+
+    fun saveToTag(tag: CompoundTag) {
+        val linkedPos = linkedPos
+        if (linkedPos != null) {
+            tag.putLong(LINKED_POS_NBT, linkedPos.asLong())
+        }
+    }
 
 }
