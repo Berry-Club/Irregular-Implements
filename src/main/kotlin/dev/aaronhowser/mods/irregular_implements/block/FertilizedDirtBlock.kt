7c2c59a6bf437763c4add1dcd01d8b179e7c1899
Aaron Howser <aaronhowser1@gmail.com>
2024-11-30 09:10:45 -0600

    Working on FertilizedDirtBlock

--- src/main/kotlin/dev/aaronhowser/mods/irregular_implements/block/FertilizedDirtBlock.kt
+++ src/main/kotlin/dev/aaronhowser/mods/irregular_implements/block/FertilizedDirtBlock.kt
@@ -1,21 +1,61 @@
 package dev.aaronhowser.mods.irregular_implements.block
 
 import net.minecraft.core.BlockPos
-import net.minecraft.world.entity.Entity
-import net.minecraft.world.level.Level
-import net.minecraft.world.level.LevelReader
+import net.minecraft.core.Direction
+import net.minecraft.world.level.BlockGetter
+import net.minecraft.world.level.block.Block
 import net.minecraft.world.level.block.Blocks
-import net.minecraft.world.level.block.FarmBlock
 import net.minecraft.world.level.block.state.BlockState
+import net.minecraft.world.level.block.state.StateDefinition
+import net.minecraft.world.level.block.state.properties.BooleanProperty
+import net.minecraft.world.phys.shapes.CollisionContext
+import net.minecraft.world.phys.shapes.Shapes
+import net.minecraft.world.phys.shapes.VoxelShape
+import net.neoforged.neoforge.common.util.TriState
 
-class FertilizedDirtBlock : FarmBlock(Properties.ofFullCopy(Blocks.FARMLAND)) {
+class FertilizedDirtBlock : Block(Properties.ofFullCopy(Blocks.FARMLAND)) {
 
-    override fun canSurvive(state: BlockState, level: LevelReader, pos: BlockPos): Boolean {
-        return true
+    companion object {
+        val TILLED: BooleanProperty = BooleanProperty.create("tilled")
+
+        val TILLED_SHAPE: VoxelShape = box(0.0, 0.0, 0.0, 16.0, 15.0, 16.0)
     }
 
-    override fun fallOn(level: Level, state: BlockState, pos: BlockPos, entity: Entity, fallDistance: Float) {
-        // Do nothing
+    init {
+        registerDefaultState(
+            stateDefinition.any()
+                .setValue(TILLED, false)
+        )
+    }
+
+    override fun createBlockStateDefinition(builder: StateDefinition.Builder<Block, BlockState>) {
+        builder.add(TILLED)
+    }
+
+    override fun getShape(state: BlockState, level: BlockGetter, pos: BlockPos, context: CollisionContext): VoxelShape {
+        return if (state.getValue(TILLED)) {
+            TILLED_SHAPE
+        } else {
+            Shapes.block()
+        }
+    }
+
+    override fun isFertile(state: BlockState, level: BlockGetter, pos: BlockPos): Boolean {
+        return state.getValue(TILLED)
+    }
+
+    override fun isOcclusionShapeFullBlock(state: BlockState, level: BlockGetter, pos: BlockPos): Boolean {
+        return !state.getValue(TILLED)
+    }
+
+    override fun canSustainPlant(
+        state: BlockState,
+        level: BlockGetter,
+        soilPosition: BlockPos,
+        facing: Direction,
+        plant: BlockState
+    ): TriState {
+
     }
 
 }
