3e198768a69c632af76b86b24e52574af90221a4
Aaron Howser <aaronhowser1@gmail.com>
2024-11-30 05:11:19 -0600

    Revert "getting partway through making a custom emi recipe for it but then gave up bc i cant i think actually"
    This reverts commit 74d55361a3a527424630f3524156511eccf7c2a5.

--- src/main/kotlin/dev/aaronhowser/mods/irregular_implements/recipe/LubricateBootRecipe.kt
+++ src/main/kotlin/dev/aaronhowser/mods/irregular_implements/recipe/LubricateBootRecipe.kt
@@ -15,12 +15,14 @@ class LubricateBootRecipe(
     craftingCategory: CraftingBookCategory = CraftingBookCategory.MISC
 ) : CustomRecipe(craftingCategory) {
 
-    companion object {
-        val boots = BuiltInRegistries.ITEM.filter { it is ArmorItem && it.type == ArmorItem.Type.BOOTS }.map { it.defaultInstance }
-    }
-
-    val bootIngredient: Ingredient = Ingredient.of(*boots.toTypedArray())
+    val bootIngredient: Ingredient
     val lubeIngredient: Ingredient = Ingredient.of(ModItems.SUPER_LUBRICANT_TINCTURE)
+
+    init {
+        val boots = BuiltInRegistries.ITEM.filter { it is ArmorItem && it.type == ArmorItem.Type.BOOTS }.map { it.defaultInstance }
+
+        bootIngredient = Ingredient.of(*boots.toTypedArray())
+    }
 
     override fun matches(input: CraftingInput, level: Level): Boolean {
         var bootStack: ItemStack? = null
