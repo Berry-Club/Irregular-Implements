4a6cc85c2eeb968e149d5b4aa6461424251f0f75
Aaron Howser <aaronhowser1@gmail.com>
2024-11-29 22:03:23 -0600

    cancel all that

--- src/main/kotlin/dev/aaronhowser/mods/irregular_implements/event/OtherEvents.kt
+++ src/main/kotlin/dev/aaronhowser/mods/irregular_implements/event/OtherEvents.kt
@@ -4,14 +4,12 @@ import dev.aaronhowser.mods.irregular_implements.IrregularImplements
 import dev.aaronhowser.mods.irregular_implements.block.ContactButton
 import dev.aaronhowser.mods.irregular_implements.block.ContactLever
 import dev.aaronhowser.mods.irregular_implements.effect.ImbueEffect
-import dev.aaronhowser.mods.irregular_implements.item.FluidWalkingArmorItem
 import net.minecraft.world.InteractionHand
 import net.neoforged.bus.api.SubscribeEvent
 import net.neoforged.fml.common.EventBusSubscriber
 import net.neoforged.neoforge.event.entity.living.LivingDamageEvent
 import net.neoforged.neoforge.event.entity.living.LivingExperienceDropEvent
 import net.neoforged.neoforge.event.entity.player.PlayerInteractEvent
-import net.neoforged.neoforge.event.tick.EntityTickEvent
 
 @EventBusSubscriber(
     modid = IrregularImplements.ID
@@ -37,11 +35,6 @@ object OtherEvents {
             ContactLever.handleClickBlock(level, pos)
             ContactButton.handleClickBlock(level, pos)
         }
-    }
-
-    @SubscribeEvent
-    fun onLivingTick(event: EntityTickEvent.Post) {
-        FluidWalkingArmorItem.moveToSurface(event)
     }
 
 }
