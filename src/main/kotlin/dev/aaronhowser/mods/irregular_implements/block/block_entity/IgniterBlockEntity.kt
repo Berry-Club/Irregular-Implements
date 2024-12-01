767580de02032d1e48cacc05cfc8a5e6714b3200
Aaron Howser <aaronhowser1@gmail.com>
2024-12-01 16:41:31 -0600

    Working on Igniter

--- src/main/kotlin/dev/aaronhowser/mods/irregular_implements/block/block_entity/IgniterBlockEntity.kt
+++ src/main/kotlin/dev/aaronhowser/mods/irregular_implements/block/block_entity/IgniterBlockEntity.kt
@@ -2,6 +2,11 @@ package dev.aaronhowser.mods.irregular_implements.block.block_entity
 
 import dev.aaronhowser.mods.irregular_implements.registry.ModBlockEntities
 import net.minecraft.core.BlockPos
+import net.minecraft.core.HolderLookup
+import net.minecraft.nbt.CompoundTag
+import net.minecraft.network.protocol.Packet
+import net.minecraft.network.protocol.game.ClientGamePacketListener
+import net.minecraft.network.protocol.game.ClientboundBlockEntityDataPacket
 import net.minecraft.world.level.block.entity.BlockEntity
 import net.minecraft.world.level.block.state.BlockState
 
@@ -9,4 +14,21 @@ class IgniterBlockEntity(
     pPos: BlockPos,
     pBlockState: BlockState
 ) : BlockEntity(ModBlockEntities.IGNITER.get(), pPos, pBlockState) {
+
+    enum class Mode {
+        TOGGLE,         // Make fire when powered, extinguish when unpowered
+        IGNITE,         // Make fire when powered, do nothing when unpowered
+        KEEP_IGNITED    // Make fire when powered, make another fire if it goes out while powered
+    }
+
+    private var mode: Mode = Mode.TOGGLE
+        set(value) {
+            field = value
+            setChanged()
+        }
+
+    // Syncs with client
+    override fun getUpdateTag(pRegistries: HolderLookup.Provider): CompoundTag = saveWithoutMetadata(pRegistries)
+    override fun getUpdatePacket(): Packet<ClientGamePacketListener> = ClientboundBlockEntityDataPacket.create(this)
+
 }
