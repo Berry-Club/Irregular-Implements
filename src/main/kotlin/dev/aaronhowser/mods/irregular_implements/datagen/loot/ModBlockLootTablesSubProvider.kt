358a972ed824cb209982658abd2d60305e2ffb37
Aaron Howser <aaronhowser1@gmail.com>
2024-12-02 09:18:14 -0600

    lotus drops seeds, and a blossom if it has one

--- src/main/kotlin/dev/aaronhowser/mods/irregular_implements/datagen/loot/ModBlockLootTablesSubProvider.kt
+++ src/main/kotlin/dev/aaronhowser/mods/irregular_implements/datagen/loot/ModBlockLootTablesSubProvider.kt
@@ -1,6 +1,9 @@
 package dev.aaronhowser.mods.irregular_implements.datagen.loot
 
+import dev.aaronhowser.mods.irregular_implements.block.LotusBlock
 import dev.aaronhowser.mods.irregular_implements.registry.ModBlocks
+import dev.aaronhowser.mods.irregular_implements.registry.ModItems
+import net.minecraft.advancements.critereon.StatePropertiesPredicate
 import net.minecraft.core.HolderLookup
 import net.minecraft.data.loot.BlockLootSubProvider
 import net.minecraft.world.flag.FeatureFlags
@@ -8,6 +11,10 @@ import net.minecraft.world.item.DyeColor
 import net.minecraft.world.item.Items
 import net.minecraft.world.level.block.Block
 import net.minecraft.world.level.block.Blocks
+import net.minecraft.world.level.storage.loot.LootPool
+import net.minecraft.world.level.storage.loot.LootTable
+import net.minecraft.world.level.storage.loot.entries.LootItem
+import net.minecraft.world.level.storage.loot.predicates.LootItemBlockStatePropertyCondition
 
 class ModBlockLootTablesSubProvider(
     provider: HolderLookup.Provider
@@ -29,6 +36,24 @@ class ModBlockLootTablesSubProvider(
 
         add(ModBlocks.COMPRESSED_SLIME_BLOCK.get()) { createSingleItemTable(Blocks.SLIME_BLOCK) }
 
+        add(ModBlocks.LOTUS.get()) {
+            super.applyExplosionDecay(
+                it,
+                LootTable.lootTable()
+                    .withPool(
+                        LootPool.lootPool()
+                            .`when`(
+                                LootItemBlockStatePropertyCondition.hasBlockStateProperties(it)
+                                    .setProperties(StatePropertiesPredicate.Builder.properties().hasProperty(LotusBlock.AGE, LotusBlock.MAXIMUM_AGE))
+                            )
+                            .add(LootItem.lootTableItem(ModItems.LOTUS_BLOSSOM.get()))
+                    )
+                    .withPool(
+                        LootPool.lootPool()
+                            .add(LootItem.lootTableItem(ModItems.LOTUS_SEEDS.get()))
+                    )
+            )
+        }
     }
 
     private val dropsDirtWithoutSilkTouch = buildList {
@@ -38,7 +63,8 @@ class ModBlockLootTablesSubProvider(
     }
 
     private val noDropBlocks = listOf(
-        ModBlocks.BLAZE_FIRE
+        ModBlocks.BLAZE_FIRE,
+        ModBlocks.LOTUS
     ).map { it.get() }.toSet()
 
     private val nonDropSelfBlocks: Set<Block> = noDropBlocks + dropsDirtWithoutSilkTouch + setOf(
