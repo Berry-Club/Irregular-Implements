9c0098e65af3c2b6d61085f0b79c0fae4009cd60
Aaron Howser <aaronhowser1@gmail.com>
2024-12-08 23:40:25 -0600

    RedirectorPlateBlock

--- src/main/kotlin/dev/aaronhowser/mods/irregular_implements/block/plate/RedirectorPlateBlock.kt
+++ src/main/kotlin/dev/aaronhowser/mods/irregular_implements/block/plate/RedirectorPlateBlock.kt
@@ -2,6 +2,7 @@ package dev.aaronhowser.mods.irregular_implements.block.plate
 
 import net.minecraft.core.BlockPos
 import net.minecraft.core.Direction
+import net.minecraft.util.Mth
 import net.minecraft.world.InteractionResult
 import net.minecraft.world.entity.Entity
 import net.minecraft.world.entity.player.Player
@@ -47,7 +48,7 @@ class RedirectorPlateBlock : BasePlateBlock() {
         player: Player,
         hitResult: BlockHitResult
     ): InteractionResult {
-        if (level.isClientSide) return InteractionResult.PASS
+        if (level.isClientSide) return InteractionResult.SUCCESS
 
         val currentInput = state.getValue(INPUT_DIRECTION)
         val currentOutput = state.getValue(OUTPUT_DIRECTION)
@@ -65,13 +66,35 @@ class RedirectorPlateBlock : BasePlateBlock() {
         return InteractionResult.SUCCESS
     }
 
+    //FIXME: Seems to not work great for players, but works fine for everything else
     override fun entityInside(state: BlockState, level: Level, pos: BlockPos, entity: Entity) {
         val entityMotion = entity.deltaMovement
-        val motionDirection = Direction.getNearest(entityMotion.x, entityMotion.y, entityMotion.z)
+        if (entityMotion.horizontalDistanceSqr() < 0.001) return
+
+        val movingInDirection = Direction.getNearest(entityMotion.x, 0.0, entityMotion.z)
 
         val blockCenter = pos.toVec3().add(0.5, 0.0, 0.5)
-        val differenceVec = blockCenter.subtract(entity.position())
+        val entityDisplacementVector = blockCenter.vectorTo(entity.position())
+        val entityComingFromDirection = Direction.getNearest(entityDisplacementVector.x, 0.0, entityDisplacementVector.z)
 
+        val currentInput = state.getValue(INPUT_DIRECTION)
+        val currentOutput = state.getValue(OUTPUT_DIRECTION)
+
+        val moveToDirection = if (entityComingFromDirection == currentInput && movingInDirection == currentInput.opposite) {
+            currentOutput
+        } else if (entityComingFromDirection == currentOutput && movingInDirection == currentOutput.opposite) {
+            currentInput
+        } else {
+            return
+        }
+
+        val moveToPos = moveToDirection.normal.toVec3().scale(0.4).add(blockCenter)
+
+        val lookAngleDifference = entityComingFromDirection.opposite.toYRot() - moveToDirection.toYRot()
+        val outputMotion = entityMotion.yRot(Mth.DEG_TO_RAD * lookAngleDifference)
+
+        entity.teleportTo(moveToPos.x, entity.y, moveToPos.z)
+        entity.deltaMovement = outputMotion
     }
 
 }
