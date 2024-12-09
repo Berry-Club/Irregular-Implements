9f7abe2d9f8c74e0e8c4342faa2e5750fbf9dd09
Aaron Howser <aaronhowser1@gmail.com>
2024-12-09 01:25:02 -0600

    invert controls when collapse

--- src/main/kotlin/dev/aaronhowser/mods/irregular_implements/event/ClientEvents.kt
+++ src/main/kotlin/dev/aaronhowser/mods/irregular_implements/event/ClientEvents.kt
@@ -2,9 +2,12 @@ package dev.aaronhowser.mods.irregular_implements.event
 
 import dev.aaronhowser.mods.irregular_implements.IrregularImplements
 import dev.aaronhowser.mods.irregular_implements.item.ModArmorItems
+import dev.aaronhowser.mods.irregular_implements.registry.ModEffects
+import dev.aaronhowser.mods.irregular_implements.util.ClientUtil
 import net.neoforged.api.distmarker.Dist
 import net.neoforged.bus.api.SubscribeEvent
 import net.neoforged.fml.common.EventBusSubscriber
+import net.neoforged.neoforge.client.event.CalculatePlayerTurnEvent
 import net.neoforged.neoforge.event.entity.player.ItemTooltipEvent
 
 @EventBusSubscriber(
@@ -16,6 +19,16 @@ object ClientEvents {
     @SubscribeEvent
     fun tooltipEvent(event: ItemTooltipEvent) {
         ModArmorItems.tooltip(event)
+    }
+
+    @SubscribeEvent
+    fun onPlayerTurn(event: CalculatePlayerTurnEvent) {
+        val player = ClientUtil.localPlayer ?: return
+
+        //TODO: Add a config to disable this part (or just remove it entirely)
+        if (player.hasEffect(ModEffects.COLLAPSE_IMBUE)) {
+            event.mouseSensitivity *= -1.75
+        }
     }
 
 }
