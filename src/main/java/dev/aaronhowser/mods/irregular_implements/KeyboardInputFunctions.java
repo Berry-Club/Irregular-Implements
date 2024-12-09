0c696450d7dd39e282cdf692f56cd60b2d539190
Aaron Howser <aaronhowser1@gmail.com>
2024-12-09 01:20:02 -0600

    fix Collapse

--- src/main/java/dev/aaronhowser/mods/irregular_implements/KeyboardInputFunctions.java
+++ src/main/java/dev/aaronhowser/mods/irregular_implements/KeyboardInputFunctions.java
@@ -2,24 +2,20 @@ package dev.aaronhowser.mods.irregular_implements;
 
 import dev.aaronhowser.mods.irregular_implements.registry.ModEffects;
 import dev.aaronhowser.mods.irregular_implements.util.ClientUtil;
-import net.minecraft.client.Options;
 import net.minecraft.client.player.KeyboardInput;
 import net.minecraft.client.player.LocalPlayer;
 
 public interface KeyboardInputFunctions {
 
-    //FIXME: Not working
-    default void checkInvert(Options options) {
+    default void checkInvert() {
         LocalPlayer player = ClientUtil.getLocalPlayer();
         if (player == null) return;
 
         KeyboardInput input = (KeyboardInput) this;
 
         if (player.hasEffect(ModEffects.getCOLLAPSE_IMBUE())) {
-            input.up = options.keyDown.isDown();
-            input.down = options.keyUp.isDown();
-            input.left = options.keyRight.isDown();
-            input.right = options.keyLeft.isDown();
+            input.leftImpulse = -input.leftImpulse;
+            input.forwardImpulse = -input.forwardImpulse;
         }
     }
 
--- src/main/java/dev/aaronhowser/mods/irregular_implements/mixin/KeyboardInputMixin.java
+++ src/main/java/dev/aaronhowser/mods/irregular_implements/mixin/KeyboardInputMixin.java
@@ -1,28 +1,20 @@
 package dev.aaronhowser.mods.irregular_implements.mixin;
 
 import dev.aaronhowser.mods.irregular_implements.KeyboardInputFunctions;
-import net.minecraft.client.Options;
-import net.minecraft.client.player.Input;
 import net.minecraft.client.player.KeyboardInput;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(KeyboardInput.class)
-public class KeyboardInputMixin extends Input implements KeyboardInputFunctions {
-
-    @Shadow
-    @Final
-    private Options options;
+public class KeyboardInputMixin implements KeyboardInputFunctions {
 
     @Inject(
             method = "tick",
             at = @At("TAIL")
     )
     private void irregular_implements$invertWhenCollapse(boolean isSneaking, float sneakingSpeedMultiplier, CallbackInfo ci) {
-        checkInvert(options);
+        checkInvert();
     }
 }
