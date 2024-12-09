d82974b9750f4e482be1f8516f5fee15f932de8b
Aaron Howser <aaronhowser1@gmail.com>
2024-12-08 22:05:38 -0600

    set direction

--- src/main/kotlin/dev/aaronhowser/mods/irregular_implements/block/plate/RedirectorPlateBlock.kt
+++ src/main/kotlin/dev/aaronhowser/mods/irregular_implements/block/plate/RedirectorPlateBlock.kt
@@ -2,13 +2,16 @@ package dev.aaronhowser.mods.irregular_implements.block.plate
 
 import net.minecraft.core.BlockPos
 import net.minecraft.core.Direction
+import net.minecraft.world.InteractionResult
 import net.minecraft.world.entity.Entity
+import net.minecraft.world.entity.player.Player
 import net.minecraft.world.item.context.BlockPlaceContext
 import net.minecraft.world.level.Level
 import net.minecraft.world.level.block.Block
 import net.minecraft.world.level.block.state.BlockState
 import net.minecraft.world.level.block.state.StateDefinition
 import net.minecraft.world.level.block.state.properties.DirectionProperty
+import net.minecraft.world.phys.BlockHitResult
 import thedarkcolour.kotlinforforge.neoforge.forge.vectorutil.v3d.toVec3
 
 class RedirectorPlateBlock : BasePlateBlock() {
@@ -35,6 +38,31 @@ class RedirectorPlateBlock : BasePlateBlock() {
         return defaultBlockState()
             .setValue(INPUT_DIRECTION, context.horizontalDirection.opposite)
             .setValue(OUTPUT_DIRECTION, context.horizontalDirection)
+    }
+
+    override fun useWithoutItem(
+        state: BlockState,
+        level: Level,
+        pos: BlockPos,
+        player: Player,
+        hitResult: BlockHitResult
+    ): InteractionResult {
+        if (level.isClientSide) return InteractionResult.PASS
+
+        val currentInput = state.getValue(INPUT_DIRECTION)
+        val currentOutput = state.getValue(OUTPUT_DIRECTION)
+
+        val centerPos = pos.toVec3().add(0.5, 0.0, 0.5)
+        val deltaVec = centerPos.vectorTo(hitResult.location)
+
+        val direction = Direction.getNearest(deltaVec.x, deltaVec.y, deltaVec.z)
+
+        if (direction == currentInput || direction == currentOutput) return InteractionResult.FAIL
+
+        val newState = state.setValue(OUTPUT_DIRECTION, direction)
+        level.setBlockAndUpdate(pos, newState)
+
+        return InteractionResult.SUCCESS
     }
 
     override fun entityInside(state: BlockState, level: Level, pos: BlockPos, entity: Entity) {
