f0c02d5995393ea13ca4176f4ea4dfa5dc832d34
Aaron Howser <aaronhowser1@gmail.com>
2024-12-11 16:52:36 -0600

    block placing works, doesnt do nbt yet

--- src/main/kotlin/dev/aaronhowser/mods/irregular_implements/item/BlockMoverItem.kt
+++ src/main/kotlin/dev/aaronhowser/mods/irregular_implements/item/BlockMoverItem.kt
@@ -3,6 +3,7 @@ package dev.aaronhowser.mods.irregular_implements.item
 import dev.aaronhowser.mods.irregular_implements.datagen.tag.ModBlockTagsProvider
 import dev.aaronhowser.mods.irregular_implements.item.component.BlockDataComponent
 import dev.aaronhowser.mods.irregular_implements.registry.ModDataComponents
+import net.minecraft.server.level.ServerLevel
 import net.minecraft.world.InteractionResult
 import net.minecraft.world.entity.ExperienceOrb
 import net.minecraft.world.entity.item.ItemEntity
@@ -10,9 +11,11 @@ import net.minecraft.world.entity.player.Player
 import net.minecraft.world.item.Item
 import net.minecraft.world.item.ItemStack
 import net.minecraft.world.item.context.UseOnContext
+import net.minecraft.world.level.block.Block
 import net.minecraft.world.level.block.Blocks
+import net.minecraft.world.phys.AABB
+import net.minecraft.world.phys.Vec3
 import net.neoforged.neoforge.common.NeoForge
-import net.neoforged.neoforge.common.util.BlockSnapshot
 import net.neoforged.neoforge.event.entity.EntityJoinLevelEvent
 import net.neoforged.neoforge.event.level.BlockEvent
 
@@ -74,26 +77,44 @@ class BlockMoverItem : Item(
         }
 
         fun tryPlaceBlock(player: Player, stack: ItemStack, context: UseOnContext): InteractionResult {
-            val level = context.level
+            val level = context.level as? ServerLevel ?: return InteractionResult.FAIL
             val clickedPos = context.clickedPos
             val clickedState = level.getBlockState(clickedPos)
 
+            val blockDataComponent = stack.get(ModDataComponents.BLOCK_DATA) ?: return InteractionResult.FAIL
+
             val posToPlaceBlock = if (clickedState.canBeReplaced()) clickedPos else clickedPos.relative(context.clickedFace)
             val stateAlreadyThere = level.getBlockState(posToPlaceBlock)
+            val stateToPlace = blockDataComponent.blockState
 
             if (!stateAlreadyThere.canBeReplaced()
                 || !level.mayInteract(player, clickedPos)
                 || !player.mayUseItemAt(clickedPos, context.clickedFace, stack)
-                || NeoForge.EVENT_BUS.post(
-                    BlockEvent.EntityPlaceEvent(
-                        BlockSnapshot.create(level.dimension(), level, posToPlaceBlock),
-                        clickedState,
-                        player
-                    )
-                ).isCanceled
+                || !stateToPlace.canSurvive(level, posToPlaceBlock)
+                || level.getEntities(null, AABB.unitCubeFromLowerCorner(Vec3.atLowerCornerOf(posToPlaceBlock))).isNotEmpty()
             ) return InteractionResult.FAIL
 
-            return InteractionResult.PASS
+            level.captureBlockSnapshots = true
+            level.setBlockAndUpdate(posToPlaceBlock, stateToPlace)
+            level.captureBlockSnapshots = false
+
+            val snapshots = level.capturedBlockSnapshots.toList()
+            level.capturedBlockSnapshots.clear()
+
+            val snapshot = snapshots.firstOrNull() ?: return InteractionResult.FAIL
+
+            if (BlockEvent.EntityPlaceEvent(snapshot, clickedState, player).isCanceled) {
+                level.restoringBlockSnapshots = true
+                snapshot.restore(snapshot.flags or Block.UPDATE_CLIENTS)
+                level.restoringBlockSnapshots = false
+                return InteractionResult.FAIL
+            }
+
+            //TODO: Nbt
+
+            stack.remove(ModDataComponents.BLOCK_DATA)
+
+            return InteractionResult.SUCCESS
         }
     }
 
