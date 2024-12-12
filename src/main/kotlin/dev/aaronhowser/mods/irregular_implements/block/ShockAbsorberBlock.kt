f9fa566b5902a4776b7295142e1d5f3b59b5dd55
Aaron Howser <aaronhowser1@gmail.com>
2024-12-11 21:13:55 -0600

    made it emit power when landed on

--- src/main/kotlin/dev/aaronhowser/mods/irregular_implements/block/ShockAbsorberBlock.kt
+++ src/main/kotlin/dev/aaronhowser/mods/irregular_implements/block/ShockAbsorberBlock.kt
@@ -1,15 +1,52 @@
 package dev.aaronhowser.mods.irregular_implements.block
 
 import net.minecraft.core.BlockPos
+import net.minecraft.core.Direction
+import net.minecraft.server.level.ServerLevel
+import net.minecraft.util.RandomSource
 import net.minecraft.world.entity.Entity
+import net.minecraft.world.level.BlockGetter
 import net.minecraft.world.level.Level
 import net.minecraft.world.level.block.Block
 import net.minecraft.world.level.block.state.BlockState
+import net.minecraft.world.level.block.state.StateDefinition
+import net.minecraft.world.level.block.state.properties.BlockStateProperties
+import net.minecraft.world.level.block.state.properties.BooleanProperty
 
 class ShockAbsorberBlock : Block(Properties.of()) {
 
+    companion object {
+        val ENABLED: BooleanProperty = BlockStateProperties.ENABLED
+    }
+
+    init {
+        registerDefaultState(
+            defaultBlockState()
+                .setValue(ENABLED, false)
+        )
+    }
+
+    override fun createBlockStateDefinition(builder: StateDefinition.Builder<Block, BlockState>) {
+        builder.add(ENABLED)
+    }
+
     override fun fallOn(level: Level, state: BlockState, pos: BlockPos, entity: Entity, fallDistance: Float) {
-        // Do nothing (including causing fall damage)
+        if (state.getValue(ENABLED)) return
+
+        level.scheduleTick(pos, this, 20)
+        level.setBlockAndUpdate(pos, state.setValue(ENABLED, true))
+    }
+
+    override fun tick(state: BlockState, level: ServerLevel, pos: BlockPos, random: RandomSource) {
+        level.setBlockAndUpdate(pos, state.setValue(ENABLED, false))
+    }
+
+    override fun isSignalSource(state: BlockState): Boolean {
+        return true
+    }
+
+    override fun getSignal(state: BlockState, level: BlockGetter, pos: BlockPos, direction: Direction): Int {
+        return if (state.getValue(ENABLED)) 15 else 0
     }
 
 }
