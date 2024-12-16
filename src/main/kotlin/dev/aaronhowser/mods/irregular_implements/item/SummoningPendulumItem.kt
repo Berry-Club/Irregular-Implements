86ad3ef9eba3bc2989851e9adea4e4db374dfc8d
Aaron Howser <aaronhowser1@gmail.com>
2024-12-16 05:34:12 -0600

    saving the entity LOOKS like it's working

--- src/main/kotlin/dev/aaronhowser/mods/irregular_implements/item/SummoningPendulumItem.kt
+++ src/main/kotlin/dev/aaronhowser/mods/irregular_implements/item/SummoningPendulumItem.kt
@@ -6,6 +6,7 @@ import dev.aaronhowser.mods.irregular_implements.registry.ModDataComponents
 import net.minecraft.nbt.CompoundTag
 import net.minecraft.world.InteractionHand
 import net.minecraft.world.InteractionResult
+import net.minecraft.world.entity.Entity
 import net.minecraft.world.entity.LivingEntity
 import net.minecraft.world.entity.player.Player
 import net.minecraft.world.item.Item
@@ -17,15 +18,18 @@ class SummoningPendulumItem : Item(
         .stacksTo(1)
 ) {
 
+    //FIXME: I should probably add a way to avoid NBT overflow or something
     override fun interactLivingEntity(
-        stack: ItemStack,
+        stackIgnoreIgnoreIgnore: ItemStack,     // Why the hell is this not the actual stack? Why do I have to make a usedStack?
         player: Player,
         interactionTarget: LivingEntity,
         usedHand: InteractionHand
     ): InteractionResult {
         if (interactionTarget.type.`is`(ModEntityTypeTagsProvider.SUMMONING_PENDULUM_BLACKLIST)) return InteractionResult.PASS
 
-        val currentEntityList = stack.get(ModDataComponents.ENTITY_LIST) ?: emptyList()
+        val usedStack = player.getItemInHand(usedHand)      // WHY IS THIS NECESSARY???
+
+        val currentEntityList = usedStack.get(ModDataComponents.ENTITY_LIST) ?: emptyList()
         if (currentEntityList.size >= ServerConfig.SUMMONING_PENDULUM_CAPACITY.get()) return InteractionResult.PASS
 
         val entityNbt = CompoundTag()
@@ -34,8 +38,11 @@ class SummoningPendulumItem : Item(
         val customData = CustomData.of(entityNbt)
         val newEntityList = currentEntityList + customData
 
-        //FIXME: WHY????????
-        stack.set(ModDataComponents.ENTITY_LIST, newEntityList)
+        if (usedStack.set(ModDataComponents.ENTITY_LIST, newEntityList) == null) {
+            return InteractionResult.FAIL
+        }
+
+        interactionTarget.remove(Entity.RemovalReason.DISCARDED)
 
         return InteractionResult.SUCCESS
     }
