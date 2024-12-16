b17492321dec58389246063d9a1c3298b364d198
Aaron Howser <aaronhowser1@gmail.com>
2024-12-16 05:57:23 -0600

    placing entities is apparently working too

--- src/main/kotlin/dev/aaronhowser/mods/irregular_implements/item/SummoningPendulumItem.kt
+++ src/main/kotlin/dev/aaronhowser/mods/irregular_implements/item/SummoningPendulumItem.kt
@@ -3,7 +3,9 @@ package dev.aaronhowser.mods.irregular_implements.item
 import dev.aaronhowser.mods.irregular_implements.config.ServerConfig
 import dev.aaronhowser.mods.irregular_implements.datagen.tag.ModEntityTypeTagsProvider
 import dev.aaronhowser.mods.irregular_implements.registry.ModDataComponents
+import net.minecraft.core.registries.Registries
 import net.minecraft.nbt.CompoundTag
+import net.minecraft.resources.ResourceLocation
 import net.minecraft.world.InteractionHand
 import net.minecraft.world.InteractionResult
 import net.minecraft.world.entity.Entity
@@ -12,6 +14,7 @@ import net.minecraft.world.entity.player.Player
 import net.minecraft.world.item.Item
 import net.minecraft.world.item.ItemStack
 import net.minecraft.world.item.component.CustomData
+import net.minecraft.world.item.context.UseOnContext
 
 class SummoningPendulumItem : Item(
     Properties()
@@ -43,6 +46,48 @@ class SummoningPendulumItem : Item(
         }
 
         interactionTarget.remove(Entity.RemovalReason.DISCARDED)
+
+        return InteractionResult.SUCCESS
+    }
+
+    override fun useOn(context: UseOnContext): InteractionResult {
+        val stack = context.itemInHand
+        val component = stack.get(ModDataComponents.ENTITY_LIST) ?: return InteractionResult.PASS
+
+        val entityData = component.lastOrNull() ?: return InteractionResult.PASS
+
+        val level = context.level
+
+        val clickedPos = context.clickedPos
+        val clickedFace = context.clickedFace
+        val clickedState = level.getBlockState(clickedPos)
+
+        val posToSpawn = if (clickedState.isSuffocating(level, clickedPos)) {
+            val relative = clickedPos.relative(clickedFace)
+            if (level.getBlockState(relative).isSuffocating(level, relative)) {
+                return InteractionResult.FAIL
+            }
+
+            relative
+        } else {
+            clickedPos
+        }
+
+        val entityTypeString = entityData.copyTag().getString("id")
+        val entityType = level.registryAccess()
+            .registryOrThrow(Registries.ENTITY_TYPE)
+            .get(ResourceLocation.parse(entityTypeString)) ?: return InteractionResult.FAIL
+
+        val entity = entityType.create(level) ?: return InteractionResult.FAIL
+
+        val newEntityList = component.dropLast(1)
+        if (stack.set(ModDataComponents.ENTITY_LIST, newEntityList) == null) {
+            return InteractionResult.FAIL
+        }
+
+        entityData.loadInto(entity)
+        entity.moveTo(posToSpawn.bottomCenter)
+        level.addFreshEntity(entity)
 
         return InteractionResult.SUCCESS
     }
