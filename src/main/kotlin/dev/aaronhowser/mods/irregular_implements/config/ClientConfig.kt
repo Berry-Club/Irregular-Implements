c46c41d39a35fbf66781fffde078f0f342a867b8
Aaron Howser <aaronhowser1@gmail.com>
2024-12-19 11:15:37 -0600

    trying to move color to config

--- src/main/kotlin/dev/aaronhowser/mods/irregular_implements/config/ClientConfig.kt
+++ src/main/kotlin/dev/aaronhowser/mods/irregular_implements/config/ClientConfig.kt
@@ -1,6 +1,7 @@
 package dev.aaronhowser.mods.irregular_implements.config
 
 import net.neoforged.neoforge.common.ModConfigSpec
+import net.neoforged.neoforge.common.Tags
 import org.apache.commons.lang3.tuple.Pair
 
 class ClientConfig(
@@ -15,6 +16,17 @@ class ClientConfig(
 
         lateinit var COLLAPSE_INVERTS_MOUSE: ModConfigSpec.BooleanValue
         lateinit var DIVINING_ROD_CHECK_RADIUS: ModConfigSpec.IntValue  //TODO: Make serverside? For lag reasons
+        lateinit var DIVINING_ROD_COLORS: ModConfigSpec.ConfigValue<Map<String, String>>
+
+        private val defaultRodColors = mapOf(
+            Tags.Blocks.ORES_COAL.location.toString() to "0x32141414",
+            Tags.Blocks.ORES_IRON.location.toString() to "0x32D3B49F",
+            Tags.Blocks.ORES_GOLD.location.toString() to "0x32F6E950",
+            Tags.Blocks.ORES_LAPIS.location.toString() to "0x32052D96",
+            Tags.Blocks.ORES_REDSTONE.location.toString() to "0x32D30101",
+            Tags.Blocks.ORES_EMERALD.location.toString() to "0x3200DC00",
+            Tags.Blocks.ORES_DIAMOND.location.toString() to "0x3255DDE5",
+        )
     }
 
     init {
@@ -31,6 +43,10 @@ class ClientConfig(
         DIVINING_ROD_CHECK_RADIUS = builder
             .comment("The radius around the player to check for blocks with the Divining Rod")
             .defineInRange("diviningRodCheckRadius", 5, 1, 100)
+
+        DIVINING_ROD_COLORS = builder
+            .comment("What colors should each ore tag be when using the Divining Rod?")
+            .define("diviningRodColors", defaultRodColors)
 
     }
 
