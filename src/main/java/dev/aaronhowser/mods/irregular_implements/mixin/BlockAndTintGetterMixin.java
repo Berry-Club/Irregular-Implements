bcc39c4f0b5063e808b410b91c227745d9674a7e
Aaron Howser <aaronhowser1@gmail.com>
2024-12-20 10:22:10 -0600

    working on changing brightness

--- src/main/java/dev/aaronhowser/mods/irregular_implements/mixin/BlockAndTintGetterMixin.java
+++ src/main/java/dev/aaronhowser/mods/irregular_implements/mixin/BlockAndTintGetterMixin.java
@@ -3,6 +3,7 @@ package dev.aaronhowser.mods.irregular_implements.mixin;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.BlockAndTintGetter;
 import net.minecraft.world.level.LightLayer;
+import net.minecraft.world.level.ServerLevelAccessor;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -17,7 +18,20 @@ public interface BlockAndTintGetterMixin {
             cancellable = true
     )
     default void irregular_implements$getBrightness(LightLayer lightType, BlockPos blockPos, CallbackInfoReturnable<Integer> cir) {
-        cir.setReturnValue(15);
+        if (this instanceof ServerLevelAccessor) {
+            cir.setReturnValue(0);
+        }
+    }
+
+    @Inject(
+            method = "getRawBrightness",
+            at = @At("HEAD"),
+            cancellable = true
+    )
+    default void irregular_implements$getRawBrightness(BlockPos blockPos, int amount, CallbackInfoReturnable<Integer> cir) {
+        if (this instanceof ServerLevelAccessor) {
+            cir.setReturnValue(0);
+        }
     }
 
 }
