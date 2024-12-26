68adb95594f4c945ada85ae3dbbe4d003f84d77b
Aaron Howser <aaronhowser1@gmail.com>
2024-12-26 17:22:58 -0600

    no occlusion

--- src/main/kotlin/dev/aaronhowser/mods/irregular_implements/block/DiaphanousBlock.kt
+++ src/main/kotlin/dev/aaronhowser/mods/irregular_implements/block/DiaphanousBlock.kt
@@ -2,12 +2,15 @@ package dev.aaronhowser.mods.irregular_implements.block
 
 import dev.aaronhowser.mods.irregular_implements.block.block_entity.DiaphanousBlockEntity
 import net.minecraft.core.BlockPos
+import net.minecraft.world.level.BlockGetter
 import net.minecraft.world.level.block.Block
 import net.minecraft.world.level.block.Blocks
 import net.minecraft.world.level.block.EntityBlock
 import net.minecraft.world.level.block.RenderShape
 import net.minecraft.world.level.block.entity.BlockEntity
 import net.minecraft.world.level.block.state.BlockState
+import net.minecraft.world.phys.shapes.Shapes
+import net.minecraft.world.phys.shapes.VoxelShape
 
 class DiaphanousBlock : Block(Properties.ofFullCopy(Blocks.STONE)), EntityBlock {
 
@@ -17,6 +20,10 @@ class DiaphanousBlock : Block(Properties.ofFullCopy(Blocks.STONE)), EntityBlock 
 
     override fun getRenderShape(state: BlockState): RenderShape {
         return RenderShape.ENTITYBLOCK_ANIMATED
+    }
+
+    override fun getOcclusionShape(state: BlockState, level: BlockGetter, pos: BlockPos): VoxelShape {
+        return Shapes.empty()
     }
 
 }
