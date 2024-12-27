52da837e982fa135c1bc312b02244549e35aab93
Aaron Howser <aaronhowser1@gmail.com>
2024-12-26 23:57:29 -0600

    BEWLR works kinda

--- src/main/kotlin/dev/aaronhowser/mods/irregular_implements/client/render/item/DiaphanousBEWLR.kt
+++ src/main/kotlin/dev/aaronhowser/mods/irregular_implements/client/render/item/DiaphanousBEWLR.kt
@@ -1,11 +1,13 @@
 package dev.aaronhowser.mods.irregular_implements.client.render.item
 
 import com.mojang.blaze3d.vertex.PoseStack
+import dev.aaronhowser.mods.irregular_implements.registry.ModDataComponents
 import net.minecraft.client.Minecraft
 import net.minecraft.client.renderer.BlockEntityWithoutLevelRenderer
 import net.minecraft.client.renderer.MultiBufferSource
 import net.minecraft.world.item.ItemDisplayContext
 import net.minecraft.world.item.ItemStack
+import net.minecraft.world.level.block.Blocks
 import net.neoforged.neoforge.client.extensions.common.IClientItemExtensions
 
 class DiaphanousBEWLR : BlockEntityWithoutLevelRenderer(
@@ -31,7 +33,26 @@ class DiaphanousBEWLR : BlockEntityWithoutLevelRenderer(
         packedLight: Int,
         packedOverlay: Int
     ) {
+        val block = stack.get(ModDataComponents.BLOCK) ?: Blocks.STONE
 
+        val itemRenderer = Minecraft.getInstance().itemRenderer
+
+        poseStack.pushPose()
+
+        poseStack.translate(0.5, 0.5, 0.5)
+
+        itemRenderer.renderStatic(
+            block.asItem().defaultInstance,
+            displayContext,
+            packedLight,
+            packedOverlay,
+            poseStack,
+            buffer,
+            null,
+            0,
+        )
+
+        poseStack.popPose()
     }
 
 }
