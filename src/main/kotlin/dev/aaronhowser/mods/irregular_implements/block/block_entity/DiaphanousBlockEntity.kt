8312a9f9a1a0e6cd175b3af12546293c05a694b4
Aaron Howser <aaronhowser1@gmail.com>
2024-12-26 21:33:45 -0600

    save nbt

--- src/main/kotlin/dev/aaronhowser/mods/irregular_implements/block/block_entity/DiaphanousBlockEntity.kt
+++ src/main/kotlin/dev/aaronhowser/mods/irregular_implements/block/block_entity/DiaphanousBlockEntity.kt
@@ -1,7 +1,12 @@
 package dev.aaronhowser.mods.irregular_implements.block.block_entity
 
+import dev.aaronhowser.mods.irregular_implements.IrregularImplements
 import dev.aaronhowser.mods.irregular_implements.registry.ModBlockEntities
 import net.minecraft.core.BlockPos
+import net.minecraft.core.HolderLookup
+import net.minecraft.core.registries.BuiltInRegistries
+import net.minecraft.nbt.CompoundTag
+import net.minecraft.resources.ResourceLocation
 import net.minecraft.world.level.block.Block
 import net.minecraft.world.level.block.Blocks
 import net.minecraft.world.level.block.entity.BlockEntity
@@ -12,7 +17,38 @@ class DiaphanousBlockEntity(
     pBlockState: BlockState
 ) : BlockEntity(ModBlockEntities.DIAPHANOUS_BLOCK.get(), pPos, pBlockState) {
 
+    companion object {
+        const val BLOCK_NBT = "block"
+        const val IS_INVERTED_NBT = "is_inverted"
+    }
+
     var renderedBlock: Block = Blocks.STONE
     var isInverted: Boolean = false
+
+    override fun saveAdditional(tag: CompoundTag, registries: HolderLookup.Provider) {
+        super.saveAdditional(tag, registries)
+
+        tag.putString(BLOCK_NBT, BuiltInRegistries.BLOCK.getKey(this.renderedBlock).toString())
+        tag.putBoolean(IS_INVERTED_NBT, this.isInverted)
+    }
+
+    override fun loadAdditional(tag: CompoundTag, registries: HolderLookup.Provider) {
+        super.loadAdditional(tag, registries)
+
+        this.isInverted = tag.getBoolean(IS_INVERTED_NBT)
+
+        val blockString = tag.getString(BLOCK_NBT)
+        val blockRl = ResourceLocation.tryParse(blockString)
+
+        if (blockRl != null) {
+            val block = BuiltInRegistries.BLOCK.getOptional(blockRl)
+
+            if (block.isPresent) {
+                this.renderedBlock = block.get()
+            } else {
+                IrregularImplements.LOGGER.error("A Diaphanous Block at ${blockPos.x} ${blockPos.y} ${blockPos.z} tried to load a block that doesn't exist: $blockString")
+            }
+        }
+    }
 
 }
