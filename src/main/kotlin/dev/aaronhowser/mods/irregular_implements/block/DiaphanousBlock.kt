72c0fdecfc15200241bd2a20fb71415e9ff1ee5b
Aaron Howser <aaronhowser1@gmail.com>
2024-12-26 18:19:09 -0600

    Revert "dont model the actual block"
    This reverts commit 36cb944db3cde45e6324782b1309ca1744c806da.

--- src/main/kotlin/dev/aaronhowser/mods/irregular_implements/block/DiaphanousBlock.kt
+++ src/main/kotlin/dev/aaronhowser/mods/irregular_implements/block/DiaphanousBlock.kt
@@ -5,7 +5,6 @@ import net.minecraft.core.BlockPos
 import net.minecraft.world.level.block.Block
 import net.minecraft.world.level.block.Blocks
 import net.minecraft.world.level.block.EntityBlock
-import net.minecraft.world.level.block.RenderShape
 import net.minecraft.world.level.block.entity.BlockEntity
 import net.minecraft.world.level.block.state.BlockState
 
@@ -13,10 +12,6 @@ class DiaphanousBlock : Block(Properties.ofFullCopy(Blocks.STONE)), EntityBlock 
 
     override fun newBlockEntity(pos: BlockPos, state: BlockState): BlockEntity {
         return DiaphanousBlockEntity(pos, state)
-    }
-
-    override fun getRenderShape(state: BlockState): RenderShape {
-        return RenderShape.ENTITYBLOCK_ANIMATED
     }
 
 }
