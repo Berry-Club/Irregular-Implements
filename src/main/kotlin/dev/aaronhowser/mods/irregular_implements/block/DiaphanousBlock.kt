11912c285fbcc33610c6d70dfdacc40bbf2337a8
Aaron Howser <aaronhowser1@gmail.com>
2024-12-26 20:32:41 -0600

    Revert "Revert "click block to set it""
    This reverts commit 46648a9d8de046b5a32500e7dae32dc76c90abad.

--- src/main/kotlin/dev/aaronhowser/mods/irregular_implements/block/DiaphanousBlock.kt
+++ src/main/kotlin/dev/aaronhowser/mods/irregular_implements/block/DiaphanousBlock.kt
@@ -2,13 +2,20 @@ package dev.aaronhowser.mods.irregular_implements.block
 
 import dev.aaronhowser.mods.irregular_implements.block.block_entity.DiaphanousBlockEntity
 import net.minecraft.core.BlockPos
+import net.minecraft.world.InteractionHand
+import net.minecraft.world.ItemInteractionResult
+import net.minecraft.world.entity.player.Player
+import net.minecraft.world.item.BlockItem
+import net.minecraft.world.item.ItemStack
 import net.minecraft.world.level.BlockGetter
+import net.minecraft.world.level.Level
 import net.minecraft.world.level.block.Block
 import net.minecraft.world.level.block.Blocks
 import net.minecraft.world.level.block.EntityBlock
 import net.minecraft.world.level.block.RenderShape
 import net.minecraft.world.level.block.entity.BlockEntity
 import net.minecraft.world.level.block.state.BlockState
+import net.minecraft.world.phys.BlockHitResult
 import net.minecraft.world.phys.shapes.Shapes
 import net.minecraft.world.phys.shapes.VoxelShape
 
@@ -24,6 +31,29 @@ class DiaphanousBlock : Block(Properties.ofFullCopy(Blocks.STONE)), EntityBlock 
 
     override fun getOcclusionShape(state: BlockState, level: BlockGetter, pos: BlockPos): VoxelShape {
         return Shapes.empty()
+    }
+
+    override fun useItemOn(
+        stack: ItemStack,
+        state: BlockState,
+        level: Level,
+        pos: BlockPos,
+        player: Player,
+        hand: InteractionHand,
+        hitResult: BlockHitResult
+    ): ItemInteractionResult {
+        val item = stack.item
+        if (item is BlockItem) {
+            val block = item.block
+            val blockEntity = level.getBlockEntity(pos) as? DiaphanousBlockEntity
+
+            if (blockEntity != null) {
+                blockEntity.renderedBlock = block
+                return ItemInteractionResult.SUCCESS
+            }
+        }
+
+        return ItemInteractionResult.PASS_TO_DEFAULT_BLOCK_INTERACTION
     }
 
 }
