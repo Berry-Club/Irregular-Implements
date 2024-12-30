f7a899fdc892c8da0713c28b9b0be9a5ebecdb68
Aaron Howser <aaronhowser1@gmail.com>
2024-12-29 18:09:56 -0600

    :(

--- src/main/kotlin/dev/aaronhowser/mods/irregular_implements/client/render/block/SpectreEnergyInjectorBlockEntityRenderer.kt
+++ src/main/kotlin/dev/aaronhowser/mods/irregular_implements/client/render/block/SpectreEnergyInjectorBlockEntityRenderer.kt
@@ -1,13 +1,18 @@
 package dev.aaronhowser.mods.irregular_implements.client.render.block
 
+import com.mojang.blaze3d.platform.GlStateManager
+import com.mojang.blaze3d.platform.GlStateManager.SourceFactor
 import com.mojang.blaze3d.systems.RenderSystem
-import com.mojang.blaze3d.vertex.*
+import com.mojang.blaze3d.vertex.DefaultVertexFormat
+import com.mojang.blaze3d.vertex.PoseStack
+import com.mojang.blaze3d.vertex.Tesselator
+import com.mojang.blaze3d.vertex.VertexFormat
 import com.mojang.math.Axis
 import dev.aaronhowser.mods.irregular_implements.block.block_entity.SpectreEnergyInjectorBlockEntity
 import net.minecraft.client.renderer.MultiBufferSource
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderer
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider
-import net.minecraft.util.Mth
+import kotlin.math.sin
 import kotlin.random.Random
 
 class SpectreEnergyInjectorBlockEntityRenderer(
@@ -22,32 +27,35 @@ class SpectreEnergyInjectorBlockEntityRenderer(
         packedLight: Int,
         packedOverlay: Int
     ) {
+        val level = blockEntity.level ?: return
+        val animationCounter = level.gameTime + partialTick
+
         val tesselator = Tesselator.getInstance()
 
-        val animationCounter = ((blockEntity.level?.gameTime ?: 0) + partialTick) / 200f
-
         poseStack.pushPose()
-        poseStack.translate(0.5, 0.6, 0.5)
+
+        val random = Random(432L)
 
         RenderSystem.enableBlend()
         RenderSystem.defaultBlendFunc()
+        RenderSystem.blendFunc(SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA)
         RenderSystem.disableCull()
         RenderSystem.depthMask(false)
 
         for (i in 0 until 25) {
             poseStack.pushPose()
 
-            poseStack.mulPose(Axis.XP.rotationDegrees(Random.nextFloat() * 360f))
-            poseStack.mulPose(Axis.YP.rotationDegrees(Random.nextFloat() * 360.0f))
-            poseStack.mulPose(Axis.ZP.rotationDegrees(Random.nextFloat() * 360.0f))
-            poseStack.mulPose(Axis.XP.rotationDegrees(Random.nextFloat() * 360.0f))
-            poseStack.mulPose(Axis.YP.rotationDegrees(Random.nextFloat() * 360.0f))
-            poseStack.mulPose(Axis.ZP.rotationDegrees(Random.nextFloat() * 360.0f + animationCounter * 90.0f))
+            poseStack.mulPose(Axis.XP.rotationDegrees(random.nextFloat() * 360f))
+            poseStack.mulPose(Axis.YP.rotationDegrees(random.nextFloat() * 360.0f))
+            poseStack.mulPose(Axis.ZP.rotationDegrees(random.nextFloat() * 360.0f))
+            poseStack.mulPose(Axis.XP.rotationDegrees(random.nextFloat() * 360.0f))
+            poseStack.mulPose(Axis.YP.rotationDegrees(random.nextFloat() * 360.0f))
+            poseStack.mulPose(Axis.ZP.rotationDegrees(random.nextFloat() * 360.0f + animationCounter * 90.0f))
 
-            val size1 = Random.nextFloat() * 0.2f + 0.1f
-            val size2 = Random.nextFloat() * 0.1f
+            val size1 = random.nextFloat() * 0.2f + 0.1f
+            val size2 = random.nextFloat() * 0.1f
 
-            val red = ((Mth.sin(animationCounter) / 2 + 0.5).toFloat() * 80 + 40).toInt()
+            val red = ((sin(animationCounter) / 2 + 0.5).toFloat() * 80 + 40).toInt()
             val green = 230
             val blue = 226
             val alpha = 255
