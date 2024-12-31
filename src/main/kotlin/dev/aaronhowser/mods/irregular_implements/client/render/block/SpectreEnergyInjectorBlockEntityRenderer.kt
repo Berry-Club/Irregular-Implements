c18d1dcccf6437775238649a6b0eda969470ec79
Aaron Howser <aaronhowser1@gmail.com>
2024-12-31 08:23:54 -0600

    an attempt at a one to one remake

--- src/main/kotlin/dev/aaronhowser/mods/irregular_implements/client/render/block/SpectreEnergyInjectorBlockEntityRenderer.kt
+++ src/main/kotlin/dev/aaronhowser/mods/irregular_implements/client/render/block/SpectreEnergyInjectorBlockEntityRenderer.kt
@@ -1,13 +1,12 @@
 package dev.aaronhowser.mods.irregular_implements.client.render.block
 
-import com.mojang.blaze3d.platform.GlStateManager
+import com.mojang.blaze3d.platform.GlStateManager.DestFactor
 import com.mojang.blaze3d.platform.GlStateManager.SourceFactor
 import com.mojang.blaze3d.systems.RenderSystem
 import com.mojang.blaze3d.vertex.DefaultVertexFormat
 import com.mojang.blaze3d.vertex.PoseStack
 import com.mojang.blaze3d.vertex.Tesselator
 import com.mojang.blaze3d.vertex.VertexFormat
-import com.mojang.math.Axis
 import dev.aaronhowser.mods.irregular_implements.block.block_entity.SpectreEnergyInjectorBlockEntity
 import net.minecraft.client.renderer.MultiBufferSource
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderer
@@ -28,54 +27,59 @@ class SpectreEnergyInjectorBlockEntityRenderer(
         packedOverlay: Int
     ) {
         val level = blockEntity.level ?: return
-        val animationCounter = level.gameTime + partialTick
+        val f = (level.gameTime + partialTick) / 200f
 
         val tesselator = Tesselator.getInstance()
         val bufferBuilder = tesselator.begin(VertexFormat.Mode.TRIANGLES, DefaultVertexFormat.POSITION_COLOR)
 
         poseStack.pushPose()
+        poseStack.translate(0.5, 0.6, 0.5)
+
+        val f1 = 0f
 
         val random = Random(432L)
 
+//        GlStateManager.disableTexture2D();
+//        GlStateManager.shadeModel(7425);
         RenderSystem.enableBlend()
-        RenderSystem.defaultBlendFunc()
-        RenderSystem.blendFunc(SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA)
+        RenderSystem.blendFunc(SourceFactor.SRC_ALPHA, DestFactor.ONE_MINUS_SRC_ALPHA)
+//        GlStateManager.disableAlpha();
         RenderSystem.disableCull()
         RenderSystem.depthMask(false)
 
         for (i in 0 until 25) {
-            poseStack.pushPose()
 
-            poseStack.mulPose(Axis.XP.rotationDegrees(random.nextFloat() * 360f))
-            poseStack.mulPose(Axis.YP.rotationDegrees(random.nextFloat() * 360.0f))
-            poseStack.mulPose(Axis.ZP.rotationDegrees(random.nextFloat() * 360.0f))
-            poseStack.mulPose(Axis.XP.rotationDegrees(random.nextFloat() * 360.0f))
-            poseStack.mulPose(Axis.YP.rotationDegrees(random.nextFloat() * 360.0f))
-            poseStack.mulPose(Axis.ZP.rotationDegrees(random.nextFloat() * 360.0f + animationCounter * 90.0f))
+//            GlStateManager.rotate(random.nextFloat() * 360.0F, 1.0F, 0.0F, 0.0F);
+//            GlStateManager.rotate(random.nextFloat() * 360.0F, 0.0F, 1.0F, 0.0F);
+//            GlStateManager.rotate(random.nextFloat() * 360.0F, 0.0F, 0.0F, 1.0F);
+//            GlStateManager.rotate(random.nextFloat() * 360.0F, 1.0F, 0.0F, 0.0F);
+//            GlStateManager.rotate(random.nextFloat() * 360.0F, 0.0F, 1.0F, 0.0F);
+//            GlStateManager.rotate(random.nextFloat() * 360.0F + f * 90.0F, 0.0F, 0.0F, 1.0F);
 
-            val size1 = random.nextFloat() * 0.2f + 0.1f
-            val size2 = random.nextFloat() * 0.1f
+            val f2 = random.nextFloat() * 0.2f + 0.1f + f1 * 10.0f
+            val f3 = random.nextFloat() * 0.1f + f1 * 2.0f
 
-            val red = ((sin(animationCounter) / 2 + 0.5).toFloat() * 80 + 40).toInt()
-            val green = 230
-            val blue = 226
-            val alpha = 255
+            val red = ((sin(f.toDouble()) / 2 + 0.5) * 80).toInt() + 40
 
-            bufferBuilder.addVertex(poseStack.last().pose(), 0.0f, 0.0f, 0.0f).setColor(0, 0, 0, 255)
+            bufferBuilder.addVertex(0f, 0f, 0f).setColor(0, 0, 0, (255f * (1f - f1)).toInt())
+            bufferBuilder.addVertex(-0.866f * f3, f2, (-0.5f * f3)).setColor(red, 230, 226, 0)
+            bufferBuilder.addVertex(0.866f * f3, f2, (-0.5f * f3)).setColor(red, 230, 226, 0)
+            bufferBuilder.addVertex(0f, f2, f3).setColor(red, 230, 226, 0)
+            bufferBuilder.addVertex(-0.866f * f3, f2, (-0.5f * f3)).setColor(red, 230, 226, 0)
 
-            bufferBuilder.addVertex(poseStack.last().pose(), -0.866f * size2, size1, -0.5f * size2).setColor(red, green, blue, alpha)
-            bufferBuilder.addVertex(poseStack.last().pose(), 0.866f * size2, size1, -0.5f * size2).setColor(red, green, blue, alpha)
-            bufferBuilder.addVertex(poseStack.last().pose(), 0.0f, size1, size2).setColor(red, green, blue, alpha)
-            bufferBuilder.addVertex(poseStack.last().pose(), -0.866f * size2, size1, -0.5f * size2).setColor(red, green, blue, alpha)
-
-            poseStack.popPose()
+//            tessellator.draw();
         }
 
-        RenderSystem.depthMask(true)
-        RenderSystem.enableCull()
-        RenderSystem.disableBlend()
-
         poseStack.popPose()
+        RenderSystem.depthMask(true)
+        RenderSystem.disableCull()
+        RenderSystem.disableBlend()
+//        GlStateManager.shadeModel(7424);
+//        GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
+//        GlStateManager.enableTexture2D();
+//        GlStateManager.enableAlpha();
+//        RenderHelper.enableStandardItemLighting();
+
     }
 
     override fun shouldRenderOffScreen(blockEntity: SpectreEnergyInjectorBlockEntity): Boolean {
