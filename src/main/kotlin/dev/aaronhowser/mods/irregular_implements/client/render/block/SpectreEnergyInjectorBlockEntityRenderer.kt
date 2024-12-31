338a19c8fd941b79d42206d1099a0fc1d4eecd84
Aaron Howser <aaronhowser1@gmail.com>
2024-12-31 08:49:34 -0600

    one singular triangle

--- src/main/kotlin/dev/aaronhowser/mods/irregular_implements/client/render/block/SpectreEnergyInjectorBlockEntityRenderer.kt
+++ src/main/kotlin/dev/aaronhowser/mods/irregular_implements/client/render/block/SpectreEnergyInjectorBlockEntityRenderer.kt
@@ -20,53 +20,13 @@ class SpectreEnergyInjectorBlockEntityRenderer(
         packedOverlay: Int
     ) {
         poseStack.pushPose()
-        poseStack.translate(0.5, 0.6, 0.5)
 
-        val buffer = bufferSource.getBuffer(RenderType.translucent())
+        val buffer = bufferSource.getBuffer(RenderType.endPortal())
 
-        // Define vertices for a single triangle
-        val size = 0.5f
-        val colorRed = 255
-        val colorGreen = 50
-        val colorBlue = 200
-        val alpha = 128 // Semi-transparent
-
-        buffer.addVertex(poseStack.last().pose(), 0f, size, 0f)
-            .setColor(colorRed, colorGreen, colorBlue, alpha)
-            .setUv(0f, 0f)
-            .setUv2(0xFFFFFF, 0xFFFFFF)
-            .setNormal(0f, 1f, 0f)
-
-        buffer.addVertex(poseStack.last().pose(), -size, -size, size)
-            .setColor(colorRed, colorGreen, colorBlue, alpha)
-            .setUv(0f, 0f)
-            .setUv2(0xFFFFFF, 0xFFFFFF)
-            .setNormal(0f, 1f, 0f)
-
-        buffer.addVertex(poseStack.last().pose(), size, -size, size)
-            .setColor(colorRed, colorGreen, colorBlue, alpha)
-            .setUv(0f, 0f)
-            .setUv2(0xFFFFFF, 0xFFFFFF)
-            .setNormal(0f, 1f, 0f)
-
-        // Second side of the triangle for double-sided rendering
-        buffer.addVertex(poseStack.last().pose(), 0f, size, 0f)
-            .setColor(colorRed, colorGreen, colorBlue, alpha)
-            .setUv(0f, 0f)
-            .setUv2(0xFFFFFF, 0xFFFFFF)
-            .setNormal(0f, 1f, 0f)
-
-        buffer.addVertex(poseStack.last().pose(), size, -size, size)
-            .setColor(colorRed, colorGreen, colorBlue, alpha)
-            .setUv(0f, 0f)
-            .setUv2(0xFFFFFF, 0xFFFFFF)
-            .setNormal(0f, 1f, 0f)
-
-        buffer.addVertex(poseStack.last().pose(), -size, -size, size)
-            .setColor(colorRed, colorGreen, colorBlue, alpha)
-            .setUv(0f, 0f)
-            .setUv2(0xFFFFFF, 0xFFFFFF)
-            .setNormal(0f, 1f, 0f)
+        buffer.addVertex(poseStack.last(), 0f, 0.25f, 0f)
+        buffer.addVertex(poseStack.last(), 0f, 0.25f, 0f)
+        buffer.addVertex(poseStack.last(), 1f, 0.25f, 1f)
+        buffer.addVertex(poseStack.last(), 1f, 0.25f, 0f)
 
         poseStack.popPose()
     }
