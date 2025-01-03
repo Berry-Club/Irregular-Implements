b12a0abc1039db13adf20b5cd601a8cacb332b17
Aaron Howser <aaronhowser1@gmail.com>
2025-01-03 05:53:11 -0600

    allow you to hit it too

--- src/main/kotlin/dev/aaronhowser/mods/irregular_implements/entity/SpectreIlluminatorEntity.kt
+++ src/main/kotlin/dev/aaronhowser/mods/irregular_implements/entity/SpectreIlluminatorEntity.kt
@@ -12,6 +12,7 @@ import net.minecraft.network.syncher.EntityDataSerializers
 import net.minecraft.network.syncher.SynchedEntityData
 import net.minecraft.world.InteractionHand
 import net.minecraft.world.InteractionResult
+import net.minecraft.world.damagesource.DamageSource
 import net.minecraft.world.entity.Entity
 import net.minecraft.world.entity.EntityType
 import net.minecraft.world.entity.player.Player
@@ -83,18 +84,31 @@ class SpectreIlluminatorEntity(
         return true
     }
 
-    override fun getPickResult(): ItemStack? {
-        return ModItems.SPECTRE_ILLUMINATOR.toStack()
+    override fun hurt(source: DamageSource, amount: Float): Boolean {
+        val entity = source.entity ?: return false
+        removedByEntity(entity)
+        return false
     }
 
     override fun interact(player: Player, hand: InteractionHand): InteractionResult {
-        if (player.hasInfiniteMaterials()) {
-            this.discard()
+        removedByEntity(player)
+        return InteractionResult.SUCCESS
+    }
+
+    private fun removedByEntity(entity: Entity) {
+        if (entity is Player) {
+            if (entity.hasInfiniteMaterials()) {
+                this.discard()
+            } else {
+                this.kill()
+            }
         } else {
             this.kill()
         }
+    }
 
-        return InteractionResult.SUCCESS
+    override fun getPickResult(): ItemStack? {
+        return ModItems.SPECTRE_ILLUMINATOR.toStack()
     }
 
     override fun onAddedToLevel() {
