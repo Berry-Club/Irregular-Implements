aef2110eef5629f4fc3985a0083a3c7682914137
Aaron Howser <aaronhowser1@gmail.com>
2025-01-08 17:26:28 -0600

    split into two methods

--- src/main/kotlin/dev/aaronhowser/mods/irregular_implements/block/BlockBreakerBlock.kt
+++ src/main/kotlin/dev/aaronhowser/mods/irregular_implements/block/BlockBreakerBlock.kt
@@ -7,12 +7,12 @@ import dev.aaronhowser.mods.irregular_implements.util.OtherUtil
 import net.minecraft.core.BlockPos
 import net.minecraft.core.Direction
 import net.minecraft.world.InteractionHand
+import net.minecraft.world.InteractionResult
 import net.minecraft.world.ItemInteractionResult
 import net.minecraft.world.entity.player.Player
 import net.minecraft.world.item.ItemStack
 import net.minecraft.world.item.context.BlockPlaceContext
 import net.minecraft.world.level.Level
-import net.minecraft.world.level.LevelAccessor
 import net.minecraft.world.level.block.BaseEntityBlock
 import net.minecraft.world.level.block.Block
 import net.minecraft.world.level.block.Blocks
@@ -88,24 +88,30 @@ class BlockBreakerBlock : Block(
         if (level.isClientSide) return ItemInteractionResult.PASS_TO_DEFAULT_BLOCK_INTERACTION
         val blockEntity = level.getBlockEntity(pos) as? BlockBreakerBlockEntity ?: return ItemInteractionResult.FAIL
 
-        if (stack.`is`(ModItems.DIAMOND_BREAKER) && !state.getValue(IS_UPGRADED)) {
-            blockEntity.upgrade(stack.copy())
-            val newState = state.setValue(IS_UPGRADED, true)
-            level.setBlockAndUpdate(pos, newState)
+        if (!stack.`is`(ModItems.DIAMOND_BREAKER) || state.getValue(IS_UPGRADED)) return ItemInteractionResult.PASS_TO_DEFAULT_BLOCK_INTERACTION
 
-            stack.shrink(1)
-            return ItemInteractionResult.SUCCESS
-        }
+        blockEntity.upgrade(stack.copy())
 
-        if (state.getValue(IS_UPGRADED) && player.isSecondaryUseActive && stack.isEmpty) {
-            blockEntity.downgrade(player)
-            val newState = state.setValue(IS_UPGRADED, false)
-            level.setBlockAndUpdate(pos, newState)
+        val newState = state.setValue(IS_UPGRADED, true)
+        level.setBlockAndUpdate(pos, newState)
 
-            return ItemInteractionResult.SUCCESS
-        }
+        stack.shrink(1)
+        return ItemInteractionResult.SUCCESS
+    }
 
-        return ItemInteractionResult.PASS_TO_DEFAULT_BLOCK_INTERACTION
+    override fun useWithoutItem(state: BlockState, level: Level, pos: BlockPos, player: Player, hitResult: BlockHitResult): InteractionResult {
+        if (level.isClientSide
+            || !player.isSecondaryUseActive
+            || !state.getValue(IS_UPGRADED)
+        ) return InteractionResult.PASS
+
+        val blockEntity = level.getBlockEntity(pos) as? BlockBreakerBlockEntity ?: return InteractionResult.FAIL
+        blockEntity.downgrade(player)
+
+        val newState = state.setValue(IS_UPGRADED, false)
+        level.setBlockAndUpdate(pos, newState)
+
+        return InteractionResult.SUCCESS
     }
 
     override fun onRemove(state: BlockState, level: Level, pos: BlockPos, newState: BlockState, movedByPiston: Boolean) {
