925cc4a3d32159a9352ccb24ae8e47a0854d0b08
Aaron Howser <aaronhowser1@gmail.com>
2025-01-08 19:31:48 -0600

    better dispense behavior

--- src/main/kotlin/dev/aaronhowser/mods/irregular_implements/block/block_entity/IronDropperBlockEntity.kt
+++ src/main/kotlin/dev/aaronhowser/mods/irregular_implements/block/block_entity/IronDropperBlockEntity.kt
@@ -1,11 +1,19 @@
 package dev.aaronhowser.mods.irregular_implements.block.block_entity
 
 import dev.aaronhowser.mods.irregular_implements.registry.ModBlockEntities
+import dev.aaronhowser.mods.irregular_implements.registry.ModBlocks
 import net.minecraft.core.BlockPos
+import net.minecraft.network.chat.Component
 import net.minecraft.world.level.block.entity.DispenserBlockEntity
 import net.minecraft.world.level.block.state.BlockState
 
 class IronDropperBlockEntity(
     pPos: BlockPos,
     pBlockState: BlockState
-) : DispenserBlockEntity(ModBlockEntities.IRON_DROPPER.get(), pPos, pBlockState)
+) : DispenserBlockEntity(ModBlockEntities.IRON_DROPPER.get(), pPos, pBlockState) {
+
+    override fun getDefaultName(): Component {
+        return ModBlocks.IRON_DROPPER.get().name
+    }
+
+}
