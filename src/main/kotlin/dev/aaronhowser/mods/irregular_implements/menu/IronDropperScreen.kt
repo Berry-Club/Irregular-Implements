09dafdfc9579898611240ef566c1ef7e819cba7a
Aaron Howser <aaronhowser1@gmail.com>
2025-01-08 23:14:28 -0600

    IronDropperScreen

--- src/main/kotlin/dev/aaronhowser/mods/irregular_implements/menu/IronDropperScreen.kt
+++ src/main/kotlin/dev/aaronhowser/mods/irregular_implements/menu/IronDropperScreen.kt
@@ -3,6 +3,7 @@ package dev.aaronhowser.mods.irregular_implements.menu
 import net.minecraft.client.gui.GuiGraphics
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen
 import net.minecraft.network.chat.Component
+import net.minecraft.resources.ResourceLocation
 import net.minecraft.world.entity.player.Inventory
 
 class IronDropperScreen(
@@ -11,8 +12,24 @@ class IronDropperScreen(
     title: Component
 ) : AbstractContainerScreen<IronDropperMenu>(menu, playerInventory, title) {
 
+    companion object {
+        private val texture = ResourceLocation.withDefaultNamespace("textures/gui/container/dispenser.png")
+    }
+
+    override fun init() {
+        super.init()
+        this.titleLabelX = (this.imageWidth - font.width(this.title)) / 2
+    }
+
+    override fun render(guiGraphics: GuiGraphics, mouseX: Int, mouseY: Int, partialTick: Float) {
+        super.render(guiGraphics, mouseX, mouseY, partialTick)
+        this.renderTooltip(guiGraphics, mouseX, mouseY)
+    }
+
     override fun renderBg(guiGraphics: GuiGraphics, partialTick: Float, mouseX: Int, mouseY: Int) {
-        println("Rendered Iron Dropper Screen")
+        val i = (this.width - this.imageWidth) / 2
+        val j = (this.height - this.imageHeight) / 2
+        guiGraphics.blit(texture, i, j, 0, 0, this.imageWidth, this.imageHeight)
     }
 
 }
