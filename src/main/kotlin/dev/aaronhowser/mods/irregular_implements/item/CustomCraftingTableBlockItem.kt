69e0876401eab1225d22d0cd173e4f81558bce19
Aaron Howser <aaronhowser1@gmail.com>
2025-01-14 00:50:25 -0600

    placing

--- src/main/kotlin/dev/aaronhowser/mods/irregular_implements/item/CustomCraftingTableBlockItem.kt
+++ src/main/kotlin/dev/aaronhowser/mods/irregular_implements/item/CustomCraftingTableBlockItem.kt
@@ -1,10 +1,13 @@
 package dev.aaronhowser.mods.irregular_implements.item
 
+import dev.aaronhowser.mods.irregular_implements.block.block_entity.CustomCraftingTableBlockEntity
 import dev.aaronhowser.mods.irregular_implements.registry.ModBlocks
 import dev.aaronhowser.mods.irregular_implements.registry.ModDataComponents
 import dev.aaronhowser.mods.irregular_implements.registry.ModItems
+import net.minecraft.world.InteractionResult
 import net.minecraft.world.item.ItemNameBlockItem
 import net.minecraft.world.item.ItemStack
+import net.minecraft.world.item.context.BlockPlaceContext
 import net.minecraft.world.level.block.Block
 
 class CustomCraftingTableBlockItem : ItemNameBlockItem(
@@ -19,6 +22,30 @@ class CustomCraftingTableBlockItem : ItemNameBlockItem(
 
             return stack
         }
+    }
+
+    override fun place(context: BlockPlaceContext): InteractionResult {
+        val result = super.place(context)
+
+        if (!result.consumesAction()) {
+            return result
+        }
+
+        val blockToRender = context.itemInHand.get(ModDataComponents.BLOCK)
+
+        if (blockToRender != null) {
+            val stateToRender = getPlacementState(context)
+
+            if (stateToRender != null) {
+                val blockEntity = context.level.getBlockEntity(context.clickedPos)
+
+                if (blockEntity is CustomCraftingTableBlockEntity) {
+                    blockEntity.renderedBlockState = stateToRender
+                }
+            }
+        }
+
+        return result
     }
 
 }
