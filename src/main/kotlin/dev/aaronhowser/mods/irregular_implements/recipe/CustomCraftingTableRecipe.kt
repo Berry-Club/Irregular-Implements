4d2637957c1d6f0335e43bf72a29f6f81f8ad433
Aaron Howser <aaronhowser1@gmail.com>
2025-01-14 02:07:02 -0600

    TODO

--- src/main/kotlin/dev/aaronhowser/mods/irregular_implements/recipe/CustomCraftingTableRecipe.kt
+++ src/main/kotlin/dev/aaronhowser/mods/irregular_implements/recipe/CustomCraftingTableRecipe.kt
@@ -14,6 +14,7 @@ import net.minecraft.world.level.Level
 import net.minecraft.world.level.block.EntityBlock
 import net.neoforged.neoforge.common.Tags
 
+//TODO: EMI
 class CustomCraftingTableRecipe(
     craftingCategory: CraftingBookCategory = CraftingBookCategory.MISC
 ) : CustomRecipe(craftingCategory) {
