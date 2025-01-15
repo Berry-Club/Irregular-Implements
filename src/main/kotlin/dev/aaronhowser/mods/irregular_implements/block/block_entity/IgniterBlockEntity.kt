24fa3af47be8029e1ec5a69f2c2db270d5c6356a
Aaron Howser <aaronhowser1@gmail.com>
2025-01-15 11:00:46 -0600

    mode

--- src/main/kotlin/dev/aaronhowser/mods/irregular_implements/block/block_entity/IgniterBlockEntity.kt
+++ src/main/kotlin/dev/aaronhowser/mods/irregular_implements/block/block_entity/IgniterBlockEntity.kt
@@ -2,25 +2,42 @@ package dev.aaronhowser.mods.irregular_implements.block.block_entity
 
 import dev.aaronhowser.mods.irregular_implements.registry.ModBlockEntities
 import net.minecraft.core.BlockPos
-import net.minecraft.util.StringRepresentable
+import net.minecraft.core.HolderLookup
+import net.minecraft.nbt.CompoundTag
 import net.minecraft.world.level.block.entity.DispenserBlockEntity
 import net.minecraft.world.level.block.state.BlockState
-import java.util.*
 
 class IgniterBlockEntity(
     pPos: BlockPos,
     pBlockState: BlockState
 ) : DispenserBlockEntity(ModBlockEntities.IGNITER.get(), pPos, pBlockState) {
 
-    enum class Mode : StringRepresentable {
+    enum class Mode {
         TOGGLE,         // Make fire when powered, extinguish when unpowered
         IGNITE,         // Make fire when powered, do nothing when unpowered
         KEEP_IGNITED    // Make fire when powered, make another fire if it goes out while powered
-        ;
+    }
 
-        override fun getSerializedName(): String {
-            return name.lowercase(Locale.getDefault())
+    companion object {
+        const val MODE_NBT = "Mode"
+    }
+
+    var mode: Mode = Mode.TOGGLE
+        private set(value) {
+            field = value
+            setChanged()
         }
+
+    override fun saveAdditional(tag: CompoundTag, registries: HolderLookup.Provider) {
+        super.saveAdditional(tag, registries)
+
+        tag.putInt(MODE_NBT, this.mode.ordinal)
+    }
+
+    override fun loadAdditional(tag: CompoundTag, registries: HolderLookup.Provider) {
+        super.loadAdditional(tag, registries)
+
+        this.mode = Mode.entries[tag.getInt(MODE_NBT)]
     }
 
 }
