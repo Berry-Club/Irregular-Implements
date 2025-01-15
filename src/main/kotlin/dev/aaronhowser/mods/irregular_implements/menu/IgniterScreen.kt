2ae21db065e9c49079bb0ec29f221cfe30f0ba69
Aaron Howser <aaronhowser1@gmail.com>
2025-01-15 12:20:34 -0600

    FIXME

--- src/main/kotlin/dev/aaronhowser/mods/irregular_implements/menu/IgniterScreen.kt
+++ src/main/kotlin/dev/aaronhowser/mods/irregular_implements/menu/IgniterScreen.kt
@@ -30,12 +30,12 @@ class IgniterScreen(
         this.topPos = (this.height - this.imageHeight) / 2
 
         this.changeModeButton = ChangingTextButton(
-            this.leftPos + 5,
-            this.topPos + 5,
-            this.imageWidth - 10,
-            this.imageHeight - 10,
-            { this.menu.mode.nameComponent },
-            { ModPacketHandler.messageServer(ClientClickedMenuButton(IgniterMenu.CYCLE_MODE_BUTTON_ID)) }
+            x = this.leftPos + 5,
+            y = this.topPos + 5,
+            width = this.imageWidth - 10,
+            height = this.imageHeight - 10,
+            messageGetter = { this.menu.mode.nameComponent },   //FIXME: For some reason this won't sync to client, it gets stuck on the first value
+            onPress = { ModPacketHandler.messageServer(ClientClickedMenuButton(IgniterMenu.CYCLE_MODE_BUTTON_ID)) }
         )
 
         this.addRenderableWidget(this.changeModeButton)
