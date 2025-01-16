2364f6cadfc5b2d1209535e9877d6391770f9fe7
Aaron Howser <aaronhowser1@gmail.com>
2025-01-15 21:27:28 -0600

    fix sync

--- src/main/kotlin/dev/aaronhowser/mods/irregular_implements/block/block_entity/DiaphanousBlockEntity.kt
+++ src/main/kotlin/dev/aaronhowser/mods/irregular_implements/block/block_entity/DiaphanousBlockEntity.kt
@@ -6,6 +6,9 @@ import net.minecraft.core.HolderLookup
 import net.minecraft.core.registries.Registries
 import net.minecraft.nbt.CompoundTag
 import net.minecraft.nbt.NbtUtils
+import net.minecraft.network.protocol.Packet
+import net.minecraft.network.protocol.game.ClientGamePacketListener
+import net.minecraft.network.protocol.game.ClientboundBlockEntityDataPacket
 import net.minecraft.world.level.block.Blocks
 import net.minecraft.world.level.block.entity.BlockEntity
 import net.minecraft.world.level.block.state.BlockState
@@ -54,5 +57,9 @@ class DiaphanousBlockEntity(
 
         this.renderedBlockState = readBlockState
     }
+
+    // Syncs with client
+    override fun getUpdateTag(pRegistries: HolderLookup.Provider): CompoundTag = saveWithoutMetadata(pRegistries)
+    override fun getUpdatePacket(): Packet<ClientGamePacketListener> = ClientboundBlockEntityDataPacket.create(this)
 
 }
