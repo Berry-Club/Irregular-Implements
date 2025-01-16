2c8f03200094f02be1c3c4543b76ec84c17ced7a
Aaron Howser <aaronhowser1@gmail.com>
2025-01-15 23:43:32 -0600

    boot recipes use it

--- src/main/kotlin/dev/aaronhowser/mods/irregular_implements/compatibility/emi/recipe/LubricateBootRecipes.kt
+++ src/main/kotlin/dev/aaronhowser/mods/irregular_implements/compatibility/emi/recipe/LubricateBootRecipes.kt
@@ -3,83 +3,54 @@ package dev.aaronhowser.mods.irregular_implements.compatibility.emi.recipe
 import dev.aaronhowser.mods.irregular_implements.registry.ModDataComponents
 import dev.aaronhowser.mods.irregular_implements.registry.ModItems
 import dev.aaronhowser.mods.irregular_implements.util.OtherUtil
-import dev.emi.emi.api.recipe.EmiCraftingRecipe
 import dev.emi.emi.api.recipe.EmiRecipe
 import net.minecraft.core.registries.BuiltInRegistries
 import net.minecraft.util.Unit
 import net.minecraft.world.item.ArmorItem
+import net.minecraft.world.item.ItemStack
+import net.minecraft.world.item.alchemy.Potions
 
 object LubricateBootRecipes {
 
     fun getRecipes(): List<EmiRecipe> {
-        return lubricateRecipes() + washRecipes()
+        return lubricateRecipes()
     }
 
     private fun lubricateRecipes(): List<MutatingEmiRecipe> {
-
         val boots = BuiltInRegistries.ITEM
             .filter { it is ArmorItem && it.type == ArmorItem.Type.BOOTS }
             .map { it.defaultInstance }
 
-        val builder = MutatingEmiRecipe.Builder()
-
-        builder.otherItem(ModItems.SUPER_LUBRICANT_TINCTURE.toStack())
+        val bootMap: MutableMap<ItemStack, ItemStack> = mutableMapOf()
 
         for (boot in boots) {
-
             val lubricatedBoot = boot.copy()
             lubricatedBoot.set(ModDataComponents.LUBRICATED, Unit.INSTANCE)
+            bootMap[boot] = lubricatedBoot
+        }
 
-            builder.addStage(
-                boot,
-                lubricatedBoot
+        val lubricateBuilder = MutatingEmiRecipe.Builder()
+            .otherItem(ModItems.SUPER_LUBRICANT_TINCTURE.toStack())
+
+        val cleanBuilder = MutatingEmiRecipe.Builder()
+            .otherItem(OtherUtil.getPotionStack(Potions.WATER))
+
+        for ((cleanBoot, lubedBoot) in bootMap) {
+            lubricateBuilder.addStage(
+                cleanBoot,
+                lubedBoot
+            )
+
+            cleanBuilder.addStage(
+                lubedBoot,
+                cleanBoot
             )
         }
 
-        return listOf(builder.build(OtherUtil.modResource("/lubricate_boot")))
+        val lubricateRecipe = lubricateBuilder.build(OtherUtil.modResource("/lubricate_boot"))
+        val cleanRecipe = cleanBuilder.build(OtherUtil.modResource("/clean_boot"))
 
-//        val lubeIngredient = ModItems.SUPER_LUBRICANT_TINCTURE.emiIngredient
-//
-//        return boots.map { bootStack ->
-//            val name = BuiltInRegistries.ITEM.getKey(bootStack.item).toString().replace(':', '/')
-//
-//            val bootIngredient = EmiIngredient.of(Ingredient.of(bootStack))
-//            val output = bootStack.copy().apply { set(ModDataComponents.LUBRICATED, Unit.INSTANCE) }
-//
-//            EmiCraftingRecipe(
-//                listOf(bootIngredient, lubeIngredient),
-//                EmiStack.of(output),
-//                OtherUtil.modResource("/lubricate_boot/$name"),
-//                true
-//            )
-//        }
-    }
-
-    private fun washRecipes(): List<EmiCraftingRecipe> {
-//        val boots = BuiltInRegistries.ITEM
-//            .filter { it is ArmorItem && it.type == ArmorItem.Type.BOOTS }
-//            .map {
-//                it.defaultInstance
-//                    .apply { set(ModDataComponents.LUBRICATED, Unit.INSTANCE) }
-//            }
-//
-//        val waterIngredient = EmiIngredient.of(Ingredient.of(OtherUtil.getPotionStack(Potions.WATER)))
-//
-//        return boots.map { bootStack ->
-//            val name = BuiltInRegistries.ITEM.getKey(bootStack.item).toString().replace(':', '/')
-//
-//            val bootIngredient = EmiIngredient.of(Ingredient.of(bootStack))
-//            val output = bootStack.copy().apply { remove(ModDataComponents.LUBRICATED) }
-//
-//            EmiCraftingRecipe(
-//                listOf(bootIngredient, waterIngredient),
-//                EmiStack.of(output),
-//                OtherUtil.modResource("/wash_boot/$name"),
-//                true
-//            )
-//        }
-
-        return emptyList()
+        return listOf(lubricateRecipe, cleanRecipe)
     }
 
 }
