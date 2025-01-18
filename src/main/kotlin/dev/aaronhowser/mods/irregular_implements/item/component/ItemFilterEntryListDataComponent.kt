4be413c048d3c197eaaf78dc3a81351a2c5e6c35
Aaron Howser <aaronhowser1@gmail.com>
2025-01-17 20:54:41 -0600

    getDisplayStack

--- src/main/kotlin/dev/aaronhowser/mods/irregular_implements/item/component/ItemFilterEntryListDataComponent.kt
+++ src/main/kotlin/dev/aaronhowser/mods/irregular_implements/item/component/ItemFilterEntryListDataComponent.kt
@@ -4,6 +4,7 @@ import com.mojang.datafixers.util.Either
 import com.mojang.serialization.Codec
 import com.mojang.serialization.codecs.RecordCodecBuilder
 import dev.aaronhowser.mods.irregular_implements.util.OtherUtil
+import net.minecraft.core.registries.BuiltInRegistries
 import net.minecraft.core.registries.Registries
 import net.minecraft.network.RegistryFriendlyByteBuf
 import net.minecraft.network.codec.ByteBufCodecs
@@ -11,13 +12,13 @@ import net.minecraft.network.codec.StreamCodec
 import net.minecraft.tags.TagKey
 import net.minecraft.world.item.Item
 import net.minecraft.world.item.ItemStack
+import kotlin.random.Random
 
 data class ItemFilterEntryListDataComponent(
     val entries: List<Either<SpecificItem, TagKey<Item>>>
 ) {
 
     fun test(testedStack: ItemStack): Boolean {
-
         for (entry in this.entries) {
             var passes = false
 
@@ -64,6 +65,30 @@ data class ItemFilterEntryListDataComponent(
     }
 
     companion object {
+
+        private val random = Random(123L)
+
+        fun getDisplayStack(entry: Either<SpecificItem, TagKey<Item>>): ItemStack {
+            var displayStack: ItemStack? = null
+
+            entry.ifLeft { specificItem ->
+                displayStack = specificItem.stack
+            }
+
+            entry.ifRight { tagKey ->
+
+                val allItemsWithTag = BuiltInRegistries.ITEM
+                    .getTag(tagKey).get().toList()
+
+                val randomIndex = random.nextInt(allItemsWithTag.size)
+                val randomItem = allItemsWithTag[randomIndex]
+
+                displayStack = randomItem.value().defaultInstance
+            }
+
+            return displayStack ?: ItemStack.EMPTY
+        }
+
         val CODEC: Codec<ItemFilterEntryListDataComponent> =
             Codec.either(SpecificItem.CODEC, TagKey.codec(Registries.ITEM))
                 .listOf()
