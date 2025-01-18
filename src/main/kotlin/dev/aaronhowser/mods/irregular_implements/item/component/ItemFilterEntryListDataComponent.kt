d609fe2a3ea01651cb82d20f856f18c2c851898f
Aaron Howser <aaronhowser1@gmail.com>
2025-01-17 19:55:23 -0600

    working on ItemFilterEntryListDataComponent

--- src/main/kotlin/dev/aaronhowser/mods/irregular_implements/item/component/ItemFilterEntryListDataComponent.kt
+++ src/main/kotlin/dev/aaronhowser/mods/irregular_implements/item/component/ItemFilterEntryListDataComponent.kt
@@ -1,15 +1,61 @@
 package dev.aaronhowser.mods.irregular_implements.item.component
 
+import com.mojang.datafixers.util.Either
 import com.mojang.serialization.Codec
-import net.minecraft.advancements.critereon.ItemPredicate
+import com.mojang.serialization.codecs.EitherCodec
+import com.mojang.serialization.codecs.RecordCodecBuilder
+import net.minecraft.core.registries.Registries
+import net.minecraft.tags.TagKey
+import net.minecraft.world.item.Item
+import net.minecraft.world.item.ItemStack
 
 data class ItemFilterEntryListDataComponent(
-    val entries: List<ItemPredicate>
+    val entries: List<Either<SpecificItem, TagKey<Item>>>
 ) {
+
+    fun test(testedStack: ItemStack): Boolean {
+
+        for (entry in this.entries) {
+            var passes = false
+
+            entry.ifLeft { specificItem ->
+                passes = if (specificItem.requireSameComponents) {
+                    ItemStack.isSameItemSameComponents(specificItem.stack, testedStack)
+                } else {
+                    ItemStack.isSameItem(specificItem.stack, testedStack)
+                }
+            }.ifRight { tagKey ->
+                passes = testedStack.`is`(tagKey)
+            }
+
+            if (passes) return true
+        }
+
+        return false
+    }
+
+    data class SpecificItem(
+        val stack: ItemStack,
+        val requireSameComponents: Boolean
+    ) {
+        companion object {
+            val CODEC: Codec<SpecificItem> =
+                RecordCodecBuilder.create { instance ->
+                    instance.group(
+                        ItemStack.CODEC
+                            .fieldOf("stack")
+                            .forGetter(SpecificItem::stack),
+                        Codec.BOOL
+                            .fieldOf("require_same_components")
+                            .forGetter(SpecificItem::requireSameComponents)
+                    ).apply(instance, ::SpecificItem)
+                }
+        }
+    }
 
     companion object {
         val CODEC: Codec<ItemFilterEntryListDataComponent> =
-            ItemPredicate.CODEC
+            EitherCodec(SpecificItem.CODEC, TagKey.codec(Registries.ITEM))
                 .listOf()
                 .xmap(::ItemFilterEntryListDataComponent, ItemFilterEntryListDataComponent::entries)
     }
