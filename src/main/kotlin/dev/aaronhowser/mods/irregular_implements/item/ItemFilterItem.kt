3cf8bdb57120186a6cae11202b4f843adcfea431
Aaron Howser <aaronhowser1@gmail.com>
2025-01-17 21:11:48 -0600

    Trying to remove the Either

--- src/main/kotlin/dev/aaronhowser/mods/irregular_implements/item/ItemFilterItem.kt
+++ src/main/kotlin/dev/aaronhowser/mods/irregular_implements/item/ItemFilterItem.kt
@@ -1,11 +1,12 @@
 package dev.aaronhowser.mods.irregular_implements.item
 
-import net.minecraft.core.component.DataComponents
 import net.minecraft.world.item.Item
-import net.minecraft.world.item.component.ItemContainerContents
 
 class ItemFilterItem : Item(
     Properties()
         .stacksTo(1)
-        .component(DataComponents.CONTAINER, ItemContainerContents.EMPTY)
-)
+) {
+
+    companion object
+
+}
--- src/main/kotlin/dev/aaronhowser/mods/irregular_implements/item/component/ItemFilterEntryListDataComponent.kt
+++ src/main/kotlin/dev/aaronhowser/mods/irregular_implements/item/component/ItemFilterEntryListDataComponent.kt
@@ -1,6 +1,5 @@
 package dev.aaronhowser.mods.irregular_implements.item.component
 
-import com.mojang.datafixers.util.Either
 import com.mojang.serialization.Codec
 import com.mojang.serialization.codecs.RecordCodecBuilder
 import dev.aaronhowser.mods.irregular_implements.util.OtherUtil
@@ -16,99 +15,98 @@ import net.minecraft.world.item.ItemStack
 import kotlin.random.Random
 
 data class ItemFilterEntryListDataComponent(
-    val entries: List<Either<SpecificItemEntry, TagEntry>>
+    val entries: List<FilterEntry>
 ) {
 
     fun test(testedStack: ItemStack): Boolean {
-        for (entry in this.entries) {
-            var passes = false
+        return this.entries.any { filter ->
+            filter.test(testedStack)
+        }
+    }
 
-            entry.ifLeft { specificItem ->
-                passes = if (specificItem.requireSameComponents) {
-                    ItemStack.isSameItemSameComponents(specificItem.stack, testedStack)
-                } else {
-                    ItemStack.isSameItem(specificItem.stack, testedStack)
-                }
-            }.ifRight { tag ->
-                passes = testedStack.`is`(tag.tagKey)
+    sealed interface FilterEntry {
+
+        fun getDisplayStack(): ItemStack
+        fun test(stack: ItemStack): Boolean
+
+        data class ItemTag(
+            val tagKey: TagKey<Item>
+        ) : FilterEntry {
+
+            val matchingItems = BuiltInRegistries.ITEM.getTag(tagKey).get().toList()
+
+            override fun getDisplayStack(): ItemStack {
+                val randomIndex = random.nextInt(matchingItems.size)
+                val randomItem = matchingItems[randomIndex]
+
+                return randomItem.value().defaultInstance
             }
 
-            if (passes) return true
+            override fun test(stack: ItemStack): Boolean {
+                return stack.`is`(tagKey)
+            }
+
+            companion object {
+                private val random = Random(123L)
+
+                val CODEC: Codec<ItemTag> =
+                    TagKey.codec(Registries.ITEM).xmap(::ItemTag, ItemTag::tagKey)
+
+                val STREAM_CODEC: StreamCodec<ByteBuf, ItemTag> =
+                    OtherUtil.tagKeyStreamCodec(Registries.ITEM)
+                        .map(::ItemTag, ItemTag::tagKey)
+            }
         }
 
-        return false
-    }
+        data class SpecificItem(
+            val stack: ItemStack,
+            val requireSameComponents: Boolean
+        ) : FilterEntry {
 
-    data class TagEntry(
-        val tagKey: TagKey<Item>
-    ) {
+            override fun getDisplayStack(): ItemStack {
+                return stack.copy()
+            }
 
-        val matchingItems = BuiltInRegistries.ITEM.getTag(tagKey).get().toList()
-
-        companion object {
-            val CODEC: Codec<TagEntry> =
-                TagKey.codec(Registries.ITEM).xmap(::TagEntry, TagEntry::tagKey)
-
-            val STREAM_CODEC: StreamCodec<ByteBuf, TagEntry> =
-                OtherUtil.tagKeyStreamCodec(Registries.ITEM)
-                    .map(::TagEntry, TagEntry::tagKey)
-        }
-    }
-
-    data class SpecificItemEntry(
-        val stack: ItemStack,
-        val requireSameComponents: Boolean
-    ) {
-        companion object {
-            val CODEC: Codec<SpecificItemEntry> =
-                RecordCodecBuilder.create { instance ->
-                    instance.group(
-                        ItemStack.CODEC
-                            .fieldOf("stack")
-                            .forGetter(SpecificItemEntry::stack),
-                        Codec.BOOL
-                            .fieldOf("require_same_components")
-                            .forGetter(SpecificItemEntry::requireSameComponents)
-                    ).apply(instance, ::SpecificItemEntry)
+            override fun test(stack: ItemStack): Boolean {
+                return if (this.requireSameComponents) {
+                    ItemStack.isSameItemSameComponents(this.stack, stack)
+                } else {
+                    ItemStack.isSameItem(this.stack, stack)
                 }
+            }
 
-            val STREAM_CODEC: StreamCodec<RegistryFriendlyByteBuf, SpecificItemEntry> =
-                StreamCodec.composite(
-                    ItemStack.STREAM_CODEC, SpecificItemEntry::stack,
-                    ByteBufCodecs.BOOL, SpecificItemEntry::requireSameComponents,
-                    ::SpecificItemEntry
-                )
+            companion object {
+                val CODEC: Codec<SpecificItem> =
+                    RecordCodecBuilder.create { instance ->
+                        instance.group(
+                            ItemStack.CODEC
+                                .fieldOf("stack")
+                                .forGetter(SpecificItem::stack),
+                            Codec.BOOL
+                                .fieldOf("require_same_components")
+                                .forGetter(SpecificItem::requireSameComponents)
+                        ).apply(instance, ::SpecificItem)
+                    }
+
+                val STREAM_CODEC: StreamCodec<RegistryFriendlyByteBuf, SpecificItem> =
+                    StreamCodec.composite(
+                        ItemStack.STREAM_CODEC, SpecificItem::stack,
+                        ByteBufCodecs.BOOL, SpecificItem::requireSameComponents,
+                        ::SpecificItem
+                    )
+            }
         }
     }
 
     companion object {
 
-        private val random = Random(123L)
-
-        fun getDisplayStack(entry: Either<SpecificItemEntry, TagEntry>): ItemStack {
-            var displayStack: ItemStack? = null
-
-            entry.ifLeft { specificItem ->
-                displayStack = specificItem.stack
-            }
-
-            entry.ifRight { tag ->
-                val randomIndex = random.nextInt(tag.matchingItems.size)
-                val randomItem = tag.matchingItems[randomIndex]
-
-                displayStack = randomItem.value().defaultInstance
-            }
-
-            return displayStack ?: ItemStack.EMPTY
-        }
-
         val CODEC: Codec<ItemFilterEntryListDataComponent> =
-            Codec.either(SpecificItemEntry.CODEC, TagEntry.CODEC)
+            Codec.either(FilterEntry.SpecificItem.CODEC, FilterEntry.ItemTag.CODEC)
                 .listOf()
                 .xmap(::ItemFilterEntryListDataComponent, ItemFilterEntryListDataComponent::entries)
 
         val STREAM_CODEC: StreamCodec<RegistryFriendlyByteBuf, ItemFilterEntryListDataComponent> =
-            ByteBufCodecs.either(SpecificItemEntry.STREAM_CODEC, TagEntry.STREAM_CODEC)
+            ByteBufCodecs.either(FilterEntry.SpecificItem.STREAM_CODEC, FilterEntry.ItemTag.STREAM_CODEC)
                 .apply(ByteBufCodecs.list())
                 .map(::ItemFilterEntryListDataComponent, ItemFilterEntryListDataComponent::entries)
     }
