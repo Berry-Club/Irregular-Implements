83cd40c246c620b48d9bd49949506205f293fd46
Aaron Howser <aaronhowser1@gmail.com>
2025-01-21 10:25:46 -0600

    remove lambdas, move to ::

--- src/main/kotlin/dev/aaronhowser/mods/irregular_implements/registry/ModBlockEntities.kt
+++ src/main/kotlin/dev/aaronhowser/mods/irregular_implements/registry/ModBlockEntities.kt
@@ -17,7 +17,7 @@ object ModBlockEntities {
     val RAIN_SHIELD: DeferredHolder<BlockEntityType<*>, BlockEntityType<RainShieldBlockEntity>> =
         BLOCK_ENTITY_REGISTRY.register("rain_shield", Supplier {
             BlockEntityType.Builder.of(
-                { pos, state -> RainShieldBlockEntity(pos, state) },
+                ::RainShieldBlockEntity,
                 ModBlocks.RAIN_SHIELD.get()
             ).build(null)
         })
@@ -25,7 +25,7 @@ object ModBlockEntities {
     val REDSTONE_INTERFACE: DeferredHolder<BlockEntityType<*>, BlockEntityType<RedstoneInterfaceBasicBlockEntity>> =
         BLOCK_ENTITY_REGISTRY.register("redstone_interface", Supplier {
             BlockEntityType.Builder.of(
-                { pos, state -> RedstoneInterfaceBasicBlockEntity(pos, state) },
+                ::RedstoneInterfaceBasicBlockEntity,
                 ModBlocks.BASIC_REDSTONE_INTERFACE.get()
             ).build(null)
         })
@@ -33,7 +33,7 @@ object ModBlockEntities {
     val REDSTONE_OBSERVER: DeferredHolder<BlockEntityType<*>, BlockEntityType<RedstoneObserverBlockEntity>> =
         BLOCK_ENTITY_REGISTRY.register("redstone_observer", Supplier {
             BlockEntityType.Builder.of(
-                { pos, state -> RedstoneObserverBlockEntity(pos, state) },
+                ::RedstoneObserverBlockEntity,
                 ModBlocks.REDSTONE_OBSERVER.get()
             ).build(null)
         })
@@ -41,7 +41,7 @@ object ModBlockEntities {
     val BLOCK_DESTABILIZER: DeferredHolder<BlockEntityType<*>, BlockEntityType<BlockDestabilizerBlockEntity>> =
         BLOCK_ENTITY_REGISTRY.register("block_destabilizer", Supplier {
             BlockEntityType.Builder.of(
-                { pos, state -> BlockDestabilizerBlockEntity(pos, state) },
+                ::BlockDestabilizerBlockEntity,
                 ModBlocks.BLOCK_DESTABILIZER.get()
             ).build(null)
         })
@@ -49,7 +49,7 @@ object ModBlockEntities {
     val BLOCK_BREAKER: DeferredHolder<BlockEntityType<*>, BlockEntityType<BlockBreakerBlockEntity>> =
         BLOCK_ENTITY_REGISTRY.register("block_breaker", Supplier {
             BlockEntityType.Builder.of(
-                { pos, state -> BlockBreakerBlockEntity(pos, state) },
+                ::BlockBreakerBlockEntity,
                 ModBlocks.BLOCK_BREAKER.get()
             ).build(null)
         })
@@ -57,7 +57,7 @@ object ModBlockEntities {
     val SPECTRE_LENS: DeferredHolder<BlockEntityType<*>, BlockEntityType<SpectreLensBlockEntity>> =
         BLOCK_ENTITY_REGISTRY.register("spectre_lens", Supplier {
             BlockEntityType.Builder.of(
-                { pos, state -> SpectreLensBlockEntity(pos, state) },
+                ::SpectreLensBlockEntity,
                 ModBlocks.SPECTRE_LENS.get()
             ).build(null)
         })
@@ -65,7 +65,7 @@ object ModBlockEntities {
     val SPECTRE_ENERGY_INJECTOR: DeferredHolder<BlockEntityType<*>, BlockEntityType<SpectreEnergyInjectorBlockEntity>> =
         BLOCK_ENTITY_REGISTRY.register("spectre_energy_injector", Supplier {
             BlockEntityType.Builder.of(
-                { pos, state -> SpectreEnergyInjectorBlockEntity(pos, state) },
+                ::SpectreEnergyInjectorBlockEntity,
                 ModBlocks.SPECTRE_ENERGY_INJECTOR.get()
             ).build(null)
         })
@@ -73,7 +73,7 @@ object ModBlockEntities {
     val SPECTRE_COIL: DeferredHolder<BlockEntityType<*>, BlockEntityType<SpectreCoilBlockEntity>> =
         BLOCK_ENTITY_REGISTRY.register("spectre_coil", Supplier {
             BlockEntityType.Builder.of(
-                { pos, state -> SpectreCoilBlockEntity(pos, state) },
+                ::SpectreCoilBlockEntity,
                 ModBlocks.SPECTRE_COIL_BASIC.get(),
                 ModBlocks.SPECTRE_COIL_REDSTONE.get(),
                 ModBlocks.SPECTRE_COIL_ENDER.get(),
@@ -85,7 +85,7 @@ object ModBlockEntities {
     val MOON_PHASE_DETECTOR: DeferredHolder<BlockEntityType<*>, BlockEntityType<MoonPhaseDetectorBlockEntity>> =
         BLOCK_ENTITY_REGISTRY.register("moon_phase_detector", Supplier {
             BlockEntityType.Builder.of(
-                { pos, state -> MoonPhaseDetectorBlockEntity(pos, state) },
+                ::MoonPhaseDetectorBlockEntity,
                 ModBlocks.MOON_PHASE_DETECTOR.get()
             ).build(null)
         })
@@ -93,7 +93,7 @@ object ModBlockEntities {
     val CHAT_DETECTOR: DeferredHolder<BlockEntityType<*>, BlockEntityType<ChatDetectorBlockEntity>> =
         BLOCK_ENTITY_REGISTRY.register("chat_detector", Supplier {
             BlockEntityType.Builder.of(
-                { pos, state -> ChatDetectorBlockEntity(pos, state) },
+                ::ChatDetectorBlockEntity,
                 ModBlocks.CHAT_DETECTOR.get()
             ).build(null)
         })
@@ -101,7 +101,7 @@ object ModBlockEntities {
     val GLOBAL_CHAT_DETECTOR: DeferredHolder<BlockEntityType<*>, BlockEntityType<GlobalChatDetectorBlockEntity>> =
         BLOCK_ENTITY_REGISTRY.register("global_chat_detector", Supplier {
             BlockEntityType.Builder.of(
-                { pos, state -> GlobalChatDetectorBlockEntity(pos, state) },
+                ::GlobalChatDetectorBlockEntity,
                 ModBlocks.GLOBAL_CHAT_DETECTOR.get()
             ).build(null)
         })
@@ -109,7 +109,7 @@ object ModBlockEntities {
     val ONLINE_DETECTOR: DeferredHolder<BlockEntityType<*>, BlockEntityType<OnlineDetectorBlockEntity>> =
         BLOCK_ENTITY_REGISTRY.register("online_detector", Supplier {
             BlockEntityType.Builder.of(
-                { pos, state -> OnlineDetectorBlockEntity(pos, state) },
+                ::OnlineDetectorBlockEntity,
                 ModBlocks.ONLINE_DETECTOR.get()
             ).build(null)
         })
@@ -117,7 +117,7 @@ object ModBlockEntities {
     val DIAPHANOUS_BLOCK: DeferredHolder<BlockEntityType<*>, BlockEntityType<DiaphanousBlockEntity>> =
         BLOCK_ENTITY_REGISTRY.register("diaphanous_block", Supplier {
             BlockEntityType.Builder.of(
-                { pos, state -> DiaphanousBlockEntity(pos, state) },
+                ::DiaphanousBlockEntity,
                 ModBlocks.DIAPHANOUS_BLOCK.get()
             ).build(null)
         })
@@ -125,7 +125,7 @@ object ModBlockEntities {
     val IRON_DROPPER: DeferredHolder<BlockEntityType<*>, BlockEntityType<IronDropperBlockEntity>> =
         BLOCK_ENTITY_REGISTRY.register("iron_dropper", Supplier {
             BlockEntityType.Builder.of(
-                { pos, state -> IronDropperBlockEntity(pos, state) },
+                ::IronDropperBlockEntity,
                 ModBlocks.IRON_DROPPER.get()
             ).build(null)
         })
@@ -133,7 +133,7 @@ object ModBlockEntities {
     val CUSTOM_CRAFTING_TABLE: DeferredHolder<BlockEntityType<*>, BlockEntityType<CustomCraftingTableBlockEntity>> =
         BLOCK_ENTITY_REGISTRY.register("custom_crafting_table", Supplier {
             BlockEntityType.Builder.of(
-                { pos, state -> CustomCraftingTableBlockEntity(pos, state) },
+                ::CustomCraftingTableBlockEntity,
                 ModBlocks.CUSTOM_CRAFTING_TABLE.get()
             ).build(null)
         })
@@ -141,7 +141,7 @@ object ModBlockEntities {
     val IGNITER: DeferredHolder<BlockEntityType<*>, BlockEntityType<IgniterBlockEntity>> =
         BLOCK_ENTITY_REGISTRY.register("igniter", Supplier {
             BlockEntityType.Builder.of(
-                { pos, state -> IgniterBlockEntity(pos, state) },
+                ::IgniterBlockEntity,
                 ModBlocks.IGNITER.get()
             ).build(null)
         })
@@ -149,7 +149,7 @@ object ModBlockEntities {
     val NOTIFICATION_INTERFACE: DeferredHolder<BlockEntityType<*>, BlockEntityType<NotificationInterfaceBlockEntity>> =
         BLOCK_ENTITY_REGISTRY.register("notification_interface", Supplier {
             BlockEntityType.Builder.of(
-                { pos, state -> NotificationInterfaceBlockEntity(pos, state) },
+                ::NotificationInterfaceBlockEntity,
                 ModBlocks.NOTIFICATION_INTERFACE.get()
             ).build(null)
         })
@@ -157,15 +157,15 @@ object ModBlockEntities {
     val IMBUING_STATION: DeferredHolder<BlockEntityType<*>, BlockEntityType<ImbuingStationBlockEntity>> =
         BLOCK_ENTITY_REGISTRY.register("imbuing_station", Supplier {
             BlockEntityType.Builder.of(
-                { pos, state -> ImbuingStationBlockEntity(pos, state) },
+                ::ImbuingStationBlockEntity,
                 ModBlocks.IMBUING_STATION.get()
             ).build(null)
         })
 
-    val FILTERED_PLATFORM =
+    val FILTERED_PLATFORM: DeferredHolder<BlockEntityType<*>, BlockEntityType<FilteredPlatformBlockEntity>> =
         BLOCK_ENTITY_REGISTRY.register("filtered_platform", Supplier {
             BlockEntityType.Builder.of(
-                { pos, state -> FilteredPlatformBlockEntity(pos, state) },
+                ::FilteredPlatformBlockEntity,
                 ModBlocks.FILTERED_SUPER_LUBRICANT_PLATFORM.get()
             ).build(null)
         })
