458791c80bd1d4124d176e1c0d2a1b732d62ea00
Aaron Howser <aaronhowser1@gmail.com>
2025-01-30 16:43:15 -0600

    made it use biome tags

--- src/main/kotlin/dev/aaronhowser/mods/irregular_implements/datagen/datapack/features/NatureCoreFeature.kt
+++ src/main/kotlin/dev/aaronhowser/mods/irregular_implements/datagen/datapack/features/NatureCoreFeature.kt
@@ -1,7 +1,10 @@
 package dev.aaronhowser.mods.irregular_implements.datagen.datapack.features
 
+import dev.aaronhowser.mods.irregular_implements.datagen.tag.ModBiomeTagsProvider
 import dev.aaronhowser.mods.irregular_implements.registry.ModBlocks
 import net.minecraft.core.Direction
+import net.minecraft.core.Holder
+import net.minecraft.world.level.biome.Biome
 import net.minecraft.world.level.block.Blocks
 import net.minecraft.world.level.block.LeavesBlock
 import net.minecraft.world.level.block.RotatedPillarBlock
@@ -14,15 +17,55 @@ class NatureCoreFeature : Feature<NoneFeatureConfiguration>(NoneFeatureConfigura
 
     companion object {
         private val natureCore: BlockState = ModBlocks.NATURE_CORE.get().defaultBlockState()
-        private val log = Blocks.JUNGLE_LOG.defaultBlockState().setValue(RotatedPillarBlock.AXIS, Direction.Axis.Y)
-        private val leaves = Blocks.JUNGLE_LEAVES.defaultBlockState().setValue(LeavesBlock.PERSISTENT, true)
+
+        private fun getLogFromBiome(biome: Holder<Biome>): BlockState {
+            return when {
+                biome.`is`(ModBiomeTagsProvider.NATURE_CORE_BIRCH) -> Blocks.BIRCH_LOG
+
+                biome.`is`(ModBiomeTagsProvider.NATURE_CORE_DARK_OAK) -> Blocks.DARK_OAK_LOG
+
+                biome.`is`(ModBiomeTagsProvider.NATURE_CORE_JUNGLE) -> Blocks.JUNGLE_LOG
+
+                biome.`is`(ModBiomeTagsProvider.NATURE_CORE_SPRUCE) -> Blocks.SPRUCE_LOG
+
+                biome.`is`(ModBiomeTagsProvider.NATURE_CORE_ACACIA) -> Blocks.ACACIA_LOG
+
+                biome.`is`(ModBiomeTagsProvider.NATURE_CORE_MANGROVE) -> Blocks.MANGROVE_LOG
+
+                biome.`is`(ModBiomeTagsProvider.NATURE_CORE_OAK) -> Blocks.OAK_LOG
+                else -> Blocks.OAK_LOG
+            }.defaultBlockState().setValue(RotatedPillarBlock.AXIS, Direction.Axis.Y)
+        }
+
+        private fun getLeavesFromBiome(biome: Holder<Biome>): BlockState {
+            return when {
+                biome.`is`(ModBiomeTagsProvider.NATURE_CORE_BIRCH) -> Blocks.BIRCH_LEAVES
+
+                biome.`is`(ModBiomeTagsProvider.NATURE_CORE_DARK_OAK) -> Blocks.DARK_OAK_LEAVES
+
+                biome.`is`(ModBiomeTagsProvider.NATURE_CORE_JUNGLE) -> Blocks.JUNGLE_LEAVES
+
+                biome.`is`(ModBiomeTagsProvider.NATURE_CORE_SPRUCE) -> Blocks.SPRUCE_LEAVES
+
+                biome.`is`(ModBiomeTagsProvider.NATURE_CORE_ACACIA) -> Blocks.ACACIA_LEAVES
+
+                biome.`is`(ModBiomeTagsProvider.NATURE_CORE_MANGROVE) -> Blocks.MANGROVE_LEAVES
+
+                biome.`is`(ModBiomeTagsProvider.NATURE_CORE_OAK) -> Blocks.OAK_LEAVES
+                else -> Blocks.OAK_LEAVES
+            }.defaultBlockState().setValue(LeavesBlock.PERSISTENT, true)
+        }
+
     }
 
     override fun place(context: FeaturePlaceContext<NoneFeatureConfiguration>): Boolean {
         val origin = context.origin()
         val level = context.level()
 
+        val biome = level.getBiome(origin)
 
+        val log = getLogFromBiome(biome)
+        val leaves = getLeavesFromBiome(biome)
 
         for (dX in -1..1) for (dY in -1..1) for (dZ in -1..1) {
 
--- src/main/kotlin/dev/aaronhowser/mods/irregular_implements/datagen/tag/ModBiomeTagsProvider.kt
+++ src/main/kotlin/dev/aaronhowser/mods/irregular_implements/datagen/tag/ModBiomeTagsProvider.kt
@@ -6,10 +6,11 @@ import net.minecraft.core.HolderLookup
 import net.minecraft.core.registries.Registries
 import net.minecraft.data.PackOutput
 import net.minecraft.data.tags.BiomeTagsProvider
-import net.minecraft.data.tags.EnchantmentTagsProvider
+import net.minecraft.tags.BiomeTags
 import net.minecraft.tags.TagKey
 import net.minecraft.world.level.biome.Biome
 import net.minecraft.world.level.biome.Biomes
+import net.neoforged.neoforge.common.Tags
 import net.neoforged.neoforge.common.data.ExistingFileHelper
 import java.util.concurrent.CompletableFuture
 
@@ -20,11 +21,50 @@ class ModBiomeTagsProvider(
 ) : BiomeTagsProvider(pOutput, pProvider, IrregularImplements.ID, existingFileHelper) {
 
     companion object {
-        val BIOME_CRYSTAL_BLACKLIST: TagKey<Biome> = TagKey.create(Registries.BIOME, OtherUtil.modResource("biome_crystal_blacklist"))
+        private fun create(name: String): TagKey<Biome> {
+            return TagKey.create(Registries.BIOME, OtherUtil.modResource(name))
+        }
+
+        val BIOME_CRYSTAL_BLACKLIST = create("biome_crystal_blacklist")
+        val NATURE_CORE_OAK = create("nature_core_oak")
+        val NATURE_CORE_DARK_OAK = create("nature_core_dark_oak")
+        val NATURE_CORE_BIRCH = create("nature_core_birch")
+        val NATURE_CORE_JUNGLE = create("nature_core_jungle")
+        val NATURE_CORE_ACACIA = create("nature_core_acacia")
+        val NATURE_CORE_SPRUCE = create("nature_core_spruce")
+        val NATURE_CORE_MANGROVE = create("nature_core_mangrove")
     }
 
     override fun addTags(provider: HolderLookup.Provider) {
-        // None by default
+
+        tag(BIOME_CRYSTAL_BLACKLIST)    // Empty by default
+        tag(NATURE_CORE_OAK)            // Empty by default (used as fallback)
+
+        tag(NATURE_CORE_DARK_OAK)
+            .add(
+                Biomes.DARK_FOREST
+            )
+
+        tag(NATURE_CORE_BIRCH)
+            .add(
+                Biomes.BIRCH_FOREST,
+                Biomes.OLD_GROWTH_BIRCH_FOREST
+            )
+
+        tag(NATURE_CORE_JUNGLE)
+            .addTag(BiomeTags.IS_JUNGLE)
+
+        tag(NATURE_CORE_SPRUCE)
+            .addTag(Tags.Biomes.IS_COLD_OVERWORLD)
+
+        tag(NATURE_CORE_ACACIA)
+            .addTag(BiomeTags.IS_SAVANNA)
+
+        tag(NATURE_CORE_MANGROVE)
+            .add(
+                Biomes.MANGROVE_SWAMP
+            )
+
     }
 
 }
