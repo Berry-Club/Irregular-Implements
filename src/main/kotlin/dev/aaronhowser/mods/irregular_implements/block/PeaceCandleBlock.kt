7250f47cdca7a72b36dcaa404bd610bf2277cb5a
Aaron Howser <aaronhowser1@gmail.com>
2025-02-02 22:27:20 -0600

    replace vanilla datapacks

--- src/main/kotlin/dev/aaronhowser/mods/irregular_implements/block/PeaceCandleBlock.kt
+++ src/main/kotlin/dev/aaronhowser/mods/irregular_implements/block/PeaceCandleBlock.kt
@@ -10,6 +10,9 @@ import net.minecraft.world.level.block.state.BlockState
 import net.minecraft.world.phys.shapes.CollisionContext
 import net.minecraft.world.phys.shapes.VoxelShape
 
+// Right now these are being added to the structures by recreating the structures entirely, and
+// adding the structures to the pool. This is currently being done by ENTIRELY REPLACING the vanilla
+// datapack files, which SUCKS and IS BAD. //TODO: DON'T DO THAT
 class PeaceCandleBlock : Block(Properties.ofFullCopy(Blocks.OBSIDIAN)) {
 
     companion object {
