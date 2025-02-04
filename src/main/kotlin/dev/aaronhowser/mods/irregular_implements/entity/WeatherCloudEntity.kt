17fea890172fe1036c0e6c3c40b299d2566f591e
Aaron Howser <aaronhowser1@gmail.com>
2025-02-04 02:13:30 -0600

    progress

--- src/main/kotlin/dev/aaronhowser/mods/irregular_implements/entity/WeatherCloudEntity.kt
+++ src/main/kotlin/dev/aaronhowser/mods/irregular_implements/entity/WeatherCloudEntity.kt
@@ -9,8 +9,12 @@ import net.minecraft.network.syncher.SynchedEntityData
 import net.minecraft.server.level.ServerLevel
 import net.minecraft.world.entity.Entity
 import net.minecraft.world.entity.EntityType
+import net.minecraft.world.entity.MoverType
 import net.minecraft.world.level.Level
 import net.minecraft.world.phys.Vec3
+import kotlin.math.abs
+import kotlin.math.cos
+import kotlin.math.sin
 
 class WeatherCloudEntity(entityType: EntityType<*>, level: Level) : Entity(entityType, level) {
 
@@ -44,13 +48,49 @@ class WeatherCloudEntity(entityType: EntityType<*>, level: Level) : Entity(entit
             this.addDeltaMovement(Vec3(0.0, 0.001, 0.0))
             this.deltaMovement = this.deltaMovement.scale(1.02)
 
-            if (this.position().y < this.level().maxBuildHeight) return
+            if (this.position().y >= this.level().maxBuildHeight) {
+                setWeather()
+                this.discard()
+                return
+            }
+        }
 
-            setWeather()
-            this.discard()
+        this.age++
+        this.move(MoverType.SELF, this.deltaMovement)
+        if (this.level().isClientSide) spawnParticles()
+    }
+
+    private fun spawnParticles() {
+
+        if (this.weather == WeatherEggItem.Weather.SUNNY) {
+            spawnNiceCloud()
             return
         }
 
+    }
+
+    private fun spawnNiceCloud() {
+        val level = this.level()
+        if (!level.isClientSide) return
+
+        for (y in -1..1) {
+
+            var t = 0.0
+            while (t < Math.PI * 2) {
+                t += Math.PI / 3
+
+                var a = 0.25
+                var b = 0.35
+
+                a /= abs(y) * 0.5 + 1
+                b /= abs(y) * 0.5 + 1
+
+                val elX = a * cos(t)
+                val elZ = b * sin(t)
+
+            }
+
+        }
 
     }
 
@@ -75,37 +115,6 @@ class WeatherCloudEntity(entityType: EntityType<*>, level: Level) : Entity(entit
 
     override fun addAdditionalSaveData(compound: CompoundTag) {
         compound.putInt(AGE_NBT, this.age)
-    }
-
-
-    private fun setSunny() {
-        val level = this.level() as? ServerLevel ?: return
-        level.setWeatherParameters(
-            ServerLevel.RAIN_DELAY.sample(level.random),
-            0,
-            false,
-            false
-        )
-    }
-
-    private fun setRainy() {
-        val level = this.level() as? ServerLevel ?: return
-        level.setWeatherParameters(
-            0,
-            ServerLevel.RAIN_DURATION.sample(level.random),
-            true,
-            false
-        )
-    }
-
-    private fun setStormy() {
-        val level = this.level() as? ServerLevel ?: return
-        level.setWeatherParameters(
-            0,
-            ServerLevel.RAIN_DURATION.sample(level.random),
-            true,
-            true
-        )
     }
 
 }
