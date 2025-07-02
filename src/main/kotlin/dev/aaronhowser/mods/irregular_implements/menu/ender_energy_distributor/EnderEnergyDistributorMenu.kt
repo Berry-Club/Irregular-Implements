3d3cc13e45074c2529bad03af3de11693c3580f5
aaron <aaronhowser1@gmail.com>
2025-07-02 11:13:01 -0500

    menu finished ish?

--- src/main/kotlin/dev/aaronhowser/mods/irregular_implements/menu/ender_energy_distributor/EnderEnergyDistributorMenu.kt
+++ src/main/kotlin/dev/aaronhowser/mods/irregular_implements/menu/ender_energy_distributor/EnderEnergyDistributorMenu.kt
@@ -1,12 +1,15 @@
 package dev.aaronhowser.mods.irregular_implements.menu.ender_energy_distributor
 
 import dev.aaronhowser.mods.irregular_implements.block.block_entity.EnderEnergyDistributorBlockEntity
+import dev.aaronhowser.mods.irregular_implements.registry.ModDataComponents
 import dev.aaronhowser.mods.irregular_implements.registry.ModMenuTypes
+import dev.aaronhowser.mods.irregular_implements.util.OtherUtil
 import net.minecraft.world.Container
 import net.minecraft.world.SimpleContainer
 import net.minecraft.world.entity.player.Inventory
 import net.minecraft.world.entity.player.Player
 import net.minecraft.world.inventory.AbstractContainerMenu
+import net.minecraft.world.inventory.InventoryMenu
 import net.minecraft.world.inventory.Slot
 import net.minecraft.world.item.ItemStack
 
@@ -24,8 +27,20 @@ class EnderEnergyDistributorMenu(
 			)
 
 	init {
-		checkContainerSize(container, 1)
+		checkContainerSize(container, EnderEnergyDistributorBlockEntity.INVENTORY_SIZE)
 		container.startOpen(playerInventory.player)
+
+		for (i in 0 until EnderEnergyDistributorBlockEntity.INVENTORY_SIZE) {
+			val x = 17 + (i % 9) * 18
+
+			val slot = object : Slot(container, i, x, 18) {
+				override fun mayPlace(stack: ItemStack): Boolean {
+					return stack.has(ModDataComponents.LOCATION)
+				}
+			}
+
+			this.addSlot(slot)
+		}
 
 		for (row in 0..2) {
 			for (column in 0..8) {
