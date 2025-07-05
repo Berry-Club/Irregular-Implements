c61e446774901657e1b3093fb716239f58afee31
aaron <aaronhowser1@gmail.com>
2025-07-05 16:16:37 -0500

    it works now

--- src/main/kotlin/dev/aaronhowser/mods/irregular_implements/block/block_entity/SlimeCubeBlockEntity.kt
+++ src/main/kotlin/dev/aaronhowser/mods/irregular_implements/block/block_entity/SlimeCubeBlockEntity.kt
@@ -3,11 +3,11 @@ package dev.aaronhowser.mods.irregular_implements.block.block_entity
 import dev.aaronhowser.mods.irregular_implements.registry.ModBlockEntities
 import net.minecraft.core.BlockPos
 import net.minecraft.world.Difficulty
+import net.minecraft.world.level.ChunkPos
 import net.minecraft.world.level.Level
 import net.minecraft.world.level.LevelAccessor
 import net.minecraft.world.level.block.entity.BlockEntity
 import net.minecraft.world.level.block.state.BlockState
-import org.antlr.v4.runtime.misc.MultiMap
 
 class SlimeCubeBlockEntity(
 	pos: BlockPos,
@@ -18,32 +18,34 @@ class SlimeCubeBlockEntity(
 		super.onLoad()
 		val level = this.level ?: return
 
-		CUBES.computeIfAbsent(level) { mutableListOf() }.add(this)
+		CUBES.computeIfAbsent(level) { mutableSetOf() }.add(this)
 	}
 
 	override fun setRemoved() {
 		super.setRemoved()
 		val level = this.level ?: return
 
-		CUBES.computeIfAbsent(level) { mutableListOf() }.remove(this)
+		CUBES.computeIfAbsent(level) { mutableSetOf() }.remove(this)
 	}
 
 	override fun clearRemoved() {
 		super.clearRemoved()
 		val level = this.level ?: return
 
-		CUBES.computeIfAbsent(level) { mutableListOf() }.add(this)
+		CUBES.computeIfAbsent(level) { mutableSetOf() }.add(this)
 	}
 
 	companion object {
-		private val CUBES: MultiMap<Level, SlimeCubeBlockEntity> = MultiMap()
+		private val CUBES: MutableMap<Level, MutableSet<SlimeCubeBlockEntity>> = mutableMapOf()
 
 		fun chunkHasCube(level: Level, pos: BlockPos, powered: Boolean): Boolean {
-			val chunkPos = level.getChunk(pos).pos
+			val chunkPos = ChunkPos(pos)
 
 			val cubesInThatChunk = CUBES
-				.getOrDefault(level, emptyList())
-				.filter { it.level?.getChunk(it.worldPosition)?.pos == chunkPos }
+				.getOrDefault(level, emptySet())
+				.filter {
+					ChunkPos(it.worldPosition) == chunkPos
+				}
 
 			if (cubesInThatChunk.isEmpty()) return false
 
@@ -57,7 +59,7 @@ class SlimeCubeBlockEntity(
 			if (chunkHasCube(level, pos, powered = true)) return false
 			if (chunkHasCube(level, pos, powered = false)) return true
 
-			return false
+			return null
 		}
 	}
 }
