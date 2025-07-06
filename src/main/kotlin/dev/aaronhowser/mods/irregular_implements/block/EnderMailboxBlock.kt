52a301e91abfbd906521d92f3ea3b8f7253a16ef
aaron <aaronhowser1@gmail.com>
2025-07-05 20:43:43 -0500

    dun it

--- src/main/kotlin/dev/aaronhowser/mods/irregular_implements/block/EnderMailboxBlock.kt
+++ src/main/kotlin/dev/aaronhowser/mods/irregular_implements/block/EnderMailboxBlock.kt
@@ -1,7 +1,11 @@
 package dev.aaronhowser.mods.irregular_implements.block
 
+import net.minecraft.core.BlockPos
 import net.minecraft.core.Direction
 import net.minecraft.world.item.context.BlockPlaceContext
+import net.minecraft.world.level.BlockGetter
+import net.minecraft.world.level.LevelAccessor
+import net.minecraft.world.level.LevelReader
 import net.minecraft.world.level.block.Block
 import net.minecraft.world.level.block.Blocks
 import net.minecraft.world.level.block.state.BlockState
@@ -9,6 +13,8 @@ import net.minecraft.world.level.block.state.StateDefinition
 import net.minecraft.world.level.block.state.properties.BlockStateProperties
 import net.minecraft.world.level.block.state.properties.BooleanProperty
 import net.minecraft.world.level.block.state.properties.DirectionProperty
+import net.minecraft.world.phys.shapes.CollisionContext
+import net.minecraft.world.phys.shapes.VoxelShape
 
 class EnderMailboxBlock : Block(Properties.ofFullCopy(Blocks.IRON_BLOCK)) {
 
@@ -29,9 +35,34 @@ class EnderMailboxBlock : Block(Properties.ofFullCopy(Blocks.IRON_BLOCK)) {
 			.setValue(FACING, context.nearestLookingDirections.first { it.axis.isHorizontal })
 	}
 
+	override fun canSurvive(state: BlockState, level: LevelReader, pos: BlockPos): Boolean {
+		return canSupportCenter(level, pos.below(), Direction.UP)
+	}
+
+	override fun updateShape(state: BlockState, direction: Direction, neighborState: BlockState, level: LevelAccessor, pos: BlockPos, neighborPos: BlockPos): BlockState {
+		return if (state.canSurvive(level, pos)) {
+			state
+		} else {
+			Blocks.AIR.defaultBlockState()
+		}
+	}
+
+	override fun getShape(state: BlockState, level: BlockGetter, pos: BlockPos, context: CollisionContext): VoxelShape {
+		val direction = state.getValue(FACING)
+
+		return when (direction) {
+			Direction.NORTH, Direction.SOUTH -> SHAPE_NS
+			Direction.EAST, Direction.WEST -> SHAPE_EW
+			else -> SHAPE_NS
+		}
+	}
+
 	companion object {
 		val IS_FLAG_RAISED: BooleanProperty = BooleanProperty.create("is_flag_raised")
 		val FACING: DirectionProperty = BlockStateProperties.HORIZONTAL_FACING
+
+		val SHAPE_NS: VoxelShape = box(5.0, 0.0, 1.0, 11.0, 22.0, 15.0)
+		val SHAPE_EW: VoxelShape = box(1.0, 0.0, 5.0, 15.0, 22.0, 11.0)
 	}
 
 }
