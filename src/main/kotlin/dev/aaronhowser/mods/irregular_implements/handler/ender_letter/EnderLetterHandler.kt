6f181c4747f110eac6e6f057406be2fab9a5bdfe
aaron <aaronhowser1@gmail.com>
2025-07-05 21:06:40 -0500

    working on EnderMailbox

---
+++ src/main/kotlin/dev/aaronhowser/mods/irregular_implements/handler/ender_letter/EnderLetterHandler.kt
@@ -0,0 +1,12 @@
+package dev.aaronhowser.mods.irregular_implements.handler.ender_letter
+
+import net.minecraft.core.HolderLookup
+import net.minecraft.nbt.CompoundTag
+import net.minecraft.world.level.saveddata.SavedData
+
+class EnderLetterHandler : SavedData() {
+
+	override fun save(tag: CompoundTag, registries: HolderLookup.Provider): CompoundTag {
+		TODO("Not yet implemented")
+	}
+}
---
+++ src/main/kotlin/dev/aaronhowser/mods/irregular_implements/handler/ender_letter/EnderMailboxInventory.kt
@@ -0,0 +1,56 @@
+package dev.aaronhowser.mods.irregular_implements.handler.ender_letter
+
+import dev.aaronhowser.mods.irregular_implements.registry.ModItems
+import net.minecraft.core.NonNullList
+import net.minecraft.world.item.ItemStack
+import net.neoforged.neoforge.items.IItemHandler
+
+class EnderMailboxInventory(
+	val enderLetterHandler: EnderLetterHandler
+) : IItemHandler {
+
+	val enderLetters: NonNullList<ItemStack> = NonNullList.withSize(INVENTORY_SIZE, ItemStack.EMPTY)
+
+	override fun getStackInSlot(slot: Int): ItemStack {
+		return if (slot in 0 until INVENTORY_SIZE) {
+			enderLetters[slot]
+		} else {
+			ItemStack.EMPTY
+		}
+	}
+
+	override fun insertItem(slot: Int, stack: ItemStack, simulate: Boolean): ItemStack {
+		if (!isItemValid(slot, stack) || !getStackInSlot(slot).isEmpty) return stack
+
+		if (!simulate) {
+			enderLetters[slot] = stack
+		}
+
+		return ItemStack.EMPTY
+	}
+
+	override fun extractItem(slot: Int, amount: Int, simulate: Boolean): ItemStack {
+		if (amount == 0) return ItemStack.EMPTY
+
+		val existing = getStackInSlot(slot)
+		if (existing.isEmpty) return ItemStack.EMPTY
+
+		if (!simulate) {
+			enderLetters[slot] = ItemStack.EMPTY
+		}
+
+		return existing
+	}
+
+	override fun getSlots(): Int = enderLetters.size
+	override fun getSlotLimit(slot: Int): Int = 1
+
+	override fun isItemValid(slot: Int, stack: ItemStack): Boolean {
+		return stack.`is`(ModItems.ENDER_LETTER)
+	}
+
+	companion object {
+		const val INVENTORY_SIZE = 9
+	}
+
+}
