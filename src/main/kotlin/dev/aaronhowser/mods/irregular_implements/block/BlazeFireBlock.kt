9911cb9035e8a53650322a3f056465591e4ccc43
aaron <aaronhowser1@gmail.com>
2025-07-05 23:31:38 -0500

    more

--- src/main/kotlin/dev/aaronhowser/mods/irregular_implements/block/BlazeFireBlock.kt
+++ src/main/kotlin/dev/aaronhowser/mods/irregular_implements/block/BlazeFireBlock.kt
@@ -2,6 +2,8 @@ package dev.aaronhowser.mods.irregular_implements.block
 
 import dev.aaronhowser.mods.irregular_implements.BetterFire
 import net.minecraft.core.BlockPos
+import net.minecraft.core.Direction
+import net.minecraft.world.level.BlockGetter
 import net.minecraft.world.level.LevelAccessor
 import net.minecraft.world.level.block.Blocks
 import net.minecraft.world.level.block.FireBlock
@@ -24,6 +26,14 @@ class BlazeFireBlock : FireBlock(
 	@Suppress("OVERRIDE_DEPRECATION")
 	override fun getBurnOdds(state: BlockState): Int {
 		return super.getBurnOdds(state) * 3
+	}
+
+	override fun getFlammability(state: BlockState, level: BlockGetter, pos: BlockPos, direction: Direction): Int {
+		return super.getFlammability(state, level, pos, direction) * 3
+	}
+
+	override fun getFireSpreadSpeed(state: BlockState, level: BlockGetter, pos: BlockPos, direction: Direction): Int {
+		return super.getFireSpreadSpeed(state, level, pos, direction) * 3
 	}
 
 	override fun getStateWithAge(level: LevelAccessor, pos: BlockPos, age: Int): BlockState {
