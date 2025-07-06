347cf8ab9fb6090136233a4319843df7773c29f1
aaron <aaronhowser1@gmail.com>
2025-07-05 19:32:42 -0500

    progress

--- src/main/kotlin/dev/aaronhowser/mods/irregular_implements/menu/ender_letter/EnderLetterMenu.kt
+++ src/main/kotlin/dev/aaronhowser/mods/irregular_implements/menu/ender_letter/EnderLetterMenu.kt
@@ -2,6 +2,8 @@ package dev.aaronhowser.mods.irregular_implements.menu.ender_letter
 
 import dev.aaronhowser.mods.irregular_implements.item.component.EnderLetterContentsDataComponent
 import dev.aaronhowser.mods.irregular_implements.menu.HeldItemContainerMenu
+import dev.aaronhowser.mods.irregular_implements.menu.MenuWithStrings
+import dev.aaronhowser.mods.irregular_implements.registry.ModDataComponents
 import dev.aaronhowser.mods.irregular_implements.registry.ModItems
 import dev.aaronhowser.mods.irregular_implements.registry.ModMenuTypes
 import net.minecraft.world.entity.player.Inventory
@@ -10,6 +12,7 @@ import net.minecraft.world.item.ItemStack
 import net.neoforged.neoforge.capabilities.Capabilities
 import net.neoforged.neoforge.items.IItemHandler
 import net.neoforged.neoforge.items.SlotItemHandler
+import java.util.*
 
 class EnderLetterMenu(
 	containerId: Int,
@@ -19,7 +22,7 @@ class EnderLetterMenu(
 	ModMenuTypes.ENDER_LETTER.get(),
 	containerId,
 	playerInventory
-) {
+), MenuWithStrings {
 
 	private val itemHandler: IItemHandler? = getHeldItemStack().getCapability(Capabilities.ItemHandler.ITEM)
 
@@ -46,5 +49,27 @@ class EnderLetterMenu(
 
 	override fun stillValid(player: Player): Boolean {
 		return player.getItemInHand(hand).`is`(ModItems.ENDER_LETTER)
+	}
+
+	private var recipientName: String = ""
+
+	override fun receiveString(stringId: Int, string: String) {
+		when (stringId) {
+			RECIPIENT_STRING_ID -> setNewRecipient(string)
+		}
+	}
+
+	fun setNewRecipient(recipient: String) {
+		if (recipient == recipientName) return
+		this.recipientName = recipient
+
+		val oldComponent = getHeldItemStack().get(ModDataComponents.ENDER_LETTER_CONTENTS) ?: return
+		val newComponent = oldComponent.copy(recipient = Optional.of(recipient))
+
+		getHeldItemStack().set(ModDataComponents.ENDER_LETTER_CONTENTS, newComponent)
+	}
+
+	companion object {
+		const val RECIPIENT_STRING_ID = 0
 	}
 }
--- src/main/kotlin/dev/aaronhowser/mods/irregular_implements/menu/ender_letter/EnderLetterScreen.kt
+++ src/main/kotlin/dev/aaronhowser/mods/irregular_implements/menu/ender_letter/EnderLetterScreen.kt
@@ -1,7 +1,11 @@
 package dev.aaronhowser.mods.irregular_implements.menu.ender_letter
 
+import dev.aaronhowser.mods.irregular_implements.datagen.ModLanguageProvider
+import dev.aaronhowser.mods.irregular_implements.datagen.ModLanguageProvider.Companion.toComponent
 import dev.aaronhowser.mods.irregular_implements.menu.BaseScreen
 import dev.aaronhowser.mods.irregular_implements.menu.ScreenTextures
+import dev.aaronhowser.mods.irregular_implements.menu.ScreenWithStrings
+import net.minecraft.client.gui.components.EditBox
 import net.minecraft.network.chat.Component
 import net.minecraft.world.entity.player.Inventory
 
@@ -9,12 +13,31 @@ class EnderLetterScreen(
 	menu: EnderLetterMenu,
 	playerInventory: Inventory,
 	title: Component
-) : BaseScreen<EnderLetterMenu>(menu, playerInventory, title) {
+) : BaseScreen<EnderLetterMenu>(menu, playerInventory, title), ScreenWithStrings {
 
 	override val background: ScreenTextures.Background = ScreenTextures.Background.EnderLetter
 
+	private lateinit var recipientEditBox: EditBox
+
 	override fun baseInit() {
 		this.inventoryLabelY = this.imageHeight - 94
+
+		val screenWidth = this.rightPos - this.leftPos
+
+		recipientEditBox = EditBox(
+			this.font,
+			this.leftPos + 5,
+			this.bottomPos - 5 - 20,
+			screenWidth - 5 - 5,
+			20,
+			ModLanguageProvider.Tooltips.MESSAGE_REGEX.toComponent()
+		)
+	}
+
+	override fun receivedString(stringId: Int, string: String) {
+		when (stringId) {
+			EnderLetterMenu.RECIPIENT_STRING_ID -> recipientEditBox.value = string
+		}
 	}
 
 }
