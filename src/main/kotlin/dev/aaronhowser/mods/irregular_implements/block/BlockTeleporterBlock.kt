86724edc101d2c132b0cdcf7d5195d3742fc139e
aaron <aarornhowser1@gmail.com>
2025-07-21 17:50:41 -0500

    add interaction logic for BlockTeleporterBlock to open menu

--- src/main/kotlin/dev/aaronhowser/mods/irregular_implements/block/BlockTeleporterBlock.kt
+++ src/main/kotlin/dev/aaronhowser/mods/irregular_implements/block/BlockTeleporterBlock.kt
@@ -3,7 +3,10 @@ package dev.aaronhowser.mods.irregular_implements.block
 import dev.aaronhowser.mods.irregular_implements.block.block_entity.BlockTeleporterBlockEntity
 import net.minecraft.core.BlockPos
 import net.minecraft.core.Direction
+import net.minecraft.world.InteractionResult
+import net.minecraft.world.entity.player.Player
 import net.minecraft.world.item.context.BlockPlaceContext
+import net.minecraft.world.level.Level
 import net.minecraft.world.level.block.Block
 import net.minecraft.world.level.block.Blocks
 import net.minecraft.world.level.block.EntityBlock
@@ -11,6 +14,8 @@ import net.minecraft.world.level.block.entity.BlockEntity
 import net.minecraft.world.level.block.state.BlockState
 import net.minecraft.world.level.block.state.StateDefinition
 import net.minecraft.world.level.block.state.properties.BlockStateProperties
+import net.minecraft.world.level.block.state.properties.DirectionProperty
+import net.minecraft.world.phys.BlockHitResult
 
 class BlockTeleporterBlock : Block(Properties.ofFullCopy(Blocks.DISPENSER)), EntityBlock {
 
@@ -34,8 +39,21 @@ class BlockTeleporterBlock : Block(Properties.ofFullCopy(Blocks.DISPENSER)), Ent
 		return BlockTeleporterBlockEntity(pos, state)
 	}
 
+	override fun useWithoutItem(
+		pState: BlockState,
+		pLevel: Level,
+		pPos: BlockPos,
+		pPlayer: Player,
+		pHitResult: BlockHitResult
+	): InteractionResult {
+		val blockEntity = pLevel.getBlockEntity(pPos) as? BlockTeleporterBlockEntity
+			?: return InteractionResult.FAIL
+		pPlayer.openMenu(blockEntity)
+		return InteractionResult.SUCCESS
+	}
+
 	companion object {
-		val FACING = BlockStateProperties.FACING
+		val FACING: DirectionProperty = BlockStateProperties.FACING
 	}
 
 }
