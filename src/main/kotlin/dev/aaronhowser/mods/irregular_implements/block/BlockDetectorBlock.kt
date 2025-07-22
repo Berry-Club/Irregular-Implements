4cce58abf87ad4ad890b019b2735ea51039907f1
aaron <aarornhowser1@gmail.com>
2025-07-21 19:33:40 -0500

    cleanup

--- src/main/kotlin/dev/aaronhowser/mods/irregular_implements/block/BlockDetectorBlock.kt
+++ src/main/kotlin/dev/aaronhowser/mods/irregular_implements/block/BlockDetectorBlock.kt
@@ -1,12 +1,10 @@
 package dev.aaronhowser.mods.irregular_implements.block
 
 import dev.aaronhowser.mods.irregular_implements.block.block_entity.BlockDetectorBlockEntity
-import dev.aaronhowser.mods.irregular_implements.block.block_entity.BlockTeleporterBlockEntity
 import net.minecraft.core.BlockPos
 import net.minecraft.core.Direction
 import net.minecraft.world.InteractionResult
 import net.minecraft.world.entity.player.Player
-import net.minecraft.world.item.BlockItem
 import net.minecraft.world.item.context.BlockPlaceContext
 import net.minecraft.world.level.BlockGetter
 import net.minecraft.world.level.Level
--- src/main/kotlin/dev/aaronhowser/mods/irregular_implements/block/block_entity/BlockDetectorBlockEntity.kt
+++ src/main/kotlin/dev/aaronhowser/mods/irregular_implements/block/block_entity/BlockDetectorBlockEntity.kt
@@ -1,16 +1,9 @@
 package dev.aaronhowser.mods.irregular_implements.block.block_entity
 
 import dev.aaronhowser.mods.irregular_implements.block.BlockDetectorBlock
-import dev.aaronhowser.mods.irregular_implements.block.BlockTeleporterBlock
 import dev.aaronhowser.mods.irregular_implements.block.block_entity.base.ImprovedSimpleContainer
-import dev.aaronhowser.mods.irregular_implements.config.ServerConfig
-import dev.aaronhowser.mods.irregular_implements.datagen.tag.ModBlockTagsProvider
-import dev.aaronhowser.mods.irregular_implements.item.component.LocationDataComponent
 import dev.aaronhowser.mods.irregular_implements.menu.block_detector.BlockDetectorMenu
-import dev.aaronhowser.mods.irregular_implements.menu.block_teleporter.BlockTeleporterMenu
 import dev.aaronhowser.mods.irregular_implements.registry.ModBlockEntities
-import dev.aaronhowser.mods.irregular_implements.registry.ModDataComponents
-import dev.aaronhowser.mods.irregular_implements.util.OtherUtil.isTrue
 import net.minecraft.core.BlockPos
 import net.minecraft.core.HolderLookup
 import net.minecraft.nbt.CompoundTag
@@ -23,7 +16,6 @@ import net.minecraft.world.inventory.AbstractContainerMenu
 import net.minecraft.world.item.BlockItem
 import net.minecraft.world.level.block.entity.BlockEntity
 import net.minecraft.world.level.block.state.BlockState
-import kotlin.collections.getValue
 
 class BlockDetectorBlockEntity(
 	pos: BlockPos,
