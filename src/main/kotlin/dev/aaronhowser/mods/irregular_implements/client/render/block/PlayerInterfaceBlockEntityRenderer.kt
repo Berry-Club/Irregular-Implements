83151be20785088eeb68714d3142f0f16e6a122f
aaron <aarornhowser1@gmail.com>
2025-07-23 13:52:46 -0500

    enhance PlayerInterfaceBlockEntityRenderer to orient player skulls towards the local player

--- src/main/kotlin/dev/aaronhowser/mods/irregular_implements/client/render/block/PlayerInterfaceBlockEntityRenderer.kt
+++ src/main/kotlin/dev/aaronhowser/mods/irregular_implements/client/render/block/PlayerInterfaceBlockEntityRenderer.kt
@@ -1,14 +1,18 @@
 package dev.aaronhowser.mods.irregular_implements.client.render.block
 
 import com.mojang.blaze3d.vertex.PoseStack
+import com.mojang.math.Axis
 import dev.aaronhowser.mods.irregular_implements.block.block_entity.PlayerInterfaceBlockEntity
+import dev.aaronhowser.mods.irregular_implements.util.ClientUtil
 import net.minecraft.client.model.SkullModelBase
 import net.minecraft.client.renderer.MultiBufferSource
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderer
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider
 import net.minecraft.client.renderer.blockentity.SkullBlockRenderer
 import net.minecraft.core.component.DataComponents
+import net.minecraft.util.Mth
 import net.minecraft.world.level.block.SkullBlock
+import kotlin.math.atan2
 
 class PlayerInterfaceBlockEntityRenderer(
 	val context: BlockEntityRendererProvider.Context
@@ -30,6 +34,26 @@ class PlayerInterfaceBlockEntityRenderer(
 		val profile = skull.get(DataComponents.PROFILE) ?: return
 
 		poseStack.pushPose()
+
+		val localPlayer = ClientUtil.localPlayer
+		if (localPlayer != null) {
+			val deltaPos = blockEntity.blockPos
+				.above()
+				.center
+				.vectorTo(localPlayer.eyePosition)
+				.normalize()
+
+			poseStack.translate(0.5, 0.5, 0.5)
+
+			val yaw = atan2(deltaPos.x, deltaPos.z).toFloat() - 180 * Mth.DEG_TO_RAD
+			val pitch = atan2(deltaPos.y, deltaPos.horizontalDistance()).toFloat()
+
+			poseStack.mulPose(Axis.YP.rotation(yaw))
+			poseStack.mulPose(Axis.XP.rotation(pitch))
+
+			poseStack.translate(-0.5, -0.5, -0.5)
+		}
+
 		poseStack.translate(0f, 1.1f, 0f)
 
 		val model = skullModels[SkullBlock.Types.PLAYER] ?: return
