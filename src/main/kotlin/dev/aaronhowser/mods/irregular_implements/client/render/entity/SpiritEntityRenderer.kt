18984ec8849ace668630e58f7bb11abed93c0bd9
aaron <aarornhowser1@gmail.com>
2025-07-23 12:17:11 -0500

    add SpiritGelLayer to enhance Spirit entity rendering

--- src/main/kotlin/dev/aaronhowser/mods/irregular_implements/client/render/entity/SpiritEntityRenderer.kt
+++ src/main/kotlin/dev/aaronhowser/mods/irregular_implements/client/render/entity/SpiritEntityRenderer.kt
@@ -1,11 +1,17 @@
 package dev.aaronhowser.mods.irregular_implements.client.render.entity
 
+import com.mojang.blaze3d.vertex.PoseStack
 import dev.aaronhowser.mods.irregular_implements.entity.SpiritEntity
 import dev.aaronhowser.mods.irregular_implements.util.OtherUtil
 import net.minecraft.client.model.SlimeModel
+import net.minecraft.client.model.geom.EntityModelSet
 import net.minecraft.client.model.geom.ModelLayers
+import net.minecraft.client.renderer.MultiBufferSource
+import net.minecraft.client.renderer.RenderType
 import net.minecraft.client.renderer.entity.EntityRendererProvider
 import net.minecraft.client.renderer.entity.MobRenderer
+import net.minecraft.client.renderer.entity.RenderLayerParent
+import net.minecraft.client.renderer.entity.layers.RenderLayer
 import net.minecraft.resources.ResourceLocation
 
 class SpiritEntityRenderer(
@@ -16,12 +22,48 @@ class SpiritEntityRenderer(
 	0.25f
 ) {
 
+	init {
+		addLayer(SpiritGelLayer(this, context.modelSet))
+	}
+
 	override fun getTextureLocation(entity: SpiritEntity): ResourceLocation {
 		return TEXTURE
 	}
 
 	companion object {
 		val TEXTURE = OtherUtil.modResource("textures/entity/spirit.png")
+	}
+
+	class SpiritGelLayer(
+		renderer: RenderLayerParent<SpiritEntity, SlimeModel<SpiritEntity>>,
+		modelSet: EntityModelSet
+	) : RenderLayer<SpiritEntity, SlimeModel<SpiritEntity>>(renderer) {
+
+		val model = SlimeModel<SpiritEntity>(modelSet.bakeLayer(ModelLayers.SLIME_OUTER))
+
+		override fun render(
+			poseStack: PoseStack,
+			bufferSource: MultiBufferSource,
+			packedLight: Int,
+			livingEntity: SpiritEntity,
+			limbSwing: Float,
+			limbSwingAmount: Float,
+			partialTick: Float,
+			ageInTicks: Float,
+			netHeadYaw: Float,
+			headPitch: Float
+		) {
+			if (livingEntity.isInvisible) return
+			val consumer = bufferSource.getBuffer(RenderType.entityTranslucent(getTextureLocation(livingEntity)))
+
+			parentModel.copyPropertiesTo(model)
+
+			model.prepareMobModel(livingEntity, limbSwing, limbSwingAmount, partialTick)
+			model.setupAnim(livingEntity, limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch)
+			model.renderToBuffer(poseStack, consumer, packedLight, getOverlayCoords(livingEntity, 0.0f))
+
+		}
+
 	}
 
 }
