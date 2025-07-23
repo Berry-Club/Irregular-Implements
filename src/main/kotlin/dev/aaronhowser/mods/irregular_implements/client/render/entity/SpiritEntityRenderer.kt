c5ca909e2de49a5dd793fe41e501fd32124f8137
aaron <aarornhowser1@gmail.com>
2025-07-23 12:08:34 -0500

    it renders

--- src/main/kotlin/dev/aaronhowser/mods/irregular_implements/client/render/entity/SpiritEntityRenderer.kt
+++ src/main/kotlin/dev/aaronhowser/mods/irregular_implements/client/render/entity/SpiritEntityRenderer.kt
@@ -2,11 +2,19 @@ package dev.aaronhowser.mods.irregular_implements.client.render.entity
 
 import dev.aaronhowser.mods.irregular_implements.entity.SpiritEntity
 import dev.aaronhowser.mods.irregular_implements.util.OtherUtil
-import net.minecraft.client.renderer.entity.EntityRenderer
+import net.minecraft.client.model.SlimeModel
+import net.minecraft.client.model.geom.ModelLayers
 import net.minecraft.client.renderer.entity.EntityRendererProvider
+import net.minecraft.client.renderer.entity.MobRenderer
 import net.minecraft.resources.ResourceLocation
 
-class SpiritEntityRenderer(context: EntityRendererProvider.Context) : EntityRenderer<SpiritEntity>(context) {
+class SpiritEntityRenderer(
+	context: EntityRendererProvider.Context
+) : MobRenderer<SpiritEntity, SlimeModel<SpiritEntity>>(
+	context,
+	SlimeModel(context.bakeLayer(ModelLayers.SLIME)),
+	0.25f
+) {
 
 	override fun getTextureLocation(entity: SpiritEntity): ResourceLocation {
 		return TEXTURE
