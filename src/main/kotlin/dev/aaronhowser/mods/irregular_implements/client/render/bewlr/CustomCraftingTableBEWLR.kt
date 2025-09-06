6216df6639a36bbe9990011fc89823f8d69eb024
aaron <aaronhowser1@gmail.com>
2025-09-06 17:16:37 -0500

    add rendering methods for crafting table and base item in CustomCraftingTableBEWLR

--- src/main/kotlin/dev/aaronhowser/mods/irregular_implements/client/render/bewlr/CustomCraftingTableBEWLR.kt
+++ src/main/kotlin/dev/aaronhowser/mods/irregular_implements/client/render/bewlr/CustomCraftingTableBEWLR.kt
@@ -1,10 +1,17 @@
 package dev.aaronhowser.mods.irregular_implements.client.render.bewlr
 
 import com.mojang.blaze3d.vertex.PoseStack
+import dev.aaronhowser.mods.irregular_implements.client.render.block_entity.CustomCraftingTableBlockEntityRenderer.Companion.BOTTOM
+import dev.aaronhowser.mods.irregular_implements.client.render.block_entity.CustomCraftingTableBlockEntityRenderer.Companion.SAW_AND_HAMMER
+import dev.aaronhowser.mods.irregular_implements.client.render.block_entity.CustomCraftingTableBlockEntityRenderer.Companion.SCISSORS
+import dev.aaronhowser.mods.irregular_implements.client.render.block_entity.CustomCraftingTableBlockEntityRenderer.Companion.TOP
 import dev.aaronhowser.mods.irregular_implements.registry.ModDataComponents
+import dev.aaronhowser.mods.irregular_implements.util.RenderUtil
 import net.minecraft.client.Minecraft
 import net.minecraft.client.renderer.BlockEntityWithoutLevelRenderer
 import net.minecraft.client.renderer.MultiBufferSource
+import net.minecraft.client.renderer.RenderType
+import net.minecraft.world.item.Item
 import net.minecraft.world.item.ItemDisplayContext
 import net.minecraft.world.item.ItemStack
 import net.minecraft.world.item.Items
@@ -24,13 +31,47 @@ class CustomCraftingTableBEWLR : BlockEntityWithoutLevelRenderer(
 		packedLight: Int,
 		packedOverlay: Int
 	) {
+		renderCraftingTable(poseStack)
+
+		renderBaseItem(
+			poseStack,
+			buffer,
+			displayContext,
+			itemToRender = stack.get(ModDataComponents.BLOCK)?.asItem() ?: Items.OAK_PLANKS,
+			packedLight,
+			packedOverlay,
+		)
+	}
+
+	private fun renderCraftingTable(
+		poseStack: PoseStack
+	) {
+		poseStack.pushPose()
+
+		RenderUtil.renderTexturedCube(
+			poseStack,
+			RenderType.cutout(),
+			TOP, BOTTOM,
+			SAW_AND_HAMMER, SCISSORS, SCISSORS, SAW_AND_HAMMER,
+		)
+
+		poseStack.popPose()
+	}
+
+	private fun renderBaseItem(
+		poseStack: PoseStack,
+		buffer: MultiBufferSource,
+		displayContext: ItemDisplayContext,
+		itemToRender: Item,
+		packedLight: Int,
+		packedOverlay: Int,
+	) {
 		poseStack.pushPose()
 
 		poseStack.scale(0.999f, 0.999f, 0.999f)
 		poseStack.translate(0.0005f, 0.0005f, 0.0005f)
 		poseStack.translate(0.5, 0.5, 0.5)
 
-		val itemToRender = stack.get(ModDataComponents.BLOCK)?.asItem() ?: Items.OAK_PLANKS
 		val itemRenderer = Minecraft.getInstance().itemRenderer
 
 		itemRenderer.renderStatic(
