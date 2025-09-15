2828c0b1763e3b29f6da7e93fe18953212825eac
aaron <aaronhowser1@gmail.com>
2025-09-15 07:06:13 -0500

    starting on golden compass

---
+++ src/main/kotlin/dev/aaronhowser/mods/irregular_implements/item/GoldenCompassItem.kt
@@ -0,0 +1,16 @@
+package dev.aaronhowser.mods.irregular_implements.item
+
+import dev.aaronhowser.mods.irregular_implements.util.OtherUtil
+import net.minecraft.resources.ResourceLocation
+import net.minecraft.world.item.Item
+
+class GoldenCompassItem(properties: Properties) : Item(properties) {
+
+	companion object {
+		val DEFAULT_PROPERTIES: Properties = Properties().stacksTo(1)
+
+		val ANGLE: ResourceLocation = OtherUtil.modResource("golden_compass_angle")
+		private const val DEFAULT = 0.52f
+	}
+
+}
