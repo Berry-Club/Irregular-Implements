db1d2d0ce9ad34c883de0695bc696ab30c5df7f7
aaron <aaronhowser1@gmail.com>
2025-09-27 22:09:13 -0500

    add entity filtering functionality to EntityDetectorBlockEntity

--- src/main/kotlin/dev/aaronhowser/mods/irregular_implements/block/block_entity/EntityDetectorBlockEntity.kt
+++ src/main/kotlin/dev/aaronhowser/mods/irregular_implements/block/block_entity/EntityDetectorBlockEntity.kt
@@ -1,18 +1,43 @@
 package dev.aaronhowser.mods.irregular_implements.block.block_entity
 
 import dev.aaronhowser.mods.irregular_implements.registry.ModBlockEntities
+import dev.aaronhowser.mods.irregular_implements.registry.ModDataComponents
 import dev.aaronhowser.mods.irregular_implements.util.OtherUtil.isServerSide
 import net.minecraft.core.BlockPos
+import net.minecraft.world.entity.Entity
+import net.minecraft.world.entity.LivingEntity
+import net.minecraft.world.entity.animal.Animal
+import net.minecraft.world.entity.item.ItemEntity
+import net.minecraft.world.entity.monster.Monster
+import net.minecraft.world.entity.player.Player
+import net.minecraft.world.item.ItemStack
 import net.minecraft.world.level.Level
 import net.minecraft.world.level.block.entity.BlockEntity
 import net.minecraft.world.level.block.state.BlockState
+import net.minecraft.world.phys.AABB
 
 class EntityDetectorBlockEntity(
 	pos: BlockPos,
 	blockState: BlockState
 ) : BlockEntity(ModBlockEntities.ENTITY_DETECTOR.get(), pos, blockState) {
 
+	private var filter: Filter = Filter.ALL
+
+	private var xRadius: Int = 0
+	private var yRadius: Int = 0
+	private var zRadius: Int = 0
+
+	private var inverted: Boolean = false
+
+
 	fun tick() {
+		val level = level ?: return
+
+		val entities = level.getEntities(
+			null,
+			AABB(blockPos).inflate(xRadius.toDouble(), yRadius.toDouble(), zRadius.toDouble()),
+			filter.predicate(ItemStack.EMPTY)
+		)
 
 	}
 
@@ -24,6 +49,28 @@ class EntityDetectorBlockEntity(
 			blockEntity: EntityDetectorBlockEntity
 		) {
 			if (level.isServerSide) blockEntity.tick()
+		}
+	}
+
+	enum class Filter(
+		val entityClass: Class<out Entity>?,
+		val unlocalizedName: String
+	) {
+		ALL(Entity::class.java, "all"),
+		LIVING(LivingEntity::class.java, "living"),
+		ANIMAL(Animal::class.java, "animal"),
+		MONSTER(Monster::class.java, "monster"),
+		PLAYER(Player::class.java, "player"),
+		ITEM(ItemEntity::class.java, "item"),
+		CUSTOM(null, "custom")
+
+		;
+
+		fun predicate(filterStack: ItemStack): (Entity) -> Boolean {
+			return { entity ->
+				entityClass?.isAssignableFrom(entity.javaClass)
+					?: (entity.type == filterStack.get(ModDataComponents.ENTITY_TYPE))
+			}
 		}
 	}
 
