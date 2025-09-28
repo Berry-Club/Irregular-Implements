beb9c052cd780e9c8c806cdda6e3286e96ba233e
aaron <aaronhowser1@gmail.com>
2025-09-27 23:30:11 -0500

    open menu

--- src/main/kotlin/dev/aaronhowser/mods/irregular_implements/block/EntityDetectorBlock.kt
+++ src/main/kotlin/dev/aaronhowser/mods/irregular_implements/block/EntityDetectorBlock.kt
@@ -4,6 +4,9 @@ import dev.aaronhowser.mods.irregular_implements.block.block_entity.EntityDetect
 import dev.aaronhowser.mods.irregular_implements.registry.ModBlockEntities
 import net.minecraft.core.BlockPos
 import net.minecraft.core.Direction
+import net.minecraft.world.InteractionResult
+import net.minecraft.world.MenuProvider
+import net.minecraft.world.entity.player.Player
 import net.minecraft.world.level.BlockGetter
 import net.minecraft.world.level.Level
 import net.minecraft.world.level.block.BaseEntityBlock
@@ -14,6 +17,7 @@ import net.minecraft.world.level.block.entity.BlockEntity
 import net.minecraft.world.level.block.entity.BlockEntityTicker
 import net.minecraft.world.level.block.entity.BlockEntityType
 import net.minecraft.world.level.block.state.BlockState
+import net.minecraft.world.phys.BlockHitResult
 
 class EntityDetectorBlock : Block(Properties.ofFullCopy(Blocks.STONE)), EntityBlock {
 
@@ -42,6 +46,17 @@ class EntityDetectorBlock : Block(Properties.ofFullCopy(Blocks.STONE)), EntityBl
 
 	override fun <T : BlockEntity?> getTicker(level: Level, state: BlockState, blockEntityType: BlockEntityType<T>): BlockEntityTicker<T>? {
 		return BaseEntityBlock.createTickerHelper(blockEntityType, ModBlockEntities.ENTITY_DETECTOR.get(), EntityDetectorBlockEntity::tick)
+	}
+
+	override fun useWithoutItem(state: BlockState, level: Level, pos: BlockPos, player: Player, hitResult: BlockHitResult): InteractionResult {
+		val blockEntity = level.getBlockEntity(pos)
+
+		if (blockEntity is MenuProvider) {
+			player.openMenu(blockEntity)
+			return InteractionResult.SUCCESS
+		}
+
+		return InteractionResult.PASS
 	}
 
 }
