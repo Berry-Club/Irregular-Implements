32961d4449dd5dd5d5f0dd0184a70575a26d4824
aaron <aaronhowser1@gmail.com>
2025-09-27 22:35:05 -0500

    add redstone signal functionality to EntityDetectorBlock

--- src/main/kotlin/dev/aaronhowser/mods/irregular_implements/block/EntityDetectorBlock.kt
+++ src/main/kotlin/dev/aaronhowser/mods/irregular_implements/block/EntityDetectorBlock.kt
@@ -3,6 +3,8 @@ package dev.aaronhowser.mods.irregular_implements.block
 import dev.aaronhowser.mods.irregular_implements.block.block_entity.EntityDetectorBlockEntity
 import dev.aaronhowser.mods.irregular_implements.registry.ModBlockEntities
 import net.minecraft.core.BlockPos
+import net.minecraft.core.Direction
+import net.minecraft.world.level.BlockGetter
 import net.minecraft.world.level.Level
 import net.minecraft.world.level.block.BaseEntityBlock
 import net.minecraft.world.level.block.Block
@@ -14,6 +16,25 @@ import net.minecraft.world.level.block.entity.BlockEntityType
 import net.minecraft.world.level.block.state.BlockState
 
 class EntityDetectorBlock : Block(Properties.ofFullCopy(Blocks.STONE)), EntityBlock {
+
+	override fun canConnectRedstone(state: BlockState, level: BlockGetter, pos: BlockPos, direction: Direction?): Boolean {
+		return true
+	}
+
+	override fun isSignalSource(state: BlockState): Boolean {
+		return true
+	}
+
+	override fun getDirectSignal(state: BlockState, level: BlockGetter, pos: BlockPos, direction: Direction): Int {
+		val be = level.getBlockEntity(pos)
+		return if (be is EntityDetectorBlockEntity && be.isActive) {
+			15
+		} else 0
+	}
+
+	override fun getSignal(state: BlockState, level: BlockGetter, pos: BlockPos, direction: Direction): Int {
+		return getDirectSignal(state, level, pos, direction)
+	}
 
 	override fun newBlockEntity(pos: BlockPos, state: BlockState): BlockEntity {
 		return EntityDetectorBlockEntity(pos, state)
