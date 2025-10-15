042fee631dc23ef73538661a9f252cafe7e5dce5
aaron <aaronhowser1@gmail.com>
2025-10-15 15:24:49 -0500

    feat: implement Advanced Redstone Interface with improved container handling and linked position updates

--- src/main/kotlin/dev/aaronhowser/mods/irregular_implements/block/block_entity/RedstoneInterfaceAdvancedBlockEntity.kt
+++ src/main/kotlin/dev/aaronhowser/mods/irregular_implements/block/block_entity/RedstoneInterfaceAdvancedBlockEntity.kt
@@ -1,7 +1,9 @@
 package dev.aaronhowser.mods.irregular_implements.block.block_entity
 
+import dev.aaronhowser.mods.irregular_implements.block.block_entity.base.ImprovedSimpleContainer
 import dev.aaronhowser.mods.irregular_implements.block.block_entity.base.RedstoneInterfaceBlockEntity
 import dev.aaronhowser.mods.irregular_implements.registry.ModBlockEntities
+import dev.aaronhowser.mods.irregular_implements.registry.ModDataComponents
 import net.minecraft.core.BlockPos
 import net.minecraft.world.level.block.state.BlockState
 
@@ -9,4 +11,31 @@ class RedstoneInterfaceAdvancedBlockEntity(
 	pPos: BlockPos,
 	pBlockState: BlockState
 ) : RedstoneInterfaceBlockEntity(ModBlockEntities.ADVANCED_REDSTONE_INTERFACE.get(), pPos, pBlockState) {
+
+	val container = ImprovedSimpleContainer(this, CONTAINER_SIZE)
+
+	fun getLinkedPositions(): List<BlockPos> {
+		val positions = mutableListOf<BlockPos>()
+
+		for (stack in container.items) {
+			val pos = stack.get(ModDataComponents.GLOBAL_POS) ?: continue
+			if (pos.dimension == this.level?.dimension()) {
+				positions.add(pos.pos)
+			}
+		}
+
+		return positions
+	}
+
+	override fun updateTargets() {
+		val positions = getLinkedPositions()
+		for (pos in positions) {
+			updatePos(pos)
+		}
+	}
+
+	companion object {
+		const val CONTAINER_SIZE = 9
+	}
+
 }
