948465d7459b3d19cebdfe36ee507f97d8437bd6
aaron <aaronhowser1@gmail.com>
2025-10-15 15:47:39 -0500

    renderer

--- src/main/kotlin/dev/aaronhowser/mods/irregular_implements/block/block_entity/RedstoneInterfaceAdvancedBlockEntity.kt
+++ src/main/kotlin/dev/aaronhowser/mods/irregular_implements/block/block_entity/RedstoneInterfaceAdvancedBlockEntity.kt
@@ -2,8 +2,12 @@ package dev.aaronhowser.mods.irregular_implements.block.block_entity
 
 import dev.aaronhowser.mods.irregular_implements.block.block_entity.base.ImprovedSimpleContainer
 import dev.aaronhowser.mods.irregular_implements.block.block_entity.base.RedstoneInterfaceBlockEntity
+import dev.aaronhowser.mods.irregular_implements.client.render.CubeIndicatorRenderer
+import dev.aaronhowser.mods.irregular_implements.client.render.LineIndicatorRenderer
 import dev.aaronhowser.mods.irregular_implements.registry.ModBlockEntityTypes
 import dev.aaronhowser.mods.irregular_implements.registry.ModDataComponents
+import dev.aaronhowser.mods.irregular_implements.registry.ModItems
+import dev.aaronhowser.mods.irregular_implements.util.ClientUtil
 import net.minecraft.core.BlockPos
 import net.minecraft.world.level.block.state.BlockState
 
@@ -31,6 +35,41 @@ class RedstoneInterfaceAdvancedBlockEntity(
 		val positions = getLinkedPositions()
 		for (pos in positions) {
 			updatePos(pos)
+		}
+	}
+
+	override fun clientTick() {
+		val level = this.level ?: return
+		if (!level.isClientSide) return
+
+		val player = ClientUtil.localPlayer ?: return
+		if (!player.isHolding(ModItems.REDSTONE_TOOL.get())) return
+
+		val links = getLinkedPositions()
+
+		if (links.isEmpty()) return
+
+		CubeIndicatorRenderer.addIndicator(
+			this.blockPos,
+			2,
+			0x32FF0000,
+			size = 0.5f
+		)
+
+		for (link in links) {
+			LineIndicatorRenderer.addIndicator(
+				start = this.blockPos.center,
+				end = link.center,
+				duration = 2,
+				color = 0xFFFF0000.toInt()
+			)
+
+			CubeIndicatorRenderer.addIndicator(
+				target = link,
+				duration = 2,
+				color = 0x320000FF,
+				size = 0.5f
+			)
 		}
 	}
 
