6c90e4b8c80da1e2c128411e57634de4a382c273
aaron <aaronhowser1@gmail.com>
2025-10-24 05:22:48 -0500

    effects

--- src/main/kotlin/dev/aaronhowser/mods/irregular_implements/registry/ModEffects.kt
+++ src/main/kotlin/dev/aaronhowser/mods/irregular_implements/registry/ModEffects.kt
@@ -5,8 +5,8 @@ import dev.aaronhowser.mods.irregular_implements.effect.CollapseEffect
 import dev.aaronhowser.mods.irregular_implements.effect.ImbueEffect
 import net.minecraft.core.registries.Registries
 import net.minecraft.world.effect.MobEffect
-import net.neoforged.neoforge.registries.DeferredHolder
-import net.neoforged.neoforge.registries.DeferredRegister
+import net.minecraftforge.registries.DeferredRegister
+import net.minecraftforge.registries.RegistryObject
 import java.awt.Color
 import java.util.function.Supplier
 
@@ -15,21 +15,29 @@ object ModEffects {
 	val EFFECT_REGISTRY: DeferredRegister<MobEffect> =
 		DeferredRegister.create(Registries.MOB_EFFECT, IrregularImplements.ID)
 
-	val FIRE_IMBUE: DeferredHolder<MobEffect, ImbueEffect> =
-		EFFECT_REGISTRY.register("imbue_fire", Supplier { ImbueEffect(Color.ORANGE.rgb) })
-	val POISON_IMBUE: DeferredHolder<MobEffect, ImbueEffect> =
-		EFFECT_REGISTRY.register("imbue_poison", Supplier { ImbueEffect(Color.GREEN.darker().rgb) })
-	val EXPERIENCE_IMBUE: DeferredHolder<MobEffect, ImbueEffect> =
-		EFFECT_REGISTRY.register("imbue_experience", Supplier { ImbueEffect(Color.YELLOW.rgb) })
-	val WITHER_IMBUE: DeferredHolder<MobEffect, ImbueEffect> =
-		EFFECT_REGISTRY.register("imbue_wither", Supplier { ImbueEffect(Color.BLACK.brighter().rgb) })
-	val SPECTRE_IMBUE: DeferredHolder<MobEffect, ImbueEffect> =    //FIXME: No texture
-		EFFECT_REGISTRY.register("imbue_spectre", Supplier { ImbueEffect(0xBFEFFF) })
-	val COLLAPSE_IMBUE: DeferredHolder<MobEffect, ImbueEffect> =
-		EFFECT_REGISTRY.register("imbue_collapse", Supplier { ImbueEffect(Color.PINK.rgb) })
+	val FIRE_IMBUE: RegistryObject<ImbueEffect> =
+		imbue("fire", Color.ORANGE.rgb)
+	val POISON_IMBUE: RegistryObject<ImbueEffect> =
+		imbue("poison", Color.GREEN.darker().rgb)
+	val EXPERIENCE_IMBUE: RegistryObject<ImbueEffect> =
+		imbue("experience", Color.YELLOW.rgb)
+	val WITHER_IMBUE: RegistryObject<ImbueEffect> =
+		imbue("wither", Color.BLACK.brighter().rgb)
+	val SPECTRE_IMBUE: RegistryObject<ImbueEffect> =    //FIXME: No texture
+		imbue("spectre", 0xBFEFFF)
+	val COLLAPSE_IMBUE: RegistryObject<ImbueEffect> =
+		imbue("collapse", Color.PINK.rgb)
 
 	@JvmField
-	val COLLAPSE: DeferredHolder<MobEffect, CollapseEffect> =
+	val COLLAPSE: RegistryObject<CollapseEffect> =
 		EFFECT_REGISTRY.register("collapse", Supplier { CollapseEffect() })
+
+	private fun <T : MobEffect> register(name: String, supplier: Supplier<T>): RegistryObject<T> {
+		return EFFECT_REGISTRY.register(name, supplier)
+	}
+
+	private fun imbue(name: String, color: Int): RegistryObject<ImbueEffect> {
+		return register("imbue_$name") { ImbueEffect(color) }
+	}
 
 }
