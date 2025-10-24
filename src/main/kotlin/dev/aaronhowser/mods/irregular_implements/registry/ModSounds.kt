baa5e9a8e673f00bf49d0710fd6ee6e8b39e1d9e
aaron <aaronhowser1@gmail.com>
2025-10-24 05:28:22 -0500

    zounds

--- src/main/kotlin/dev/aaronhowser/mods/irregular_implements/registry/ModSounds.kt
+++ src/main/kotlin/dev/aaronhowser/mods/irregular_implements/registry/ModSounds.kt
@@ -2,43 +2,43 @@ package dev.aaronhowser.mods.irregular_implements.registry
 
 import dev.aaronhowser.mods.irregular_implements.IrregularImplements
 import dev.aaronhowser.mods.irregular_implements.util.OtherUtil
-import net.minecraft.core.registries.BuiltInRegistries
+import net.minecraft.core.registries.Registries
 import net.minecraft.sounds.SoundEvent
-import net.neoforged.neoforge.registries.DeferredHolder
-import net.neoforged.neoforge.registries.DeferredRegister
+import net.minecraftforge.registries.DeferredRegister
+import net.minecraftforge.registries.RegistryObject
 import java.util.function.Supplier
 
 object ModSounds {
 
 	val SOUND_EVENT_REGISTRY: DeferredRegister<SoundEvent> =
-		DeferredRegister.create(BuiltInRegistries.SOUND_EVENT, IrregularImplements.ID)
+		DeferredRegister.create(Registries.SOUND_EVENT, IrregularImplements.ID)
 
-	val FART: DeferredHolder<SoundEvent, SoundEvent> =
+	val FART: RegistryObject<SoundEvent> =
 		SOUND_EVENT_REGISTRY.register("fart", Supplier {
 			SoundEvent.createVariableRangeEvent(OtherUtil.modResource("fart"))
 		})
 
-	val EXTRACTINATOR: DeferredHolder<SoundEvent, SoundEvent> =
+	val EXTRACTINATOR: RegistryObject<SoundEvent> =
 		SOUND_EVENT_REGISTRY.register("extractinator", Supplier {
 			SoundEvent.createVariableRangeEvent(OtherUtil.modResource("extractinator"))
 		})
 
-	val NOTIFICATION: DeferredHolder<SoundEvent, SoundEvent> =
+	val NOTIFICATION: RegistryObject<SoundEvent> =
 		SOUND_EVENT_REGISTRY.register("notification", Supplier {
 			SoundEvent.createVariableRangeEvent(OtherUtil.modResource("notification"))
 		})
 
-	val REVIVE: DeferredHolder<SoundEvent, SoundEvent> =
+	val REVIVE: RegistryObject<SoundEvent> =
 		SOUND_EVENT_REGISTRY.register("revive", Supplier {
 			SoundEvent.createVariableRangeEvent(OtherUtil.modResource("revive"))
 		})
 
-	val TELEPORT: DeferredHolder<SoundEvent, SoundEvent> =
+	val TELEPORT: RegistryObject<SoundEvent> =
 		SOUND_EVENT_REGISTRY.register("teleport", Supplier {
 			SoundEvent.createVariableRangeEvent(OtherUtil.modResource("teleport"))
 		})
 
-	val WHITE_STONE_ACTIVATE: DeferredHolder<SoundEvent, SoundEvent> =
+	val WHITE_STONE_ACTIVATE: RegistryObject<SoundEvent> =
 		SOUND_EVENT_REGISTRY.register("white_stone_activate", Supplier {
 			SoundEvent.createVariableRangeEvent(OtherUtil.modResource("white_stone_activate"))
 		})
