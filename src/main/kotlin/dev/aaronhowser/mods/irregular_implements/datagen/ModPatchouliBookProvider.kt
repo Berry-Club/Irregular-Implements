39b126997d72c104b0b4362dac89e2696aa61034
aaron <aaronhowser1@gmail.com>
2025-10-25 23:01:14 -0500

    Add Stable Ender Pearl entry to Patchouli book and update global loot modifiers

--- src/main/kotlin/dev/aaronhowser/mods/irregular_implements/datagen/ModPatchouliBookProvider.kt
+++ src/main/kotlin/dev/aaronhowser/mods/irregular_implements/datagen/ModPatchouliBookProvider.kt
@@ -7,6 +7,9 @@ import dev.aaronhowser.mods.irregular_implements.registry.ModItems
 import dev.aaronhowser.mods.patchoulidatagen.book_element.PatchouliBook
 import dev.aaronhowser.mods.patchoulidatagen.book_element.PatchouliBookCategory
 import dev.aaronhowser.mods.patchoulidatagen.book_element.PatchouliBookElement
+import dev.aaronhowser.mods.patchoulidatagen.book_element.PatchouliBookEntry
+import dev.aaronhowser.mods.patchoulidatagen.page.defaults.SpotlightPage
+import dev.aaronhowser.mods.patchoulidatagen.page.defaults.TextPage
 import dev.aaronhowser.mods.patchoulidatagen.provider.PatchouliBookProvider
 import net.minecraft.data.DataGenerator
 import java.util.function.Consumer
@@ -17,7 +20,6 @@ class ModPatchouliBookProvider(
 ) : PatchouliBookProvider(generator, bookName, IrregularImplements.MOD_ID) {
 
 	override fun buildPages(consumer: Consumer<PatchouliBookElement>) {
-
 		val book = PatchouliBook.builder()
 			.setBookText(
 				bookModId = IrregularImplements.MOD_ID,
@@ -28,7 +30,12 @@ class ModPatchouliBookProvider(
 			.hideProgress()
 			.save(consumer)
 
-		val itemsCategory = PatchouliBookCategory.builder()
+		items(consumer, book)
+		blocks(consumer, book)
+	}
+
+	private fun items(consumer: Consumer<PatchouliBookElement>, book: PatchouliBook) {
+		val category = PatchouliBookCategory.builder()
 			.book(book)
 			.setDisplay(
 				name = "Items",
@@ -37,6 +44,28 @@ class ModPatchouliBookProvider(
 			)
 			.save(consumer, "items")
 
+		PatchouliBookEntry.builder()
+			.category(category)
+			.display(
+				entryName = "Stable Ender Pearl",
+				icon = ModItems.STABLE_ENDER_PEARL
+			)
+			.addPage(
+				TextPage.builder()
+					.title("Stable Ender Pearl")
+					.text("Test")
+					.build()
+			)
+			.addPage(
+				SpotlightPage.builder()
+					.item(ModItems.STABLE_ENDER_PEARL)
+					.build()
+			)
+			.save(consumer, "stable_ender_pearl")
+
+	}
+
+	private fun blocks(consumer: Consumer<PatchouliBookElement>, book: PatchouliBook) {
 		val blocksCategory = PatchouliBookCategory.builder()
 			.book(book)
 			.setDisplay(
@@ -45,7 +74,6 @@ class ModPatchouliBookProvider(
 				icon = ModBlocks.PLAYER_INTERFACE
 			)
 			.save(consumer, "blocks")
-
 	}
 
 }
