59e6002c08b5adcfa32c1412b68b14ccdc37c052
aaron <aaronhowser1@gmail.com>
2025-12-21 18:45:46 -0600

    feat: add animation for SidedLampBlock when lit

--- src/main/kotlin/dev/aaronhowser/mods/irregular_implements/block/SidedLampBlock.kt
+++ src/main/kotlin/dev/aaronhowser/mods/irregular_implements/block/SidedLampBlock.kt
@@ -2,7 +2,9 @@ package dev.aaronhowser.mods.irregular_implements.block
 
 import net.minecraft.core.BlockPos
 import net.minecraft.server.level.ServerLevel
+import net.minecraft.util.RandomSource
 import net.minecraft.world.level.BlockGetter
+import net.minecraft.world.level.Level
 import net.minecraft.world.level.block.Blocks
 import net.minecraft.world.level.block.RedstoneLampBlock
 import net.minecraft.world.level.block.state.BlockState
@@ -18,6 +20,12 @@ class SidedLampBlock(
 		}
 
 		return 0
+	}
+
+	override fun animateTick(state: BlockState, level: Level, pos: BlockPos, random: RandomSource) {
+		if (state.getValue(LIT)) {
+			level.lightEngine.checkBlock(pos)
+		}
 	}
 
 }
