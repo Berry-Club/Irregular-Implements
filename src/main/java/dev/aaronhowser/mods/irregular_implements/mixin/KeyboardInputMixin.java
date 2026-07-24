25980e08af8f83f6732e65d51b4c66dac0986ee9
aaron <aaronhowser1@gmail.com>
2026-07-24 10:44:09 -0500

    mixin

--- src/main/java/dev/aaronhowser/mods/irregular_implements/mixin/KeyboardInputMixin.java
+++ src/main/java/dev/aaronhowser/mods/irregular_implements/mixin/KeyboardInputMixin.java
@@ -1,20 +1,33 @@
 package dev.aaronhowser.mods.irregular_implements.mixin;
 
-import dev.aaronhowser.mods.irregular_implements.KeyboardInputFunctions;
+import dev.aaronhowser.mods.aaron.client.AaronClientUtil;
+import dev.aaronhowser.mods.irregular_implements.config.ClientConfig;
+import dev.aaronhowser.mods.irregular_implements.registry.ModMobEffects;
 import net.minecraft.client.player.KeyboardInput;
+import net.minecraft.world.entity.player.Player;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(KeyboardInput.class)
-abstract public class KeyboardInputMixin implements KeyboardInputFunctions {
+abstract public class KeyboardInputMixin {
 
 	@Inject(
 			method = "tick",
-			at = @At("TAIL")
+			at = @At("RETURN")
 	)
 	private void irregular_implements$invertWhenCollapse(boolean isSneaking, float sneakingSpeedMultiplier, CallbackInfo ci) {
-		checkInvert();
+		if (!ClientConfig.CONFIG.collapseInvertsControls.get()) return;
+
+		Player player = AaronClientUtil.getLocalPlayer();
+		if (player == null) return;
+
+		KeyboardInput input = (KeyboardInput) (Object) this;
+
+		if (player.hasEffect(ModMobEffects.COLLAPSE)) {
+			input.leftImpulse = -input.leftImpulse;
+			input.forwardImpulse = -input.forwardImpulse;
+		}
 	}
 }
