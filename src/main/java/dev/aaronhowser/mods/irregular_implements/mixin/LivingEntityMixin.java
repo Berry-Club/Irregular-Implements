cde3095bd02fbffd82fc4e374ebcbe842ac59817
aaron <aaronhowser1@gmail.com>
2026-07-24 10:55:33 -0500

    collapse now effects mobs better

--- src/main/java/dev/aaronhowser/mods/irregular_implements/mixin/LivingEntityMixin.java
+++ src/main/java/dev/aaronhowser/mods/irregular_implements/mixin/LivingEntityMixin.java
@@ -3,13 +3,16 @@ package dev.aaronhowser.mods.irregular_implements.mixin;
 import dev.aaronhowser.mods.irregular_implements.LivingEntityFunctions;
 import dev.aaronhowser.mods.irregular_implements.block.BeanStalkBlock;
 import dev.aaronhowser.mods.irregular_implements.datagen.tag.ModItemTagsProvider;
+import dev.aaronhowser.mods.irregular_implements.registry.ModMobEffects;
 import net.minecraft.core.particles.ParticleOptions;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.Vec3;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.*;
@@ -56,6 +59,18 @@ public abstract class LivingEntityMixin extends Entity implements LivingEntityFu
 	)
 	private double irregular_implements$fasterOnStalk(double constant) {
 		return constant * BeanStalkBlock.climbingFactor((LivingEntity) (Object) this);
+	}
+
+	@ModifyVariable(
+			method = "travel",
+			at = @At("HEAD"),
+			argsOnly = true
+	)
+	private Vec3 irregular_implements$invertCollapsedMovement(Vec3 movement) {
+		LivingEntity entity = (LivingEntity) (Object) this;
+		if (entity instanceof Player || !entity.hasEffect(ModMobEffects.COLLAPSE)) return movement;
+
+		return new Vec3(-movement.x, movement.y, -movement.z);
 	}
 
 	@ModifyConstant(
